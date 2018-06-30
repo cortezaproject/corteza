c82b26436f4160a50e3ff42df445081ed573792c
Tit Petric <black@scene-si.org>
2018-07-01 01:45:22 +0200

    empty types struct field, no data

--- crm/types.structs.go
+++ crm/types.structs.go
@@ -2,18 +2,8 @@ package crm
 
 // Types
 type Types struct {
-	ID string
 }
 
 func (Types) new() *Types {
 	return &Types{}
-}
-
-func (t *Types) GetID() string {
-	return t.ID
-}
-
-func (t *Types) SetID(value string) *Types {
-	t.ID = value
-	return t
 }
