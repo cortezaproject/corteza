839e2495067db9ca00f5da40648ed5a7e0bb42c3
Tit Petric <black@scene-si.org>
2018-07-01 01:46:08 +0200

    split dep/build steps

--- build.sh
+++ build.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 set -e
 PROJECT=$(basename $(dirname $(readlink -f $0)))
-
-if [ -d "vendor" ]; then
-	docker run --rm -v $(pwd):/go/src/github.com/titpetric/$PROJECT -w /go/src/github.com/titpetric/$PROJECT -e GOOS=${OS} -e GOARCH=${ARCH} -e CGO_ENABLED=0 -e GOARM=7 titpetric/golang dep ensure -update -v
-else
-	docker run --rm -v $(pwd):/go/src/github.com/titpetric/$PROJECT -w /go/src/github.com/titpetric/$PROJECT -e GOOS=${OS} -e GOARCH=${ARCH} -e CGO_ENABLED=0 -e GOARM=7 titpetric/golang dep init
-fi
-
 NAMES=$(ls cmd/* -d | xargs -n1 basename)
 for NAME in $NAMES; do
 	OSES=${OSS:-"linux"}
---
+++ dep.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+PROJECT=$(basename $(dirname $(readlink -f $0)))
+
+if [ -d "vendor" ]; then
+	docker run --rm -v $(pwd):/go/src/github.com/titpetric/$PROJECT -w /go/src/github.com/titpetric/$PROJECT -e GOOS=${OS} -e GOARCH=${ARCH} -e CGO_ENABLED=0 -e GOARM=7 titpetric/golang dep ensure -update -v
+else
+	docker run --rm -v $(pwd):/go/src/github.com/titpetric/$PROJECT -w /go/src/github.com/titpetric/$PROJECT -e GOOS=${OS} -e GOARCH=${ARCH} -e CGO_ENABLED=0 -e GOARM=7 titpetric/golang dep init
+fi
