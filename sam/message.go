0778b0920d985cc443f581eeae2d165ac2310b4a
Tit Petric <black@scene-si.org>
2018-07-05 10:34:56 +0200

    ws fix

--- sam/message.go
+++ sam/message.go
@@ -4,24 +4,30 @@ import (
 	"github.com/pkg/errors"
 )
 
-func (m *Message) Edit(r *messageEditRequest) (interface{}, error) {
+func (*Message) Edit(r *messageEditRequest) (interface{}, error) {
 	return nil, errors.New("Not implemented: Message.edit")
 }
-func (m *Message) Attach(r *messageAttachRequest) (interface{}, error) {
+
+func (*Message) Attach(r *messageAttachRequest) (interface{}, error) {
 	return nil, errors.New("Not implemented: Message.attach")
 }
-func (m *Message) Remove(r *messageRemoveRequest) (interface{}, error) {
+
+func (*Message) Remove(r *messageRemoveRequest) (interface{}, error) {
 	return nil, errors.New("Not implemented: Message.remove")
 }
-func (m *Message) Read(r *messageReadRequest) (interface{}, error) {
+
+func (*Message) Read(r *messageReadRequest) (interface{}, error) {
 	return nil, errors.New("Not implemented: Message.read")
 }
-func (m *Message) Search(r *messageSearchRequest) (interface{}, error) {
+
+func (*Message) Search(r *messageSearchRequest) (interface{}, error) {
 	return nil, errors.New("Not implemented: Message.search")
 }
-func (m *Message) Pin(r *messagePinRequest) (interface{}, error) {
+
+func (*Message) Pin(r *messagePinRequest) (interface{}, error) {
 	return nil, errors.New("Not implemented: Message.pin")
 }
-func (m *Message) Flag(r *messageFlagRequest) (interface{}, error) {
+
+func (*Message) Flag(r *messageFlagRequest) (interface{}, error) {
 	return nil, errors.New("Not implemented: Message.flag")
 }
