2504f66af6898ad59db38355aab59d5cf8cf0210
Tit Petric <black@scene-si.org>
2018-07-06 11:24:41 +0000

    add project name, makefile

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-PROJECT=$(basename $(dirname $(readlink -f $0)))
+PROJECT=$(<.project)
 NAMES=$(ls cmd/* -d | xargs -n1 basename)
 for NAME in $NAMES; do
 	OSES=${OSS:-"linux"}
@@ -8,7 +8,7 @@ for NAME in $NAMES; do
 	for ARCH in $ARCHS; do
 		for OS in $OSES; do
 			echo $OS $ARCH $NAME
-			docker run --rm -v $(pwd):/go/src/github.com/crusttech/$PROJECT -w /go/src/github.com/crusttech/$PROJECT -e GOOS=${OS} -e GOARCH=${ARCH} -e CGO_ENABLED=0 -e GOARM=7 golang:1.10-alpine go build -o build/${NAME}-${OS}-${ARCH} cmd/${NAME}/*.go
+			docker run --rm -v $(pwd):/go/src/github.com/$PROJECT -w /go/src/github.com/$PROJECT -e GOOS=${OS} -e GOARCH=${ARCH} -e CGO_ENABLED=0 -e GOARM=7 golang:1.10-alpine go build -o build/${NAME}-${OS}-${ARCH} cmd/${NAME}/*.go
 			if [ $? -eq 0 ]; then
 				echo OK
 			fi
