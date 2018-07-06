3ff042aec10edf878c7d4cfc58818ff9590319f4
Denis Arh <denis.arh@kendu.si>
2018-07-06 10:20:41 +0200

    Add go vet <..> to test.sh
    Took 44 minutes

--- test.sh
+++ test.sh
@@ -5,4 +5,5 @@ set -u
 
 PKG=github.com/crusttech/crust
 
+go vet `cd ${GOPATH}/src/; find ${PKG} -type f -name '*.go' -and -not -path '*vendor*'|xargs -n1 dirname|uniq`
 go test `cd ${GOPATH}/src/; find ${PKG} -type f -name '*_test.go' -and -not -path '*vendor*'|xargs -n1 dirname|uniq`
