81b6f2e6081caf335d1187e031208616bde513d0
Tit Petric <black@scene-si.org>
2018-07-10 14:59:05 +0000

    print debug route info on startup, alike gin/debug.

--- sam/routes.go
+++ sam/routes.go
@@ -16,7 +16,10 @@ package sam
 */
 
 import (
+	"fmt"
 	"github.com/go-chi/chi"
+	"reflect"
+	"runtime"
 )
 
 func MountRoutes(r chi.Router) {
@@ -66,4 +69,20 @@ func MountRoutes(r chi.Router) {
 	r.Route("/websocket", func(r chi.Router) {
 		r.Get("/client", websocket.Client)
 	})
+
+	var printRoutes func(chi.Routes, string, string)
+	printRoutes = func(r chi.Routes, indent string, prefix string) {
+		routes := r.Routes()
+		for _, route := range routes {
+			if route.SubRoutes != nil && len(route.SubRoutes.Routes()) > 0 {
+				fmt.Printf(indent+"%s - with %d handlers, %d subroutes\n", route.Pattern, len(route.Handlers), len(route.SubRoutes.Routes()))
+				printRoutes(route.SubRoutes, indent+"\t", prefix+route.Pattern[:len(route.Pattern)-2])
+			} else {
+				for key, fn := range route.Handlers {
+					fmt.Printf("%s%s\t%s -> %s\n", indent, key, prefix+route.Pattern, runtime.FuncForPC(reflect.ValueOf(fn).Pointer()).Name())
+				}
+			}
+		}
+	}
+	printRoutes(r, "", "")
 }
