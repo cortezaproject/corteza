0662fd76a6652df60168287ac4f848c39ec1ef2b
Denis Arh <denis.arh@kendu.si>
2018-07-11 20:32:19 +0200

    Add deps & protoc to makefile
    Took 11 minutes

--- dep.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-PROJECT=$(<.project)
-
-if [ -d "vendor" ]; then
-	docker run --rm -v $(pwd):/go/src/github.com/$PROJECT -w /go/src/github.com/$PROJECT -e GOOS=${OS} -e GOARCH=${ARCH} -e CGO_ENABLED=0 -e GOARM=7 titpetric/golang dep ensure -update -v
-else
-	docker run --rm -v $(pwd):/go/src/github.com/$PROJECT -w /go/src/github.com/$PROJECT -e GOOS=${OS} -e GOARCH=${ARCH} -e CGO_ENABLED=0 -e GOARM=7 titpetric/golang dep init
-fi
