fd8efa0b2af8ece0d4ba50c25505ea4f66a09d9a
Denis Arh <denis.arh@kendu.si>
2018-07-12 19:33:24 +0200

    Always generate db tag (either from dbname or name)

--- sam/_gen.php
+++ sam/_gen.php
@@ -81,3 +81,13 @@ foreach (array("routes") as $type) {
 
 	file_put_contents($filename, $contents);
 }
+
+// camel case to snake case
+function decamel($input) {
+  preg_match_all('!([A-Z][A-Z0-9]*(?=$|[A-Z][a-z0-9])|[A-Za-z][a-z0-9]+)!', $input, $matches);
+  $ret = $matches[0];
+  foreach ($ret as &$match) {
+    $match = $match == strtoupper($match) ? strtolower($match) : lcfirst($match);
+  }
+  return implode('_', $ret);
+}
