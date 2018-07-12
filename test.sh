dc701134737cb3ce1485d8e44be8b4704f7af929
Denis Arh <denis.arh@kendu.si>
2018-07-12 17:33:35 +0200

    Makefile cleanup & structurize
     - makefile is now divided in sections
     - remove separate test.sh and added 3 additional tasks (qa, qa.test & qa.vet)
     - add realize tool (watch & rebuild) along with .realize.yaml config

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-set -u
-
-PKG="github.com/$(<.project)"
-
-go vet `cd ${GOPATH}/src/; find ${PKG} -type f -name '*.go' -and -not -path '*vendor*'|xargs -n1 dirname|uniq`
-go test `cd ${GOPATH}/src/; find ${PKG} -type f -name '*_test.go' -and -not -path '*vendor*'|xargs -n1 dirname|uniq`
