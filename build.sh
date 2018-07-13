536fe4fa2ecafb70bdaf1ee8e3482d035a324380
Tit Petric <black@scene-si.org>
2018-07-13 14:35:57 +0200

    use go build directly

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ for NAME in $NAMES; do
 	for ARCH in $ARCHS; do
 		for OS in $OSES; do
 			echo $OS $ARCH $NAME
-			docker run --rm -v $(pwd):/go/src/github.com/$PROJECT -w /go/src/github.com/$PROJECT -e GOOS=${OS} -e GOARCH=${ARCH} -e CGO_ENABLED=0 -e GOARM=7 golang:1.10-alpine go build -o build/${NAME}-${OS}-${ARCH} cmd/${NAME}/*.go
+			GOOS=${OS} GOARCH=${ARCH} CGO_ENABLED=0 GOARM=7 go build -o build/${NAME}-${OS}-${ARCH} cmd/${NAME}/*.go
 			if [ $? -eq 0 ]; then
 				echo OK
 			fi
