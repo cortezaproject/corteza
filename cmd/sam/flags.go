ba4f5a08adbe3f86a2cef44373bf41ee21e0ae19
Tit Petric <black@scene-si.org>
2018-07-17 11:32:00 +0200

    upd(main): restructure flags

--- cmd/sam/flags.go
+++ cmd/sam/flags.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"github.com/namsral/flag"
 	_ "github.com/joho/godotenv/autoload"
+	"github.com/namsral/flag"
 
 	"github.com/crusttech/crust/rbac"
 )
 
 type configuration struct {
 	httpAddr string
-	dbDSN string
+	dbDSN    string
 }
 
 func flags(prefix string) configuration {
@@ -19,8 +19,8 @@ func flags(prefix string) configuration {
 		return prefix + "-" + s
 	}
 
-	flag.StringVar(&config.addr, p("http-addr"), ":3000", "Listen address for HTTP server")
-	flag.StringVar(&config.dsn, p("db-dsn"), "crust:crust@tcp(db1:3306)/crust?collation=utf8mb4_general_ci", "DSN for database connection")
+	flag.StringVar(&config.httpAddr, p("http-addr"), ":3000", "Listen address for HTTP server")
+	flag.StringVar(&config.dbDSN, p("db-dsn"), "crust:crust@tcp(db1:3306)/crust?collation=utf8mb4_general_ci", "DSN for database connection")
 	rbac.Flags()
 	flag.Parse()
 	return config
