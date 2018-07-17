5c6d05b9ffa82de740837eadca033e207e85e6d0
Tit Petric <black@scene-si.org>
2018-07-17 11:09:06 +0200

    update(all): use namsral/flag in main

--- cmd/sam/main.go
+++ cmd/sam/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"flag"
 	"log"
 	"net"
 	"os"
@@ -9,18 +8,9 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi"
-	_ "github.com/joho/godotenv/autoload"
 
 	"github.com/crusttech/crust/sam/rest"
 	"github.com/titpetric/factory"
-)
-
-const (
-	defaultAddr = ":3000"
-	defaultDsn  = "crust:crust@tcp(db1:3306)/crust?collation=utf8mb4_general_ci"
-
-	envVarKey_HTTP_ADDR = "SAM_HTTP_ADDR"
-	envVarKey_DB_DSN    = "SAM_DB_DSN"
 )
 
 func handleError(err error, message string) {
@@ -33,38 +23,21 @@ func handleError(err error, message string) {
 }
 
 func main() {
-	var envHttpAddr, envDbDsn string
-	var has bool
-
-	if envHttpAddr, has = os.LookupEnv(envVarKey_HTTP_ADDR); !has {
-		envHttpAddr = defaultAddr
-	}
-
-	if envDbDsn, has = os.LookupEnv(envVarKey_DB_DSN); !has {
-		envDbDsn = defaultDsn
-	}
-
-	var (
-		// set up flags
-		addr = flag.String("addr", envHttpAddr, "Listen address for HTTP server")
-		dsn  = flag.String("dsn", envDbDsn, "DSN for database connection")
-	)
-
-	flag.Parse()
+	config := flags("sam")
 
 	// log to stdout not stderr
 	log.SetOutput(os.Stdout)
 
 	// set up database connection
-	factory.Database.Add("default", dsn)
+	factory.Database.Add("default", config.dbDSN)
 	db, err := factory.Database.Get()
 	handleError(err, "Can't connect to database")
 	db.Profiler = &factory.Database.ProfilerStdout
 
 	// listen socket for http server
-	log.Println("Starting http server on address " + *addr)
-	listener, err := net.Listen("tcp", *addr)
-	handleError(err, "Can't listen on addr "+*addr)
+	log.Println("Starting http server on address " + config.httpAddr)
+	listener, err := net.Listen("tcp", config.httpAddr)
+	handleError(err, "Can't listen on addr "+config.httpAddr)
 
 	// route options
 	routeOptions, err := RouteOptions{}.New()
