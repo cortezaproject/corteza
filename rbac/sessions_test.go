f77fb432a667ab670fcd116654336031dabed071
Tit Petric <black@scene-si.org>
2018-07-23 14:00:35 +0200

    upd(rbac): full sessions impl and tests

--- rbac/sessions_test.go
+++ rbac/sessions_test.go
@@ -9,25 +9,81 @@ func TestSessions(t *testing.T) {
 	if err != nil {
 		t.Errorf("Unexpected error when creating RBAC instance: %+v", err)
 	}
-	rbac.Debug(false)
+	rbac.Debug("info")
 
 	sessions := rbac.Sessions()
 	users := rbac.Users()
+	roles := rbac.Roles()
 
 	// clean up data
 	users.Delete("test-user")
 	sessions.Delete("test-session")
+	roles.Delete("test-role")
+
+	if err := roles.Create("test-role"); err != nil {
+		t.Errorf("Unexpected error when creating test-role, %+v", err)
+		return
+	}
 
 	if err := users.Create("test-user", "test-password"); err != nil {
 		t.Errorf("Unexpected error when creating test-user, %+v", err)
 		return
 	}
-	if err := sessions.Create("test-session", "test-user", []string{}); err != nil {
-		t.Errorf("Unexpected error when creating test-session, %+v", err)
+
+	if err := users.Assign("test-user", "test-role"); err != nil {
+		t.Errorf("Unexpected error when assigning test-role to test-user, %+v", err)
+		return
 	}
 
-	if _, err := sessions.Get("test-session"); err != nil {
-		t.Errorf("Unexpected error when getting test-session, %+v", err)
+	if err := sessions.Create("test-session", "test-user", "test-role"); err != nil {
+		t.Errorf("Unexpected error when creating test-session, %+v", err)
+		return
+	}
+
+	// check role is created
+	{
+		session, err := sessions.Get("test-session")
+		assert(t, err == nil, "Unexpected error when getting test-session, %+v", err)
+		// @todo: DAASI should return session ID from a get-query as well
+		// assert(t, session.ID == "test-session", "Unexpected returned Session ID, test-session != '%s'", session.ID)
+		assert(t, session.Username == "test-user", "Unexpected returned user, test-user != '%s'", session.Username)
+		if !assert(t, len(session.Roles) == 1, "Expected one session role, got %+v", session.Roles) {
+			return
+		}
+		assert(t, session.Roles[0] == "test-role", "Unexpected session role, test-role != '%s'", session.Roles[0])
+	}
+
+	if err := sessions.DeactivateRole("test-session", "test-role"); err != nil {
+		t.Errorf("Unexpected error when deactivating session role, %+v", err)
+	}
+
+	// check role is deactivated
+	{
+		session, err := sessions.Get("test-session")
+		assert(t, err == nil, "Unexpected error when getting test-session, %+v", err)
+		// @todo: DAASI should return session ID from a get-query as well
+		// assert(t, session.ID == "test-session", "Unexpected returned Session ID, test-session != '%s'", session.ID)
+		assert(t, session.Username == "test-user", "Unexpected returned user, test-user != '%s'", session.Username)
+		if !assert(t, len(session.Roles) == 0, "Expected one session role, got %+v", session.Roles) {
+			return
+		}
+	}
+
+	if err := sessions.ActivateRole("test-session", "test-role"); err != nil {
+		t.Errorf("Unexpected error when deactivating session role, %+v", err)
+	}
+
+	// check role is activated
+	{
+		session, err := sessions.Get("test-session")
+		assert(t, err == nil, "Unexpected error when getting test-session, %+v", err)
+		// @todo: DAASI should return session ID from a get-query as well
+		// assert(t, session.ID == "test-session", "Unexpected returned Session ID, test-session != '%s'", session.ID)
+		assert(t, session.Username == "test-user", "Unexpected returned user, test-user != '%s'", session.Username)
+		if !assert(t, len(session.Roles) == 1, "Expected one session role, got %+v", session.Roles) {
+			return
+		}
+		assert(t, session.Roles[0] == "test-role", "Unexpected session role, test-role != '%s'", session.Roles[0])
 	}
 
 	if err := sessions.Delete("test-session"); err != nil {
