bf67bac35137b8a0021720cbd57194957241b011
Tit Petric <black@scene-si.org>
2018-07-23 14:08:39 +0200

    upd(rbac): update api call response types

--- rbac/types/structs.go
+++ rbac/types/structs.go
@@ -3,9 +3,12 @@ package types
 type (
 	User struct {
 		Username string `json:"username"`
+		AssignedRoles []string `json:"assignedRoles"`
+		AuthorizedRoles []string `json:"authorizedRoles"`
 	}
 
 	Session struct {
+		ID string `json:"session"`
 		Username string `json:"username"`
 		Roles []string `json:"roles"`
 	}
