69e13337225120ac86fb57bc932ad7c6aa3cdc3a
Tit Petric <black@scene-si.org>
2018-08-15 17:11:43 +0200

    upd(crm): update sql for module content

--- crm/db/schema/mysql/20180704080000.base.up.sql
+++ crm/db/schema/mysql/20180704080000.base.up.sql
@@ -4,6 +4,13 @@ CREATE TABLE `crm_module` (
  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+CREATE TABLE `crm_module_content` (
+ `id` bigint(20) unsigned NOT NULL,
+ `module_id` bigint(20) unsigned NOT NULL,
+ `json` json NOT NULL COMMENT 'Actual CRM module form fields as a JSON blob',
+ PRIMARY KEY (`id`,`module_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 CREATE TABLE `crm_module_form` (
  `module_id` bigint(20) unsigned NOT NULL,
  `place` tinyint(3) unsigned NOT NULL,
@@ -12,4 +19,5 @@ CREATE TABLE `crm_module_form` (
  `title` varchar(255) NOT NULL COMMENT 'The label of the form input',
  `placeholder` varchar(255) NOT NULL COMMENT 'Placeholder text (if supported by input)',
  PRIMARY KEY (`module_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
