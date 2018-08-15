4625a473a3e4cb9604dab45a0aaccf4e55bc67f8
Tit Petric <black@scene-si.org>
2018-08-15 20:30:56 +0200

    upd(crm): update db schema

--- crm/db/schema/mysql/20180704080000.base.up.sql
+++ crm/db/schema/mysql/20180704080000.base.up.sql
@@ -1,13 +1,14 @@
 CREATE TABLE `crm_module` (
  `id` bigint(20) unsigned NOT NULL,
  `name` varchar(64) NOT NULL COMMENT 'The name of the module',
+ `json` json NOT NULL COMMENT 'List of field definitions for the module',
  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `crm_module_content` (
  `id` bigint(20) unsigned NOT NULL,
  `module_id` bigint(20) unsigned NOT NULL,
- `json` json NOT NULL COMMENT 'Actual CRM module form fields as a JSON blob',
+ `json` json NOT NULL COMMENT 'Entry JSON blob based on module fields list',
  PRIMARY KEY (`id`,`module_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
