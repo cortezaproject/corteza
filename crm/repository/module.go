4a40060fbe8a6496348aad97557b5d032b23581e
Tit Petric <black@scene-si.org>
2018-08-15 20:31:38 +0200

    upd(crm): test friendly refactoring, gofmt

--- crm/repository/module.go
+++ crm/repository/module.go
@@ -36,25 +36,16 @@ func (r *module) With(ctx context.Context) Module {
 	}
 }
 
+// @todo: update to accepted DeletedAt column semantics from SAM
 
 func (r *module) FindByID(id uint64) (*types.Module, error) {
 	mod := &types.Module{}
-	if err := r.db().Get(mod, "SELECT * FROM crm_module WHERE id = ?", id); err != nil {
-		println(err.Error())
-		return nil, ErrDatabaseError
-	} else {
-		return mod, nil
-	}
+	return mod, r.db().Get(mod, "SELECT * FROM crm_module WHERE id=?", id)
 }
 
 func (r *module) Find() ([]*types.Module, error) {
 	mod := make([]*types.Module, 0)
-	if err := r.db().Select(&mod, "SELECT * FROM crm_module ORDER BY name ASC"); err != nil {
-		println(err.Error())
-		return nil, ErrDatabaseError
-	} else {
-		return mod, nil
-	}
+	return mod, r.db().Select(&mod, "SELECT * FROM crm_module ORDER BY name ASC")
 }
 
 func (r *module) Create(mod *types.Module) (*types.Module, error) {
@@ -64,13 +55,9 @@ func (r *module) Create(mod *types.Module) (*types.Module, error) {
 
 func (r *module) Update(mod *types.Module) (*types.Module, error) {
 	return mod, r.db().Replace("crm_module", mod)
-
 }
 
 func (r *module) DeleteByID(id uint64) error {
-	if _, err := r.db().Exec("DELETE FROM crm_module WHERE ID = ?", id); err != nil {
-		return ErrDatabaseError
-	} else {
-		return nil
-	}
+	_, err := r.db().Exec("DELETE FROM crm_module WHERE id=?", id)
+	return err
 }
