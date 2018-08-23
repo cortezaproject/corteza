491eba246fbd40ca7a88d890881c56dcbcbc1124
Tit Petric <black@scene-si.org>
2018-08-23 11:07:12 +0000

    add(crm): database table for crm_fields

--- crm/db/schema/mysql/20180704080000.base.up.sql
+++ crm/db/schema/mysql/20180704080000.base.up.sql
@@ -1,3 +1,10 @@
+CREATE TABLE `crm_fields` (
+ `field_type` varchar(16) NOT NULL COMMENT 'Short field type (string, boolean,...)',
+ `field_name` varchar(255) NOT NULL COMMENT 'Description of field contents',
+ `field_template` varchar(255) NOT NULL COMMENT 'HTML template file for field',
+ PRIMARY KEY (`field_type`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 CREATE TABLE `crm_module` (
  `id` bigint(20) unsigned NOT NULL,
  `name` varchar(64) NOT NULL COMMENT 'The name of the module',
@@ -22,3 +29,10 @@ CREATE TABLE `crm_module_form` (
  PRIMARY KEY (`module_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+INSERT INTO `crm_fields` VALUES ('bool','Boolean value (yes / no)','');
+INSERT INTO `crm_fields` VALUES ('email','E-mail input','');
+INSERT INTO `crm_fields` VALUES ('enum','Single option picker','');
+INSERT INTO `crm_fields` VALUES ('hidden','Hidden value','');
+INSERT INTO `crm_fields` VALUES ('stamp','Date/time input','');
+INSERT INTO `crm_fields` VALUES ('text','Text input','');
+INSERT INTO `crm_fields` VALUES ('textarea','Text input (multi-line)','');
