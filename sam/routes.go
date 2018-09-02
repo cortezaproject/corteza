2a55cc54c55fd8594ce719a024a751a2e0684aa1
Denis Arh <denis.arh@kendu.si>
2018-09-02 15:42:32 +0200

    Moved CORS handler 1 level lower to catch all requests
    Prior this, we failed to set CORS on invalid and expired JWT

--- sam/routes.go
+++ sam/routes.go
@@ -7,19 +7,10 @@ import (
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
-	"github.com/go-chi/cors"
 )
 
 func mountRoutes(r chi.Router, opts *configuration, mounts ...func(r chi.Router)) {
-	// CORS for local development...
-	cors := cors.New(cors.Options{
-		AllowedOrigins:   []string{"*"},
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"},
-		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
-		AllowCredentials: true,
-		MaxAge:           300, // Maximum value not ignored by any of major browsers
-	})
-	r.Use(cors.Handler)
+	r.Use(handleCORS)
 
 	if opts.http.logging {
 		r.Use(middleware.Logger)
