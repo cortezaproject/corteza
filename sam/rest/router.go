6fafd2e7406d8935ff39e37b501118ba61403105
Tit Petric <black@scene-si.org>
2018-09-04 15:38:48 +0200

    upd(crm,sam): update auth pkg usage

--- sam/rest/router.go
+++ sam/rest/router.go
@@ -2,12 +2,13 @@ package rest
 
 import (
 	"github.com/crusttech/crust/auth"
+	"github.com/crusttech/crust/auth/types"
 	"github.com/crusttech/crust/sam/rest/handlers"
 	"github.com/crusttech/crust/sam/service"
 	"github.com/go-chi/chi"
 )
 
-func MountRoutes(jwtAuth auth.TokenEncoder) func(chi.Router) {
+func MountRoutes(jwtAuth types.TokenEncoder) func(chi.Router) {
 	// Initialize services
 	var (
 		channelSvc      = service.Channel()
