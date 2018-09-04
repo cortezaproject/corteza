6632a6629943694be5847ae738b7ee157dccad1b
Tit Petric <black@scene-si.org>
2018-09-04 15:38:25 +0200

    upd(codegen): optionally generate README docs

--- codegen.sh
+++ codegen.sh
@@ -5,7 +5,9 @@ _PWD=$PWD
 SPECS=$(find $PWD -name 'spec.json' | xargs -n1 dirname)
 for SPEC in $SPECS; do
 	echo "=== spec $SPEC ==="
-	cd $SPEC && rm -rf spec && /usr/bin/env spec && cd .. && ./README.php && cd $_PWD
+	if [ -x "$(dirname $SPEC)/README.php" ]; then
+		cd $SPEC && rm -rf spec && /usr/bin/env spec && cd .. && ./README.php && cd $_PWD
+	fi
 
 	SRC=$(dirname $(dirname $SPEC))
 	echo "=== codegen $SRC ==="
