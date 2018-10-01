8d84f8ec03fe830820a674f13ba7fa0656a6a39d
Tit Petric <black@scene-si.org>
2018-10-01 15:00:04 +0200

    add(sam): resource hints for org/chan/team

--- sam/types/team.go
+++ sam/types/team.go
@@ -1,6 +1,7 @@
 package types
 
 import (
+	"fmt"
 	"time"
 )
 
@@ -20,3 +21,18 @@ type (
 		Query string
 	}
 )
+
+// Scope returns permissions group that for this type
+func (r *Team) Scope() string {
+	return "team"
+}
+
+// Resource returns a RBAC resource ID for this type
+func (r *Team) Resource() string {
+	return fmt.Sprintf("%s:%d", r.Scope(), r.ID)
+}
+
+// Operation returns a RBAC resource-scoped role name for an operation
+func (r *Team) Operation(name string) string {
+	return fmt.Sprintf("%s/%s", r.Resource(), name)
+}
