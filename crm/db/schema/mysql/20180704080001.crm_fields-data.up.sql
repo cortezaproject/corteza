8b8684d3e5646eb8f1d4158f69f337f1248e1ee8
Tit Petric <black@scene-si.org>
2018-10-08 21:08:56 +0000

    upd(crm): update db schema

--- crm/db/schema/mysql/20180704080001.crm_fields-data.up.sql
+++ crm/db/schema/mysql/20180704080001.crm_fields-data.up.sql
@@ -1,7 +1,7 @@
-INSERT INTO `crm_fields` VALUES ('bool','Boolean value (yes / no)','');
-INSERT INTO `crm_fields` VALUES ('email','E-mail input','');
-INSERT INTO `crm_fields` VALUES ('enum','Single option picker','');
-INSERT INTO `crm_fields` VALUES ('hidden','Hidden value','');
-INSERT INTO `crm_fields` VALUES ('stamp','Date/time input','');
-INSERT INTO `crm_fields` VALUES ('text','Text input','');
-INSERT INTO `crm_fields` VALUES ('textarea','Text input (multi-line)','');
+INSERT INTO `crm_field` VALUES ('bool','Boolean value (yes / no)','');
+INSERT INTO `crm_field` VALUES ('email','E-mail input','');
+INSERT INTO `crm_field` VALUES ('enum','Single option picker','');
+INSERT INTO `crm_field` VALUES ('hidden','Hidden value','');
+INSERT INTO `crm_field` VALUES ('stamp','Date/time input','');
+INSERT INTO `crm_field` VALUES ('text','Text input','');
+INSERT INTO `crm_field` VALUES ('textarea','Text input (multi-line)','');
