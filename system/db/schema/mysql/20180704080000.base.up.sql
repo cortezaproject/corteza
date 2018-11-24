985966d18840d10925aef8f0ce5a6080bec54ffc
Tit Petric <black@scene-si.org>
2018-11-24 20:41:36 +0000

    upd(sam,system): moved system tables away from sam

--- system/db/schema/mysql/20180704080000.base.up.sql
+++ system/db/schema/mysql/20180704080000.base.up.sql
@@ -1,3 +1,17 @@
+-- all known organisations (crust instances) and our relation towards them
+CREATE TABLE organisations (
+  id               BIGINT UNSIGNED NOT NULL,
+  fqn              TEXT            NOT NULL, -- fully qualified name of the organisation
+  name             TEXT            NOT NULL, -- display name of the organisation
+
+  created_at       DATETIME        NOT NULL DEFAULT NOW(),
+  updated_at       DATETIME            NULL,
+  archived_at      DATETIME            NULL,
+  deleted_at       DATETIME            NULL, -- organisation soft delete
+
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 CREATE TABLE settings (
   name  VARCHAR(200) NOT NULL   COMMENT 'Unique set of setting keys',
   value TEXT                    COMMENT 'Setting value',
@@ -28,3 +42,25 @@ CREATE TABLE users (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE UNIQUE INDEX uid_satosa ON users (satosa_id);
+
+-- Keeps all known teams
+CREATE TABLE teams (
+  id               BIGINT UNSIGNED NOT NULL,
+  name             TEXT            NOT NULL, -- display name of the team
+  handle           TEXT            NOT NULL, -- team handle string
+
+  created_at       DATETIME        NOT NULL DEFAULT NOW(),
+  updated_at       DATETIME            NULL,
+  archived_at      DATETIME            NULL,
+  deleted_at       DATETIME            NULL, -- team soft delete
+
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Keeps team memberships
+CREATE TABLE team_members (
+  rel_team         BIGINT UNSIGNED NOT NULL REFERENCES organisation(id),
+  rel_user         BIGINT UNSIGNED NOT NULL,
+
+  PRIMARY KEY (rel_team, rel_user)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
