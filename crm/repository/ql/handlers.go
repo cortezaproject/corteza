bc463fa8e1d76796c247628a26659e22376d101d
Mitja Zivkovic <mitjaz@gmail.com>
2019-01-03 22:05:36 +0100

    sorting with AST ParseColumns

--- crm/repository/ql/handlers.go
+++ crm/repository/ql/handlers.go
@@ -18,3 +18,22 @@ func MakeIdentWrapHandler(wrap string, ss ...string) IdentHandler {
 		return i, nil
 	}
 }
+
+// MakeIdentOrderWrapHandler makes an Ident wrapper for Order columns
+//
+// Functionality is the same as MakeIdentWrapHandler with extra padding.
+func MakeIdentOrderWrapHandler(wrap string, ss ...string) IdentHandler {
+	return func(i Ident) (Ident, error) {
+		for _, s := range ss {
+			if s == i.Value {
+				i.Value += " "
+				return i, nil
+			}
+		}
+
+		i.Args = []interface{}{i.Value}
+		i.Value = wrap + " "
+
+		return i, nil
+	}
+}
