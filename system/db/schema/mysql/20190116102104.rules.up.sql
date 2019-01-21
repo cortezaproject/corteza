d9761cb7a17adde06d09969c59819a6585ee7a56
Mitja Zivkovic <mitjaz@gmail.com>
2019-01-21 10:41:01 +0100

    upd(internal): rules.value as tinyint

--- system/db/schema/mysql/20190116102104.rules.up.sql
+++ system/db/schema/mysql/20190116102104.rules.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE `sys_rules` (
   `rel_team` BIGINT UNSIGNED NOT NULL,
   `resource` VARCHAR(128) NOT NULL,
   `operation` VARCHAR(128) NOT NULL,
-  `value` ENUM('no', 'yes') NOT NULL,
+  `value` TINYINT(1) NOT NULL,
 
   PRIMARY KEY (`rel_team`, `resource`, `operation`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
