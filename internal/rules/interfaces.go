b2d797e7d3fa10f2929d4f5acbf42f3136e2145b
Mitja Zivkovic <mitjaz@gmail.com>
2019-01-21 19:47:32 +0100

    upd(internal): types.Access and rules(Allow/Deny/Inherit)

--- internal/rules/interfaces.go
+++ internal/rules/interfaces.go
@@ -4,6 +4,8 @@ import (
 	"context"
 
 	"github.com/titpetric/factory"
+
+	"github.com/crusttech/crust/system/types"
 )
 
 type ResourcesInterface interface {
@@ -12,5 +14,5 @@ type ResourcesInterface interface {
 	CheckAccessMulti(resource string, operation string) error
 	CheckAccess(resource string, operation string) error
 
-	Grant(resource string, teamID uint64, operations []string, value Access) error
+	Grant(resource string, teamID uint64, operations []string, value types.Access) error
 }
