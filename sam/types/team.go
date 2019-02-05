7e04d9fc3a7695997e9f22deef76f6b8b7544fd1
Tit Petric <black@scene-si.org>
2019-02-05 14:19:44 +0100

    upd(crm): add resources and scopes to system types

--- sam/types/team.go
+++ sam/types/team.go
@@ -1,26 +1,11 @@
 package types
 
 import (
-	"fmt"
-
 	"github.com/crusttech/crust/system/types"
 )
 
 type (
-	Team types.Team
+	Team struct {
+		types.Team
+	}
 )
-
-// Scope returns permissions group that for this type
-func (r *Team) Scope() string {
-	return "team"
-}
-
-// Resource returns a RBAC resource ID for this type
-func (r *Team) Resource() string {
-	return fmt.Sprintf("%s:%d", r.Scope(), r.ID)
-}
-
-// Operation returns a RBAC resource-scoped role name for an operation
-func (r *Team) Operation(name string) string {
-	return fmt.Sprintf("%s/%s", r.Resource(), name)
-}
