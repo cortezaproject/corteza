2e48a5560331230a55ada5e944d468b26d2f9c3c
Tit Petric <black@scene-si.org>
2019-03-15 02:17:58 +0100

    fix(all): better system Init, flags

--- cmd/messaging/main.go
+++ cmd/messaging/main.go
@@ -6,12 +6,15 @@ import (
 
 	context "github.com/SentimensRG/ctx"
 	"github.com/SentimensRG/ctx/sigctx"
+	_ "github.com/joho/godotenv/autoload"
 	"github.com/namsral/flag"
 
 	"github.com/crusttech/crust/internal/auth"
 	"github.com/crusttech/crust/internal/subscription"
 	"github.com/crusttech/crust/internal/version"
-	sub "github.com/crusttech/crust/messaging"
+
+	messaging "github.com/crusttech/crust/messaging"
+	system "github.com/crusttech/crust/system"
 )
 
 func main() {
@@ -22,14 +25,18 @@ func main() {
 
 	ctx := context.AsContext(sigctx.New())
 
-	flags(
-		"messaging",
-		sub.Flags,
-		auth.Flags,
-		subscription.Flags,
-	)
+	messaging.Flags("messaging")
+	system.Flags("system")
 
-	if err := sub.Init(); err != nil {
+	auth.Flags()
+	subscription.Flags()
+
+	flag.Parse()
+
+	if err := system.Init(); err != nil {
+		log.Fatalf("Error initializing: %+v", err)
+	}
+	if err := messaging.Init(); err != nil {
 		log.Fatalf("Error initializing: %+v", err)
 	}
 
@@ -46,7 +53,7 @@ func main() {
 		// Disabled for now, system service is the only one that validates subscription
 		// ctx = subscription.Monitor(ctx)
 
-		if err := sub.StartRestAPI(ctx); err != nil {
+		if err := messaging.StartRestAPI(ctx); err != nil {
 			log.Fatalf("Error starting/running: %+v", err)
 		}
 	}
--- cmd/system/main.go
+++ cmd/system/main.go
@@ -6,12 +6,14 @@ import (
 
 	context "github.com/SentimensRG/ctx"
 	"github.com/SentimensRG/ctx/sigctx"
+	_ "github.com/joho/godotenv/autoload"
 	"github.com/namsral/flag"
 
 	"github.com/crusttech/crust/internal/auth"
 	"github.com/crusttech/crust/internal/subscription"
 	"github.com/crusttech/crust/internal/version"
-	sub "github.com/crusttech/crust/system"
+
+	system "github.com/crusttech/crust/system"
 )
 
 func main() {
@@ -22,14 +24,14 @@ func main() {
 
 	ctx := context.AsContext(sigctx.New())
 
-	flags(
-		"system",
-		sub.Flags,
-		auth.Flags,
-		subscription.Flags,
-	)
+	system.Flags("system")
 
-	if err := sub.Init(); err != nil {
+	auth.Flags()
+	subscription.Flags()
+
+	flag.Parse()
+
+	if err := system.Init(); err != nil {
 		log.Fatalf("Error initializing: %+v", err)
 	}
 
@@ -45,7 +47,7 @@ func main() {
 		// Checks subscription, will os.Exit(1) if there is an error
 		ctx = subscription.Monitor(ctx)
 
-		if err := sub.StartRestAPI(ctx); err != nil {
+		if err := system.StartRestAPI(ctx); err != nil {
 			log.Fatalf("Error starting/running: %+v", err)
 		}
 	}
