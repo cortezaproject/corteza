37f24d01fb60af52b26bc9f123ce649fb4a27862
Tit Petric <black@scene-si.org>
2019-03-31 16:51:54 +0200

    fix(messaging): import codegen files

--- messaging/types/command.gen.go
+++ messaging/types/command.gen.go
@@ -8,11 +8,6 @@ type (
 	//
 	// This type is auto-generated.
 	CommandSet []*Command
-
-	// CommandParamSet slice of CommandParam
-	//
-	// This type is auto-generated.
-	CommandParamSet []*CommandParam
 )
 
 // Walk iterates through every slice item and calls w(Command) err
@@ -34,36 +29,6 @@ func (set CommandSet) Walk(w func(*Command) error) (err error) {
 func (set CommandSet) Filter(f func(*Command) (bool, error)) (out CommandSet, err error) {
 	var ok bool
 	out = CommandSet{}
-	for i := range set {
-		if ok, err = f(set[i]); err != nil {
-			return
-		} else if ok {
-			out = append(out, set[i])
-		}
-	}
-
-	return
-}
-
-// Walk iterates through every slice item and calls w(CommandParam) err
-//
-// This function is auto-generated.
-func (set CommandParamSet) Walk(w func(*CommandParam) error) (err error) {
-	for i := range set {
-		if err = w(set[i]); err != nil {
-			return
-		}
-	}
-
-	return
-}
-
-// Filter iterates through every slice item, calls f(CommandParam) (bool, err) and return filtered slice
-//
-// This function is auto-generated.
-func (set CommandParamSet) Filter(f func(*CommandParam) (bool, error)) (out CommandParamSet, err error) {
-	var ok bool
-	out = CommandParamSet{}
 	for i := range set {
 		if ok, err = f(set[i]); err != nil {
 			return
