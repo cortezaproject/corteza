54403fca4a458c6cda62fb15dbefc351c3d346fb
Tit Petric <black@scene-si.org>
2019-04-03 14:05:50 +0200

    upd(crm): make repository consistent

--- crm/internal/repository/repository.go
+++ crm/internal/repository/repository.go
@@ -4,6 +4,10 @@ import (
 	"context"
 
 	"github.com/titpetric/factory"
+
+	"github.com/crusttech/crust/internal/auth"
+	"github.com/crusttech/crust/internal/organization"
+	"github.com/crusttech/crust/messaging/types"
 )
 
 type (
@@ -18,6 +22,19 @@ func DB(ctx context.Context) *factory.DB {
 	return factory.Database.MustGet("crm").With(ctx)
 }
 
+// Identity returns the User ID from context
+func Identity(ctx context.Context) uint64 {
+	return auth.GetIdentityFromContext(ctx).Identity()
+}
+
+// Organisation returns the Organisation from context
+func Organization(ctx context.Context) *types.Organisation {
+	return &types.Organisation{
+		organization.GetFromContext(ctx),
+	}
+}
+
+// With updates repository and database contexts
 func (r *repository) With(ctx context.Context, db *factory.DB) *repository {
 	return &repository{
 		ctx: ctx,
