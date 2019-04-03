971c5899eead1f9a4038dc1720d4d841ddbeedc3
Tit Petric <black@scene-si.org>
2019-04-03 14:55:42 +0200

    upd(messaging): repository tests import

--- messaging/internal/repository/message_test.go
+++ messaging/internal/repository/message_test.go
@@ -1,12 +1,14 @@
+// +build integration
+
 package repository
 
 import (
 	"context"
+	"testing"
 
 	"github.com/titpetric/factory"
 
-	"testing"
-
+	"github.com/crusttech/crust/internal/test"
 	"github.com/crusttech/crust/messaging/types"
 )
 
@@ -27,38 +29,38 @@ func TestMessage(t *testing.T) {
 
 	tx(t, func() error {
 		ch := &types.Channel{}
-		ch, err = chRpo.CreateChannel(ch)
+		ch, err = chRpo.Create(ch)
 		ch.Type = types.ChannelTypePublic
 
 		msg := &types.Message{ChannelID: ch.ID}
 
 		msg.Message = msg1
-		msg, err = msgRpo.CreateMessage(msg)
-		assert(t, err == nil, "CreateMessage error: %v", err)
-		assert(t, msg.Message == msg1, "Changes were not stored")
+		msg, err = msgRpo.Create(msg)
+		test.Assert(t, err == nil, "CreateMessage error: %+v", err)
+		test.Assert(t, msg.Message == msg1, "Changes were not stored")
 
 		{
 			msg.Message = msg2
-			msg, err = msgRpo.UpdateMessage(msg)
-			assert(t, err == nil, "UpdateMessage error: %v", err)
-			assert(t, msg.Message == msg2, "Changes were not stored")
+			msg, err = msgRpo.Update(msg)
+			test.Assert(t, err == nil, "UpdateMessage error: %+v", err)
+			test.Assert(t, msg.Message == msg2, "Changes were not stored")
 		}
 
 		{
-			msg, err = msgRpo.FindMessageByID(msg.ID)
-			assert(t, err == nil, "FindMessageByID error: %v", err)
-			assert(t, msg.Message == msg2, "Changes were not stored")
+			msg, err = msgRpo.FindByID(msg.ID)
+			test.Assert(t, err == nil, "FindMessageByID error: %+v", err)
+			test.Assert(t, msg.Message == msg2, "Changes were not stored")
 		}
 
 		{
-			mm, err = msgRpo.FindMessages(&types.MessageFilter{Query: msg2})
-			assert(t, err == nil, "FindMessages error: %v", err)
-			assert(t, len(mm) > 0, "No results found")
+			mm, err = msgRpo.Find(&types.MessageFilter{Query: msg2})
+			test.Assert(t, err == nil, "FindMessages error: %+v", err)
+			test.Assert(t, len(mm) > 0, "No results found")
 		}
 
 		{
-			err = msgRpo.DeleteMessageByID(msg.ID)
-			assert(t, err == nil, "DeleteMessageByID error: %v", err)
+			err = msgRpo.DeleteByID(msg.ID)
+			test.Assert(t, err == nil, "DeleteMessageByID error: %+v", err)
 		}
 
 		return nil
@@ -80,34 +82,34 @@ func TestReplies(t *testing.T) {
 
 	tx(t, func() error {
 		ch := &types.Channel{}
-		ch, err = chRpo.CreateChannel(ch)
+		ch, err = chRpo.Create(ch)
 		ch.Type = types.ChannelTypePublic
 
 		msg := &types.Message{ChannelID: ch.ID}
 		rpl := &types.Message{ChannelID: ch.ID}
 
-		msg, err = msgRpo.CreateMessage(msg)
-		assert(t, err == nil, "CreateMessage error: %v", err)
-		assert(t, msg.ID > 0, "Message did not get its ID")
+		msg, err = msgRpo.Create(msg)
+		test.Assert(t, err == nil, "CreateMessage error: %+v", err)
+		test.Assert(t, msg.ID > 0, "Message did not get its ID")
 
 		rpl.ReplyTo = msg.ID
-		rpl, err = msgRpo.CreateMessage(rpl)
-		assert(t, err == nil, "CreateMessage error: %v", err)
-		assert(t, rpl.ID > 0, "Reply did not get its ID")
+		rpl, err = msgRpo.Create(rpl)
+		test.Assert(t, err == nil, "CreateMessage error: %+v", err)
+		test.Assert(t, rpl.ID > 0, "Reply did not get its ID")
 
 		// Let's increase this so that FindThreads
 		// can include it into results
 		msgRpo.IncReplyCount(msg.ID)
 
 		{
-			mm, err = msgRpo.FindMessages(&types.MessageFilter{
+			mm, err = msgRpo.Find(&types.MessageFilter{
 				RepliesTo: msg.ID,
 				ChannelID: ch.ID,
 			})
 
-			assert(t, err == nil, "FindMessages error: %v", err)
-			assert(t, len(mm) == 1, "Failed to fetch only reply, got: %d", len(mm))
-			assert(t, mm[0].ID == rpl.ID, "Reply ID does not match")
+			test.Assert(t, err == nil, "FindMessages error: %+v", err)
+			test.Assert(t, len(mm) == 1, "Failed to fetch only reply, got: %d", len(mm))
+			test.Assert(t, mm[0].ID == rpl.ID, "Reply ID does not match")
 		}
 
 		{
@@ -115,37 +117,37 @@ func TestReplies(t *testing.T) {
 				ChannelID: ch.ID,
 			})
 
-			assert(t, err == nil, "FindThreads error: %v", err)
-			assert(t, len(mm) == 2, "Failed to fetch messages in threads (2 messages), got: %d", len(mm))
-			assert(t, mm[0].ID == msg.ID, "Original message ID does not match")
-			assert(t, mm[1].ID == rpl.ID, "Reply ID does not match")
+			test.Assert(t, err == nil, "FindThreads error: %+v", err)
+			test.Assert(t, len(mm) == 2, "Failed to fetch messages in threads (2 messages), got: %d", len(mm))
+			test.Assert(t, mm[0].ID == msg.ID, "Original message ID does not match")
+			test.Assert(t, mm[1].ID == rpl.ID, "Reply ID does not match")
 		}
 
 		{
-			mm, err = msgRpo.FindMessages(&types.MessageFilter{
+			mm, err = msgRpo.Find(&types.MessageFilter{
 				ChannelID: ch.ID,
 			})
 
-			assert(t, err == nil, "FindMessages error: %v", err)
-			assert(t, len(mm) == 1, "Failed to fetch only original message")
-			assert(t, mm[0].ID == msg.ID, "Reply ID does not match")
+			test.Assert(t, err == nil, "FindMessages error: %+v", err)
+			test.Assert(t, len(mm) == 1, "Failed to fetch only original message")
+			test.Assert(t, mm[0].ID == msg.ID, "Reply ID does not match")
 		}
 
 		{
 
-			assert(t, msgRpo.IncReplyCount(msg.ID) == nil, "IncReplyCount should not return an error")
-			assert(t, msgRpo.IncReplyCount(msg.ID) == nil, "IncReplyCount should not return an error")
+			test.Assert(t, msgRpo.IncReplyCount(msg.ID) == nil, "IncReplyCount should not return an error")
+			test.Assert(t, msgRpo.IncReplyCount(msg.ID) == nil, "IncReplyCount should not return an error")
 			// +1 that we have from before
 
-			msg, err = msgRpo.FindMessageByID(msg.ID)
-			assert(t, err == nil, "FindMessageByID error: %v", err)
-			assert(t, msg.Replies == 3, "Reply counter check failed, expecting 3, got %v", msg.Replies)
+			msg, err = msgRpo.FindByID(msg.ID)
+			test.Assert(t, err == nil, "FindMessageByID error: %+v", err)
+			test.Assert(t, msg.Replies == 3, "Reply counter check failed, expecting 3, got %d", msg.Replies)
 
-			assert(t, msgRpo.DecReplyCount(msg.ID) == nil, "DecReplyCount should not return an error")
+			test.Assert(t, msgRpo.DecReplyCount(msg.ID) == nil, "DecReplyCount should not return an error")
 
-			msg, err = msgRpo.FindMessageByID(msg.ID)
-			assert(t, err == nil, "FindMessageByID error: %v", err)
-			assert(t, msg.Replies == 2, "Reply counter check failed, expecting 1, got %v", msg.Replies)
+			msg, err = msgRpo.FindByID(msg.ID)
+			test.Assert(t, err == nil, "FindMessageByID error: %+v", err)
+			test.Assert(t, msg.Replies == 2, "Reply counter check failed, expecting 1, got %d", msg.Replies)
 		}
 
 		return nil
