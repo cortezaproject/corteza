787de29e665fefba82ef36d1670dcb0ff889078b
Tit Petric <black@scene-si.org>
2019-04-03 13:49:57 +0200

    add(internal): store file upload utilities

---
+++ internal/store/util.go
@@ -0,0 +1,54 @@
+package store
+
+import (
+	"io"
+
+	"mime/multipart"
+	"net/url"
+
+	"github.com/pkg/errors"
+
+	"github.com/crusttech/crust/internal/config"
+	"github.com/crusttech/crust/internal/http"
+)
+
+func FromURL(fileURL string) (io.ReadCloser, error) {
+	if u, err := url.ParseRequestURI(fileURL); err != nil {
+		return nil, errors.WithStack(err)
+	} else if u.Scheme != "https" {
+		return nil, errors.New("Only HTTPS is supported for file uploads")
+	}
+
+	client, err := http.New(&config.HTTPClient{
+		Timeout: 10,
+	})
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	req, err := client.Get(fileURL)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	return resp.Body, nil
+}
+
+func FromMultipartFile(file *multipart.FileHeader) (io.ReadCloser, error) {
+	reader, err := file.Open()
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	return reader, nil
+}
+
+func FromAny(file *multipart.FileHeader, url string) (io.ReadCloser, error) {
+	if file != nil {
+		return FromMultipartFile(file)
+	}
+	return FromURL(url)
+}
