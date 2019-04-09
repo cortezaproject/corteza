3f83a3362273f5e1250b37725de4be686c480faa
Denis Arh <denis.arh@kendu.si>
2019-04-09 22:35:39 +0200

    Change password reset flow, add token exchange step

--- system/rest/request/auth_internal.go
+++ system/rest/request/auth_internal.go
@@ -193,6 +193,52 @@ func (auReq *AuthInternalRequestPasswordReset) Fill(r *http.Request) (err error)
 
 var _ RequestFiller = NewAuthInternalRequestPasswordReset()
 
+// AuthInternal exchangePasswordResetToken request parameters
+type AuthInternalExchangePasswordResetToken struct {
+	Token string
+}
+
+func NewAuthInternalExchangePasswordResetToken() *AuthInternalExchangePasswordResetToken {
+	return &AuthInternalExchangePasswordResetToken{}
+}
+
+func (auReq *AuthInternalExchangePasswordResetToken) Fill(r *http.Request) (err error) {
+	if strings.ToLower(r.Header.Get("content-type")) == "application/json" {
+		err = json.NewDecoder(r.Body).Decode(auReq)
+
+		switch {
+		case err == io.EOF:
+			err = nil
+		case err != nil:
+			return errors.Wrap(err, "error parsing http request body")
+		}
+	}
+
+	if err = r.ParseForm(); err != nil {
+		return err
+	}
+
+	get := map[string]string{}
+	post := map[string]string{}
+	urlQuery := r.URL.Query()
+	for name, param := range urlQuery {
+		get[name] = string(param[0])
+	}
+	postVars := r.Form
+	for name, param := range postVars {
+		post[name] = string(param[0])
+	}
+
+	if val, ok := post["token"]; ok {
+
+		auReq.Token = val
+	}
+
+	return err
+}
+
+var _ RequestFiller = NewAuthInternalExchangePasswordResetToken()
+
 // AuthInternal resetPassword request parameters
 type AuthInternalResetPassword struct {
 	Token    string
