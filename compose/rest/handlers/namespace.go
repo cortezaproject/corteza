e3545f123e405d1bc14379bd2d3f72ff4561a27a
Denis Arh <denis.arh@kendu.si>
2019-04-28 08:56:44 +0200

    Implement namespace CRUD + various small fixes
    Other fixes and improvements:
     - add parseISODateWithErr and parseISODatePtrWithErr handlers for incoming data
     - add service & repository errors
     - cleanup old (unbound) attachment controllers from router
     - fix system repository error prefix (auth => system)

---
+++ compose/rest/handlers/namespace.go
@@ -0,0 +1,161 @@
+package handlers
+
+/*
+	Hello! This file is auto-generated from `docs/src/spec.json`.
+
+	For development:
+	In order to update the generated files, edit this file under the location,
+	add your struct fields, imports, API definitions and whatever you want, and:
+
+	1. run [spec](https://github.com/titpetric/spec) in the same folder,
+	2. run `./_gen.php` in this folder.
+
+	You may edit `namespace.go`, `namespace.util.go` or `namespace_test.go` to
+	implement your API calls, helper functions and tests. The file `namespace.go`
+	is only generated the first time, and will not be overwritten if it exists.
+*/
+
+import (
+	"context"
+
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/titpetric/factory/resputil"
+
+	"github.com/crusttech/crust/compose/rest/request"
+)
+
+// Internal API interface
+type NamespaceAPI interface {
+	List(context.Context, *request.NamespaceList) (interface{}, error)
+	Create(context.Context, *request.NamespaceCreate) (interface{}, error)
+	Read(context.Context, *request.NamespaceRead) (interface{}, error)
+	Update(context.Context, *request.NamespaceUpdate) (interface{}, error)
+	Delete(context.Context, *request.NamespaceDelete) (interface{}, error)
+}
+
+// HTTP API interface
+type Namespace struct {
+	List   func(http.ResponseWriter, *http.Request)
+	Create func(http.ResponseWriter, *http.Request)
+	Read   func(http.ResponseWriter, *http.Request)
+	Update func(http.ResponseWriter, *http.Request)
+	Delete func(http.ResponseWriter, *http.Request)
+}
+
+func NewNamespace(nh NamespaceAPI) *Namespace {
+	return &Namespace{
+		List: func(w http.ResponseWriter, r *http.Request) {
+			defer r.Body.Close()
+			params := request.NewNamespaceList()
+			if err := params.Fill(r); err != nil {
+				resputil.JSON(w, err)
+				return
+			}
+			if value, err := nh.List(r.Context(), params); err != nil {
+				resputil.JSON(w, err)
+				return
+			} else {
+				switch fn := value.(type) {
+				case func(http.ResponseWriter, *http.Request):
+					fn(w, r)
+					return
+				}
+				resputil.JSON(w, value)
+				return
+			}
+		},
+		Create: func(w http.ResponseWriter, r *http.Request) {
+			defer r.Body.Close()
+			params := request.NewNamespaceCreate()
+			if err := params.Fill(r); err != nil {
+				resputil.JSON(w, err)
+				return
+			}
+			if value, err := nh.Create(r.Context(), params); err != nil {
+				resputil.JSON(w, err)
+				return
+			} else {
+				switch fn := value.(type) {
+				case func(http.ResponseWriter, *http.Request):
+					fn(w, r)
+					return
+				}
+				resputil.JSON(w, value)
+				return
+			}
+		},
+		Read: func(w http.ResponseWriter, r *http.Request) {
+			defer r.Body.Close()
+			params := request.NewNamespaceRead()
+			if err := params.Fill(r); err != nil {
+				resputil.JSON(w, err)
+				return
+			}
+			if value, err := nh.Read(r.Context(), params); err != nil {
+				resputil.JSON(w, err)
+				return
+			} else {
+				switch fn := value.(type) {
+				case func(http.ResponseWriter, *http.Request):
+					fn(w, r)
+					return
+				}
+				resputil.JSON(w, value)
+				return
+			}
+		},
+		Update: func(w http.ResponseWriter, r *http.Request) {
+			defer r.Body.Close()
+			params := request.NewNamespaceUpdate()
+			if err := params.Fill(r); err != nil {
+				resputil.JSON(w, err)
+				return
+			}
+			if value, err := nh.Update(r.Context(), params); err != nil {
+				resputil.JSON(w, err)
+				return
+			} else {
+				switch fn := value.(type) {
+				case func(http.ResponseWriter, *http.Request):
+					fn(w, r)
+					return
+				}
+				resputil.JSON(w, value)
+				return
+			}
+		},
+		Delete: func(w http.ResponseWriter, r *http.Request) {
+			defer r.Body.Close()
+			params := request.NewNamespaceDelete()
+			if err := params.Fill(r); err != nil {
+				resputil.JSON(w, err)
+				return
+			}
+			if value, err := nh.Delete(r.Context(), params); err != nil {
+				resputil.JSON(w, err)
+				return
+			} else {
+				switch fn := value.(type) {
+				case func(http.ResponseWriter, *http.Request):
+					fn(w, r)
+					return
+				}
+				resputil.JSON(w, value)
+				return
+			}
+		},
+	}
+}
+
+func (nh *Namespace) MountRoutes(r chi.Router, middlewares ...func(http.Handler) http.Handler) {
+	r.Group(func(r chi.Router) {
+		r.Use(middlewares...)
+		r.Get("/namespace/", nh.List)
+		r.Post("/namespace/", nh.Create)
+		r.Get("/namespace/{namespaceID}", nh.Read)
+		r.Post("/namespace/{namespaceID}", nh.Update)
+		r.Delete("/namespace/{namespaceID}", nh.Delete)
+	})
+}
