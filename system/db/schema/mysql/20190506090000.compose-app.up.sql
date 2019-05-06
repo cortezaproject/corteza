6f253ce67b9d7e842303ef3eca1c01abeb9f31e9
Denis Arh <denis.arh@kendu.si>
2019-05-06 09:02:26 +0200

    CRM=>Compose application migration

---
+++ system/db/schema/mysql/20190506090000.compose-app.up.sql
@@ -0,0 +1,4 @@
+UPDATE `sys_application`
+   SET `name`  = 'Crust Compose',
+       `unify` = '{"logo": "/applications/crust.jpg", "icon": "/applications/crust_favicon.png", "url": "/compose/", "listed": true}'
+ WHERE id = 2;
