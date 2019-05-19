e00a7e064e4fcc594474b79f9799b682c606edc9
Tit Petric <black@scene-si.org>
2019-05-19 14:41:15 +0200

    upd(internal/http): perform internal test for http client
    Signed-off-by: Tit Petric <black@scene-si.org>

--- internal/http/client_test.go
+++ internal/http/client_test.go
@@ -1,22 +1,26 @@
-// +build external
-
 package http
 
 import (
 	"testing"
+
+	"net/http/httptest"
 
 	"github.com/crusttech/crust/internal/config"
 	"github.com/crusttech/crust/internal/test"
 )
 
 func TestHTTPClient(t *testing.T) {
+	handler := &Fortune{}
+	server := httptest.NewServer(handler)
+	defer server.Close()
+
 	client, err := New(&config.HTTPClient{
 		Timeout: 10,
 	})
 	test.Assert(t, err == nil, "%+v", err)
 	client.Debug(FULL)
 
-	req, err := client.Get("https://api.scene-si.org/fortune.php")
+	req, err := client.Get(server.URL)
 	test.Assert(t, err == nil, "%+v", err)
 
 	resp, err := client.Do(req)
