3f2eeaf8b35cae4b0d87a3e0e2f7ba0c62e85601
Tit Petric <black@scene-si.org>
2019-05-19 15:58:30 +0200

    add(all): test files for type-set
    Signed-off-by: Tit Petric <black@scene-si.org>

---
+++ messaging/types/command_param.gen_test.go
@@ -0,0 +1,61 @@
+package types
+
+import (
+	"testing"
+
+	"errors"
+
+	"github.com/crusttech/crust/internal/test"
+)
+
+// 	Hello! This file is auto-generated.
+
+func TestCommandParamSetWalk(t *testing.T) {
+	value := make(CommandParamSet, 3)
+
+	// check walk with no errors
+	{
+		err := value.Walk(func(*CommandParam) error {
+			return nil
+		})
+		test.NoError(t, err, "Expected no returned error from Walk, got %+v", err)
+	}
+
+	// check walk with error
+	test.Error(t, value.Walk(func(*CommandParam) error { return errors.New("Walk error") }), "Expected error from walk, got nil")
+}
+
+func TestCommandParamSetFilter(t *testing.T) {
+	value := make(CommandParamSet, 3)
+
+	// filter nothing
+	{
+		set, err := value.Filter(func(*CommandParam) (bool, error) {
+			return true, nil
+		})
+		test.NoError(t, err, "Didn't expect error when filtering set: %+v", err)
+		test.Assert(t, len(set) == len(value), "Expected equal length filter: %d != %d", len(value), len(set))
+	}
+
+	// filter one item
+	{
+		found := false
+		set, err := value.Filter(func(*CommandParam) (bool, error) {
+			if !found {
+				found = true
+				return found, nil
+			}
+			return false, nil
+		})
+		test.NoError(t, err, "Didn't expect error when filtering set: %+v", err)
+		test.Assert(t, len(set) == 1, "Expected single item, got %d", len(value))
+	}
+
+	// filter error
+	{
+		_, err := value.Filter(func(*CommandParam) (bool, error) {
+			return false, errors.New("Filter error")
+		})
+		test.Error(t, err, "Expected error, got %#v", err)
+	}
+}
