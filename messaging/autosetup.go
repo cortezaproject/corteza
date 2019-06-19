e988d8687ceb4f96515448b5848886e9acfe35b9
Denis Arh <denis.arh@kendu.si>
2019-06-19 16:17:57 +0200

    Extract autosetup

---
+++ messaging/autosetup.go
@@ -0,0 +1,17 @@
+package messaging
+
+import (
+	"context"
+
+	"github.com/spf13/cobra"
+
+	"github.com/cortezaproject/corteza-server/messaging/internal/service"
+	"github.com/cortezaproject/corteza-server/pkg/cli"
+)
+
+func accessControlSetup(ctx context.Context, cmd *cobra.Command, c *cli.Config) error {
+	// Calling grant directly on internal permissions service to avoid AC check for "grant"
+	var p = service.DefaultPermissions
+	var ac = service.DefaultAccessControl
+	return p.Grant(ctx, ac.Whitelist(), ac.DefaultRules()...)
+}
