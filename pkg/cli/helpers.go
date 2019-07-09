acffdc1c65ce7d588bd54054e3fec779c746112a
Denis Arh <denis.arh@kendu.si>
2019-07-09 17:47:58 +0200

    Simplify/improve logging configuration

--- pkg/cli/helpers.go
+++ pkg/cli/helpers.go
@@ -6,21 +6,14 @@ import (
 	"time"
 
 	"github.com/getsentry/sentry-go"
-	"go.uber.org/zap"
 
 	"github.com/cortezaproject/corteza-server/internal/auth"
 	"github.com/cortezaproject/corteza-server/internal/http"
 	"github.com/cortezaproject/corteza-server/internal/mail"
 	"github.com/cortezaproject/corteza-server/pkg/cli/options"
-	"github.com/cortezaproject/corteza-server/pkg/logger"
 )
 
-func InitGeneralServices(logOpt *options.LogOpt, smtpOpt *options.SMTPOpt, jwtOpt *options.JWTOpt, httpClientOpt *options.HttpClientOpt) {
-	// Reset logger's level to whatever we want
-	var logLevel = zap.InfoLevel
-	_ = logLevel.Set(logOpt.Level)
-	logger.DefaultLevel.SetLevel(logLevel)
-
+func InitGeneralServices(smtpOpt *options.SMTPOpt, jwtOpt *options.JWTOpt, httpClientOpt *options.HttpClientOpt) {
 	auth.SetupDefault(jwtOpt.Secret, int(jwtOpt.Expiry/time.Minute))
 	mail.SetupDialer(smtpOpt.Host, smtpOpt.Port, smtpOpt.User, smtpOpt.Pass, smtpOpt.From)
 	http.SetupDefaults(
