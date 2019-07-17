87388af41e16556a7a2fce5719d285d9114815cc
Denis Arh <denis.arh@gmail.com>
2019-07-17 10:20:41 +0200

    Improve DB conn. code, make retry/timeout configurable

--- pkg/cli/options/db.go
+++ pkg/cli/options/db.go
@@ -1,9 +1,16 @@
 package options
 
+import (
+	"time"
+)
+
 type (
 	DBOpt struct {
-		DSN      string `env:"DB_DSN"`
-		Profiler string `env:"DB_PROFILER"`
+		DSN      string        `env:"DB_DSN"`
+		Profiler string        `env:"DB_PROFILER"`
+		MaxTries int           `env:"DB_MAX_TRIES"`
+		Delay    time.Duration `env:"DB_CONN_ERR_DELAY"`
+		Timeout  time.Duration `env:"DB_CONN_TIMEOUT"`
 	}
 )
 
@@ -11,6 +18,9 @@ func DB(pfix string) (o *DBOpt) {
 	o = &DBOpt{
 		DSN:      "corteza:corteza@tcp(db:3306)/corteza?collation=utf8mb4_general_ci",
 		Profiler: "none",
+		MaxTries: 100,
+		Delay:    5 * time.Second,
+		Timeout:  1 * time.Minute,
 	}
 
 	fill(o, pfix)
