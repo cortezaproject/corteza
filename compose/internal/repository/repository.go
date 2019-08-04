3e68bfa7e0ea3e5173a148bf7cdf6e7a06864b21
Denis Arh <denis.arh@gmail.com>
2019-08-04 11:17:46 +0200

    Extract repository helpers to pkg/rh package

--- compose/internal/repository/repository.go
+++ compose/internal/repository/repository.go
@@ -3,12 +3,11 @@ package repository
 import (
 	"context"
 
-	"github.com/lann/builder"
-	"github.com/pkg/errors"
 	"github.com/titpetric/factory"
 	"gopkg.in/Masterminds/squirrel.v1"
 
 	"github.com/cortezaproject/corteza-server/internal/auth"
+	"github.com/cortezaproject/corteza-server/pkg/rh"
 )
 
 type (
@@ -51,82 +50,23 @@ func (r *repository) db() *factory.DB {
 
 // Fetches single row from table
 func (r repository) fetchOne(one interface{}, q squirrel.SelectBuilder) (err error) {
-	var (
-		sql  string
-		args []interface{}
-	)
-
-	if sql, args, err = q.ToSql(); err != nil {
-		return
-	}
-
-	if err = r.db().Get(one, sql, args...); err != nil {
-		return
-	}
-
-	return
+	return rh.FetchOne(r.db(), q, one)
 }
 
 // Counts all rows that match conditions from given query builder
 func (r repository) count(q squirrel.SelectBuilder) (uint, error) {
-	var (
-		count uint
-		cq    = q.Column("COUNT(*)")
-	)
-
-	// Remove order-bys for counting
-	cq = builder.Delete(cq, "OrderBys").(squirrel.SelectBuilder)
-
-	if sqlSelect, argsSelect, err := cq.ToSql(); err != nil {
-		return 0, err
-	} else {
-		if err := r.db().Get(&count, sqlSelect, argsSelect...); err != nil {
-			return 0, err
-		}
-	}
-
-	return count, nil
+	return rh.Count(r.db(), q)
 }
 
 // Fetches paged rows
 func (r repository) fetchPaged(set interface{}, q squirrel.SelectBuilder, page, perPage uint) error {
-	if perPage > 0 {
-		q = q.Limit(uint64(perPage))
-	}
-
-	if page > 0 {
-		q = q.Offset(uint64(page * perPage))
-	}
-
-	if sqlSelect, argsSelect, err := q.ToSql(); err != nil {
-		return err
-	} else {
-		return r.db().Select(set, sqlSelect, argsSelect...)
-	}
+	return rh.FetchPaged(r.db(), q, page, perPage, set)
 }
 
 func normalizePerPage(val, min, max, def uint) uint {
-	if val == 0 {
-		return def
-	}
-
-	if max > 0 && val > max {
-		return max
-	}
-
-	if min > 0 && val < min {
-		return min
-	}
-
-	return val
+	return rh.NormalizePerPage(val, min, max, def)
 }
 
 func isFound(err error, valid bool, nerr error) error {
-	if err != nil {
-		return errors.WithStack(err)
-	} else if !valid {
-		return errors.WithStack(nerr)
-	}
-
-	return nil
+	return rh.IsFound(err, valid, nerr)
 }
---
+++ pkg/rh/README.md
@@ -0,0 +1,4 @@
+# (rh) Repository Helpers
+
+Package name is short an abbreviated to avoid 
+colliding with per-service repository package names.
