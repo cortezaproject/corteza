e29ef58395efa601b89df77176f20d49a6ee3903
Denis Arh <denis.arh@gmail.com>
2019-09-10 02:55:09 +0200

    Add permission intg. tests

--- tests/messaging/permissions_delete_test.go
+++ tests/messaging/permissions_delete_test.go
@@ -1,9 +1,36 @@
 package messaging
 
 import (
+	"fmt"
+	"net/http"
 	"testing"
+
+	"github.com/cortezaproject/corteza-server/internal/permissions"
+	"github.com/cortezaproject/corteza-server/tests/helpers"
 )
 
 func TestPermissionsDelete(t *testing.T) {
-	t.Skip("to be implemented")
+	h := newHelper(t)
+
+	h.a.Empty(p.FindRulesByRoleID(h.roleID))
+
+	h.allow("messaging", "access")
+	h.allow("messaging", "grant")
+	h.deny("messaging", "channel.group.create")
+
+	h.a.Len(p.FindRulesByRoleID(h.roleID), 3)
+
+	h.apiInit().
+		Delete(fmt.Sprintf("/permissions/%d/rules", h.roleID)).
+		Expect(t).
+		Status(http.StatusOK).
+		Assert(helpers.AssertNoErrors).
+		End()
+
+	// Make sure everything is deleted
+	rr, _ := p.FindRulesByRoleID(h.roleID).Filter(func(r *permissions.Rule) (b bool, e error) {
+		return r.Access != permissions.Inherit, nil
+	})
+
+	h.a.Empty(rr)
 }
