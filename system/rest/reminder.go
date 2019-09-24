43d122a41571c22460920dca5c70e896cc126cef
Tomaž Jerman <tomaz.jerman@kendu.si>
2019-09-24 12:11:12 +0200

    Add reminder service

--- system/rest/reminder.go
+++ system/rest/reminder.go
@@ -2,6 +2,13 @@ package rest
 
 import (
 	"context"
+	"time"
+
+	"github.com/cortezaproject/corteza-server/internal/auth"
+	"github.com/titpetric/factory/resputil"
+
+	"github.com/cortezaproject/corteza-server/system/service"
+	"github.com/cortezaproject/corteza-server/system/types"
 
 	"github.com/pkg/errors"
 
@@ -10,38 +17,84 @@ import (
 
 var _ = errors.Wrap
 
-type Reminder struct {
-	// xxx service.XXXService
-}
+type (
+	Reminder struct {
+		reminder service.ReminderService
+	}
+
+	reminderSetPayload struct {
+		Filter types.ReminderFilter `json:"filter"`
+		Set    types.ReminderSet    `json:"set"`
+	}
+)
 
 func (Reminder) New() *Reminder {
-	return &Reminder{}
+	ctrl := &Reminder{}
+	ctrl.reminder = service.DefaultReminder
+	return ctrl
 }
 
 func (ctrl *Reminder) List(ctx context.Context, r *request.ReminderList) (interface{}, error) {
-	return nil, errors.New("Not implemented: Reminder.list")
+	f := types.ReminderFilter{
+		AssignedTo: r.AssignedTo,
+		Resource:   r.Resource,
+		Page:       r.Page,
+		PerPage:    r.PerPage,
+	}
+
+	set, filter, err := ctrl.reminder.Find(ctx, f)
+	return ctrl.makeFilterPayload(ctx, set, filter, err)
 }
 
 func (ctrl *Reminder) Create(ctx context.Context, r *request.ReminderCreate) (interface{}, error) {
-	return nil, errors.New("Not implemented: Reminder.create")
+	ntf := &types.Reminder{
+		AssignedAt: time.Now(),
+		AssignedBy: auth.GetIdentityFromContext(ctx).Identity(),
+
+		AssignedTo: r.AssignedTo,
+		Payload:    r.Payload,
+		Resource:   r.Resource,
+		RemindAt:   r.RemindAt,
+	}
+
+	return ctrl.reminder.Create(ctx, ntf)
 }
 
 func (ctrl *Reminder) Update(ctx context.Context, r *request.ReminderUpdate) (interface{}, error) {
-	return nil, errors.New("Not implemented: Reminder.update")
+	ntf := &types.Reminder{
+		ID:         r.ReminderID,
+		AssignedAt: time.Now(),
+		AssignedBy: auth.GetIdentityFromContext(ctx).Identity(),
+
+		AssignedTo: r.AssignedTo,
+		Payload:    r.Payload,
+		Resource:   r.Resource,
+		RemindAt:   r.RemindAt,
+	}
+
+	return ctrl.reminder.Update(ctx, ntf)
 }
 
 func (ctrl *Reminder) Read(ctx context.Context, r *request.ReminderRead) (interface{}, error) {
-	return nil, errors.New("Not implemented: Reminder.read")
+	return ctrl.reminder.FindByID(ctx, r.ReminderID)
 }
 
 func (ctrl *Reminder) Delete(ctx context.Context, r *request.ReminderDelete) (interface{}, error) {
-	return nil, errors.New("Not implemented: Reminder.delete")
+	return resputil.OK(), ctrl.reminder.Delete(ctx, r.ReminderID)
 }
 
 func (ctrl *Reminder) Dismiss(ctx context.Context, r *request.ReminderDismiss) (interface{}, error) {
-	return nil, errors.New("Not implemented: Reminder.dismiss")
+	return resputil.OK(), ctrl.reminder.Dismiss(ctx, r.ReminderID)
 }
 
 func (ctrl *Reminder) Snooze(ctx context.Context, r *request.ReminderSnooze) (interface{}, error) {
-	return nil, errors.New("Not implemented: Reminder.snooze")
+	return resputil.OK(), ctrl.reminder.Snooze(ctx, r.ReminderID, r.RemindAt)
+}
+
+func (ctrl *Reminder) makeFilterPayload(ctx context.Context, nn types.ReminderSet, f types.ReminderFilter, err error) (*reminderSetPayload, error) {
+	if err != nil {
+		return nil, err
+	}
+
+	return &reminderSetPayload{Filter: f, Set: nn}, nil
 }
