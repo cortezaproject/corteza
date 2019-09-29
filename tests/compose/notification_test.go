12227365cc643fc51a46988f79b9f8da21a1ca2d
Denis Arh <denis.arh@gmail.com>
2019-09-29 13:45:49 +0200

    Add notification test stub

---
+++ tests/compose/notification_test.go
@@ -0,0 +1,35 @@
+package compose
+
+import (
+	"encoding/json"
+	"net/http"
+	"testing"
+
+	"github.com/cortezaproject/corteza-server/compose/rest/request"
+	sqlxTypes "github.com/jmoiron/sqlx/types"
+	"github.com/steinfletcher/apitest"
+)
+
+func (h helper) apiSendEmailNotification(req request.NotificationEmailSend) *apitest.Response {
+	payload, err := json.Marshal(req)
+	h.a.NoError(err)
+
+	return h.apiInit().
+		Post("/notification/email").
+		JSON(string(payload)).
+		Expect(h.t).
+		Status(http.StatusOK)
+}
+
+func TestEmailNotification(t *testing.T) {
+	t.Skip("we need smtp server mock")
+	h := newHelper(t)
+
+	h.apiSendEmailNotification(request.NotificationEmailSend{
+		To:                []string{"foo+to@test.tld"},
+		Cc:                []string{"foo+cc1@test.tld", "foo+cc2@test.tld"},
+		Subject:           "Subject!",
+		Content:           sqlxTypes.JSONText(`{}`),
+		RemoteAttachments: []string{"file1", "file2"},
+	}).End()
+}
