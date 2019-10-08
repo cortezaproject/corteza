ed45f8e34850f5ffe3395ed7370c735cac90b3b5
Tomaž Jerman <tomaz.jerman@kendu.si>
2019-10-08 16:09:44 +0200

    Add provisions for messaging & compose settings

--- compose/service/access_control.go
+++ compose/service/access_control.go
@@ -37,6 +37,8 @@ func (svc accessControl) Effective(ctx context.Context) (ee permissions.Effectiv
 	ee.Push(types.ComposePermissionResource, "access", svc.CanAccess(ctx))
 	ee.Push(types.ComposePermissionResource, "grant", svc.CanGrant(ctx))
 	ee.Push(types.ComposePermissionResource, "namespace.create", svc.CanCreateNamespace(ctx))
+	ee.Push(types.ComposePermissionResource, "settings.read", svc.CanReadSettings(ctx))
+	ee.Push(types.ComposePermissionResource, "settings.manage", svc.CanManageSettings(ctx))
 
 	return
 }
@@ -206,6 +208,8 @@ func (svc accessControl) Whitelist() permissions.Whitelist {
 		"access",
 		"grant",
 		"namespace.create",
+		"settings.read",
+		"settings.manage",
 	)
 
 	wl.Set(
