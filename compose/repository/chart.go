8eaf02f2c8e554435d751405369f3ee9be600d63
Denis Arh <denis.arh@gmail.com>
2019-10-17 13:12:33 +0200

    Refactor chart repo

--- compose/repository/chart.go
+++ compose/repository/chart.go
@@ -9,6 +9,7 @@ import (
 	"gopkg.in/Masterminds/squirrel.v1"
 
 	"github.com/cortezaproject/corteza-server/compose/types"
+	"github.com/cortezaproject/corteza-server/pkg/rh"
 )
 
 type (
@@ -49,14 +50,20 @@ func (r chart) table() string {
 
 func (r chart) columns() []string {
 	return []string{
-		"id", "rel_namespace", "handle", "name", "config",
-		"created_at", "updated_at", "deleted_at",
+		"id",
+		"rel_namespace",
+		"handle",
+		"name",
+		"config",
+		"created_at",
+		"updated_at",
+		"deleted_at",
 	}
 }
 
 func (r chart) query() squirrel.SelectBuilder {
 	return squirrel.
-		Select().
+		Select(r.columns()...).
 		From(r.table()).
 		Where("deleted_at IS NULL")
 }
@@ -70,13 +77,13 @@ func (r chart) FindByHandle(namespaceID uint64, handle string) (*types.Chart, er
 }
 
 func (r chart) findOneBy(namespaceID uint64, field string, value interface{}) (*types.Chart, error) {
-	var c = &types.Chart{}
+	var (
+		c = &types.Chart{}
 
-	err := r.findOneInNamespaceBy(
-		namespaceID,
-		r.query().Columns(r.columns()...),
-		squirrel.Eq{field: value},
-		c,
+		q = r.query().
+			Where(squirrel.Eq{field: value, "rel_namespace": namespaceID})
+
+		err = rh.FetchOne(r.db(), q, c)
 	)
 
 	if err == nil && c.ID == 0 {
@@ -89,34 +96,43 @@ func (r chart) findOneBy(namespaceID uint64, field string, value interface{}) (*
 func (r chart) Find(filter types.ChartFilter) (set types.ChartSet, f types.ChartFilter, err error) {
 	f = filter
 
+	if f.Sort == "" {
+		f.Sort = "id ASC"
+	}
+
 	query := r.query()
 
 	if filter.NamespaceID > 0 {
-		query = query.Where("rel_namespace = ?", filter.NamespaceID)
+		query = query.Where(squirrel.Eq{"rel_namespace": filter.NamespaceID})
 	}
 
 	if f.Query != "" {
-		q := "%" + f.Query + "%"
-		query = query.Where("name like ?", q)
+		q := "%" + strings.ToLower(f.Query) + "%"
+		query = query.Where(squirrel.Or{
+			squirrel.Like{"LOWER(name)": q},
+		})
 	}
 
 	if f.Handle != "" {
-		query = query.Where("LOWER(handle) = ?", strings.ToLower(f.Handle))
+		query = query.Where("LOWER(handle) = LOWER(?)", f.Handle)
 	}
 
 	if f.IsReadable != nil {
 		query = query.Where(f.IsReadable)
 	}
 
-	if f.Count, err = r.count(query); err != nil || f.Count == 0 {
+	var orderBy []string
+	if orderBy, err = rh.ParseOrder(f.Sort, r.columns()...); err != nil {
+		return
+	} else {
+		query = query.OrderBy(orderBy...)
+	}
+
+	if f.Count, err = rh.Count(r.db(), query); err != nil || f.Count == 0 {
 		return
 	}
 
-	query = query.
-		Columns(r.columns()...).
-		OrderBy("id ASC")
-
-	return set, f, r.fetchPaged(&set, query, f.Page, f.PerPage)
+	return set, f, rh.FetchPaged(r.db(), query, f.Page, f.PerPage, &set)
 }
 
 func (r chart) Create(mod *types.Chart) (*types.Chart, error) {
