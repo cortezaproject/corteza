4fc4fc6208b7bd8a50d8b68e51ea94297bd93fc4
Denis Arh <denis.arh@gmail.com>
2019-10-24 10:48:58 +0200

    Add service-cloud configs
    Change file naming for CRM configs so it can go side-to-side with
    service-cloud.

--- provision/update.sh
+++ provision/update.sh
@@ -3,20 +3,34 @@
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 function download {
-  SRC="https://raw.githubusercontent.com/cortezaproject/corteza-configs/master/${1}"
+  SRC="https://raw.githubusercontent.com/cortezaproject/corteza-configs/${1}"
   DST=${2}
   echo -ne "\033[32m${DST}\033[39m (${SRC}) ..."
   curl -s $SRC > ${DST}
   echo "done"
 }
 
+# ------ ------ ------ ------ ------ ------ ------ ------ ------ ------ ------ ------ ------ ------ ------ ------ ------
+
 function getCrmConfig {
   NAMES="1000_namespace 1100_modules 1200_charts 1300_scripts 1400_pages 1500_record_settings"
 
   for NAME in $NAMES; do
-    download "crm/${NAME}.yaml" "./compose/src/${NAME}.yaml"
+    download "master/crm/${NAME}.yaml" "./compose/src/${NAME}_crm.yaml"
   done
 }
 
 getCrmConfig
+
+# ------ ------ ------ ------ ------ ------ ------ ------ ------ ------ ------ ------ ------ ------ ------ ------ ------
+
+function getServiceCloudConfig {
+  NAMES="1000_namespace 1100_modules 1200_charts 1300_scripts 1400_pages 1500_record_settings"
+
+  for NAME in $NAMES; do
+    download "master/service-cloud/${NAME}.yaml" "./compose/src/${NAME}_service_cloud.yaml"
+  done
+}
+
+getServiceCloudConfig
 
