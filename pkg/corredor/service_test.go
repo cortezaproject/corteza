ce1fc3402dc31820e91400965339e15d23f8b068
Denis Arh <denis.arh@gmail.com>
2020-01-01 18:02:57 +0100

    Add more tests to pkg/corredor

--- pkg/corredor/service_test.go
+++ pkg/corredor/service_test.go
@@ -1,10 +1,44 @@
 package corredor
 
 import (
+	"github.com/cortezaproject/corteza-server/pkg/app/options"
+	"go.uber.org/zap"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
+
+type (
+	mockEvent struct {
+		rType string
+		eType string
+		match func(name string, op string, values ...string) bool
+	}
+)
+
+func (e mockEvent) ResourceType() string {
+	return e.rType
+}
+
+func (e mockEvent) EventType() string {
+	return e.eType
+}
+
+func (e mockEvent) Encode() (map[string][]byte, error) {
+	return nil, nil
+}
+
+func (e mockEvent) Decode(map[string][]byte) error {
+	return nil
+}
+
+func (e mockEvent) Match(name string, op string, values ...string) bool {
+	if e.match == nil {
+		return true
+	}
+
+	return e.match(name, op, values...)
+}
 
 func TestFindOnManual(t *testing.T) {
 	var (
@@ -26,6 +60,14 @@ func TestFindOnManual(t *testing.T) {
 						},
 					},
 				},
+				//&Script{
+				//	Triggers: []*Trigger{
+				//		&Trigger{
+				//			Events:    []string{"not-a-match"},
+				//			Resources: []string{"not-a-match"},
+				//		},
+				//	},
+				//},
 			},
 			cScripts: ScriptSet{
 				&Script{
@@ -60,4 +102,44 @@ func TestFindOnManual(t *testing.T) {
 
 	a.NoError(err)
 	a.Len(o, 2)
+}
+
+func TestGlobals(t *testing.T) {
+	var (
+		a = assert.New(t)
+	)
+
+	gCorredor = NewService(zap.NewNop(), options.CorredorOpt{})
+	a.Equal(gCorredor, Service())
+	a.NoError(Setup(zap.NewNop(), options.CorredorOpt{}))
+	a.NotNil(gCorredor)
+
+	gCorredor = nil
+	a.NoError(Setup(zap.NewNop(), options.CorredorOpt{}))
+	a.Equal(gCorredor, Service())
+	a.NotNil(gCorredor)
+}
+
+func TestServiceBasics(t *testing.T) {
+	var (
+		svc = &service{}
+	)
+
+	svc.SetEventRegistry(nil)
+	svc.SetAuthTokenMaker(nil)
+	svc.SetUserFinder(nil)
+}
+
+func TestService_ExecOnManual(t *testing.T) {
+	var (
+		a   = assert.New(t)
+		svc = &service{
+			manual: map[string]map[string]string{},
+		}
+	)
+
+	a.Error(svc.ExecOnManual(nil, "script", &mockEvent{eType: "not-onManual"}))
+	a.Error(svc.ExecOnManual(nil, "script", &mockEvent{eType: onManualEventType}))
+	svc.manual["script"] = nil
+	a.Error(svc.ExecOnManual(nil, "script", &mockEvent{eType: onManualEventType}))
 }
