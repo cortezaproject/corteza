00a195c53ff5dd4fe624132cf23230a0605467bb
Denis Arh <denis.arh@gmail.com>
2020-01-10 10:00:42 +0100

    Implement constraint checking for compose events

--- compose/service/event/compose.go
+++ compose/service/event/compose.go
@@ -17,13 +17,6 @@ func (res composeOnTimestamp) Match(c eventbus.ConstraintMatcher) bool {
 
 // Match returns false if given conditions do not match event & resource internals
 func (res composeBase) Match(c eventbus.ConstraintMatcher) bool {
-	// By default we match no mather what kind of constraints we receive
-	//
-	// Function will be called multiple times - once for every trigger constraint
-	// All should match (return true):
-	//   constraint#1 AND constraint#2 AND constraint#3 ...
-	//
-	// When there are multiple values, Match() can decide how to treat them (OR, AND...)
-
-	return true
+	// We do not support any matchers, so if there is a constraint, fail.
+	return false
 }
---
+++ compose/service/event/namespace_test.go
@@ -0,0 +1,22 @@
+package event
+
+import (
+	"github.com/cortezaproject/corteza-server/compose/types"
+	"github.com/cortezaproject/corteza-server/pkg/eventbus"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNamespaceMatching(t *testing.T) {
+	var (
+		a   = assert.New(t)
+		res = &namespaceBase{
+			namespace: &types.Namespace{Slug: "slg1"},
+		}
+
+		cNms = eventbus.MustMakeConstraint("namespace", "eq", "slg1")
+	)
+
+	a.True(res.Match(cNms))
+}
