b9cd860bf29f0e3008964f27d05f2bd72449ad3d
Denis Arh <denis.arh@gmail.com>
2020-01-22 17:39:14 +0100

    Improve script filtering

--- pkg/corredor/filter.go
+++ pkg/corredor/filter.go
@@ -1,27 +1,118 @@
 package corredor
 
 import (
+	"github.com/cortezaproject/corteza-server/pkg/slice"
 	"strings"
 )
 
 type (
 	Filter struct {
+		ResourceTypePrefixes []string `json:"resourceTypePrefixes"`
 		ResourceTypes        []string `json:"resourceTypes"`
 		EventTypes           []string `json:"eventTypes"`
 		ExcludeServerScripts bool     `json:"excludeServerScripts"`
 		ExcludeClientScripts bool     `json:"excludeClientScripts"`
+		ExcludeInvalid       bool     `json:"excludeInvalid"`
 
-		Page    uint `json:"page"`
-		PerPage uint `json:"perPage"`
-		Count   uint `json:"count"`
+		//Page    uint `json:"page"`
+		//PerPage uint `json:"perPage"`
+		Count uint `json:"count"`
 	}
 )
 
-// PrefixResources adds service string (if not already there) to all resources
-func (f *Filter) PrefixResources(service string) {
-	for i := range f.ResourceTypes {
-		if !strings.HasPrefix(f.ResourceTypes[i], service) {
-			f.ResourceTypes[i] = service + ":" + f.ResourceTypes[i]
+func (f Filter) makeFilterFn() func(s *Script) (b bool, err error) {
+	collectTypes := func(tt ...*Trigger) (rr []string, ee []string) {
+		for _, t := range tt {
+			rr = append(rr, t.ResourceTypes...)
+			ee = append(ee, t.EventTypes...)
+		}
+
+		return
+	}
+
+	return func(s *Script) (b bool, err error) {
+		if f.ExcludeInvalid {
+			if len(s.Errors) > 0 {
+				// Skip scripts with errors
+				return
+			}
+		}
+
+		var (
+			resourceTypes, eventTypes = collectTypes(s.Triggers...)
+		)
+
+		if !f.checkPrefixes(resourceTypes...) {
+			return
+		}
+
+		if !f.checkEventTypes(eventTypes...) {
+			return
+		}
+
+		if !f.checkResourceTypes(resourceTypes...) {
+			return
+		}
+
+		return true, nil
+	}
+}
+
+// parses resource type prefixes, adds ui: and service prefix if not present
+func (f *Filter) procRTPrefixes(service string) {
+	var (
+		hasService, hasUi bool
+	)
+
+	for i, rtp := range f.ResourceTypePrefixes {
+		if rtp == service {
+			hasService = true
+		}
+
+		if strings.HasPrefix(rtp, "ui") {
+			hasUi = true
+		}
+
+		// Check all requested resource-type prefixed and make sure
+		// all start with either "ui" or resourcePrefix + ":"
+		if !strings.HasPrefix(rtp, "ui:") && !strings.HasPrefix(rtp, service) {
+			f.ResourceTypePrefixes[i] = service + ":" + rtp
 		}
 	}
+
+	if !hasService {
+		f.ResourceTypePrefixes = append(f.ResourceTypePrefixes, service)
+	}
+
+	if !hasUi {
+		f.ResourceTypePrefixes = append(f.ResourceTypePrefixes, "ui:")
+	}
+}
+
+func (f Filter) checkPrefixes(rr ...string) bool {
+	if len(f.ResourceTypePrefixes) == 0 {
+		return true
+	}
+
+	if len(rr) == 0 {
+		return false
+	}
+
+	for _, p := range f.ResourceTypePrefixes {
+		for _, r := range rr {
+			if strings.HasPrefix(r, p) {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+func (f Filter) checkResourceTypes(rr ...string) bool {
+	return len(f.ResourceTypes) == 0 || len(slice.IntersectStrings(f.ResourceTypes, rr)) > 0
+}
+
+func (f Filter) checkEventTypes(ee ...string) bool {
+	return len(f.EventTypes) == 0 || len(slice.IntersectStrings(f.EventTypes, ee)) > 0
 }
--- pkg/corredor/filter_test.go
+++ pkg/corredor/filter_test.go
@@ -6,12 +6,133 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestManualScriptFilterResourcePrefixing(t *testing.T) {
+func TestFilterResourcePrefixing(t *testing.T) {
 	f := &Filter{
-		ResourceTypes: []string{"system", "system:one", "two"},
+		ResourceTypePrefixes: []string{
+			"system",
+			"system:one",
+			"two",
+			"ui:admin:foo",
+			"ui:admin",
+		},
 	}
 
-	f.PrefixResources("system")
+	f.procRTPrefixes("system")
 
-	assert.New(t).Equal(f.ResourceTypes, []string{"system", "system:one", "system:two"})
+	assert.New(t).Equal(
+		f.ResourceTypePrefixes,
+		[]string{
+			"system",
+			"system:one",
+			"system:two",
+			"ui:admin:foo",
+			"ui:admin",
+		},
+	)
+
+	f = &Filter{}
+
+	f.procRTPrefixes("system")
+
+	assert.New(t).Equal(
+		f.ResourceTypePrefixes,
+		[]string{
+			"system",
+			"ui:",
+		},
+	)
+}
+
+func TestResourceTypesChecker(t *testing.T) {
+	var (
+		a = assert.New(t)
+	)
+
+	// nothing requested in the filter
+	a.True(Filter{ResourceTypes: []string{}}.checkResourceTypes("c"))
+
+	// no resources
+	a.False(Filter{ResourceTypes: []string{"a", "b"}}.checkResourceTypes())
+
+	// no resources, no filter
+	a.True(Filter{}.checkResourceTypes())
+
+	a.True(Filter{ResourceTypes: []string{"a"}}.checkResourceTypes("a", "b"))
+
+	a.False(Filter{ResourceTypes: []string{"c"}}.checkResourceTypes("a", "b"))
+}
+
+func TestEventTypesChecker(t *testing.T) {
+	var (
+		a = assert.New(t)
+	)
+
+	// nothing requested in the filter
+	a.True(Filter{EventTypes: []string{}}.checkEventTypes("c"))
+
+	// no resources
+	a.False(Filter{EventTypes: []string{"a", "b"}}.checkEventTypes())
+
+	// no resources, no filter
+	a.True(Filter{}.checkEventTypes())
+
+	a.True(Filter{EventTypes: []string{"a"}}.checkEventTypes("a", "b"))
+
+	a.False(Filter{EventTypes: []string{"c"}}.checkEventTypes("a", "b"))
+}
+
+func TestScriptFilterMaker(t *testing.T) {
+	var (
+		s1 = &Script{
+			Triggers: []*Trigger{
+				&Trigger{
+					EventTypes:    []string{"ev"},
+					ResourceTypes: []string{"res"},
+				},
+			},
+		}
+
+		s2 = &Script{
+			Triggers: []*Trigger{
+				&Trigger{
+					EventTypes:    []string{"foo"},
+					ResourceTypes: []string{"bar"},
+				},
+			},
+		}
+
+		s3 = &Script{
+			Triggers: []*Trigger{
+				&Trigger{
+					EventTypes:    []string{"not-a-match"},
+					ResourceTypes: []string{"not-a-match"},
+				},
+			},
+		}
+
+		a     = assert.New(t)
+		strip = func(b bool, _ error) bool { return b }
+
+		f func(s *Script) (b bool, err error)
+	)
+
+	f = Filter{}.makeFilterFn()
+	a.True(strip(f(s1)))
+	a.True(strip(f(s2)))
+	a.True(strip(f(s3)))
+
+	f = Filter{ResourceTypes: []string{"res"}}.makeFilterFn()
+	a.True(strip(f(s1)))
+	a.False(strip(f(s2)))
+	a.False(strip(f(s3)))
+
+	f = Filter{EventTypes: []string{"ev"}}.makeFilterFn()
+	a.True(strip(f(s1)))
+	a.False(strip(f(s2)))
+	a.False(strip(f(s3)))
+
+	f = Filter{EventTypes: []string{"ev", "foo"}}.makeFilterFn()
+	a.True(strip(f(s1)))
+	a.True(strip(f(s2)))
+	a.False(strip(f(s3)))
 }
