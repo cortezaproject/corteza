f7191963dc34aaad64efb01284a0affb9c41a871
Denis Arh <denis.arh@gmail.com>
2020-03-20 17:23:02 +0100

    Improve handling of complex constraint checks

--- system/service/event/application.go
+++ system/service/event/application.go
@@ -7,11 +7,11 @@ import (
 
 // Match returns false if given conditions do not match event & resource internals
 func (res applicationBase) Match(c eventbus.ConstraintMatcher) bool {
-	return applicationMatch(res.application, c, false)
+	return applicationMatch(res.application, c)
 }
 
 // Handles application matchers
-func applicationMatch(r *types.Application, c eventbus.ConstraintMatcher, def bool) bool {
+func applicationMatch(r *types.Application, c eventbus.ConstraintMatcher) bool {
 	switch c.Name() {
 	// not supported yet
 	//case "application", "application.handle":
@@ -21,5 +21,5 @@ func applicationMatch(r *types.Application, c eventbus.ConstraintMatcher, def bo
 
 	}
 
-	return def
+	return false
 }
