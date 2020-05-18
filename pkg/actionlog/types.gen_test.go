086411722e584324e4ac3b5bfd70b5ed6baf3999
Denis Arh <denis.arh@gmail.com>
2020-05-18 14:32:30 +0200

    Action log implementation

---
+++ pkg/actionlog/types.gen_test.go
@@ -0,0 +1,68 @@
+package actionlog
+
+import (
+	"testing"
+
+	"errors"
+
+	"github.com/stretchr/testify/require"
+)
+
+// 	Hello! This file is auto-generated.
+
+func TestEventSetWalk(t *testing.T) {
+	var (
+		value = make(EventSet, 3)
+		req   = require.New(t)
+	)
+
+	// check walk with no errors
+	{
+		err := value.Walk(func(*Event) error {
+			return nil
+		})
+		req.NoError(err)
+	}
+
+	// check walk with error
+	req.Error(value.Walk(func(*Event) error { return errors.New("walk error") }))
+
+}
+
+func TestEventSetFilter(t *testing.T) {
+	var (
+		value = make(EventSet, 3)
+		req   = require.New(t)
+	)
+
+	// filter nothing
+	{
+		set, err := value.Filter(func(*Event) (bool, error) {
+			return true, nil
+		})
+		req.NoError(err)
+		req.Equal(len(set), len(value))
+	}
+
+	// filter one item
+	{
+		found := false
+		set, err := value.Filter(func(*Event) (bool, error) {
+			if !found {
+				found = true
+				return found, nil
+			}
+			return false, nil
+		})
+		req.NoError(err)
+		req.Len(set, 1)
+	}
+
+	// filter error
+	{
+		_, err := value.Filter(func(*Event) (bool, error) {
+			return false, errors.New("filter error")
+		})
+		req.Error(err)
+	}
+}
