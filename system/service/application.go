84bb75e6f74d20147cf068b3623a101702eb830f
Denis Arh <denis.arh@gmail.com>
2020-05-26 08:40:01 +0200

    Implement actionlog for applications

--- system/service/application.go
+++ system/service/application.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/titpetric/factory"
 
+	"github.com/cortezaproject/corteza-server/pkg/actionlog"
 	"github.com/cortezaproject/corteza-server/pkg/eventbus"
 	"github.com/cortezaproject/corteza-server/pkg/permissions"
 	"github.com/cortezaproject/corteza-server/pkg/rh"
@@ -20,6 +21,8 @@ type (
 
 		ac       applicationAccessController
 		eventbus eventDispatcher
+
+		actionlog actionlog.Recorder
 
 		application repository.ApplicationRepository
 	}
@@ -64,69 +67,109 @@ func (svc *application) With(ctx context.Context) ApplicationService {
 		ac:       svc.ac,
 		eventbus: svc.eventbus,
 
+		actionlog: DefaultActionlog,
+
 		application: repository.Application(ctx, db),
 	}
 }
 
 func (svc *application) FindByID(ID uint64) (app *types.Application, err error) {
-	if ID == 0 {
-		return nil, ErrInvalidID.withStack()
-	}
+	var (
+		aaProps = &applicationActionProps{application: &types.Application{ID: ID}}
+	)
 
-	if app, err = svc.application.FindByID(ID); err != nil {
-		return nil, err
-	}
+	err = svc.db.Transaction(func() error {
+		if ID == 0 {
+			return ApplicationErrInvalidID()
+		}
 
-	if !svc.ac.CanReadApplication(svc.ctx, app) {
-		return nil, ErrNoPermissions.withStack()
-	}
+		if app, err = svc.application.FindByID(ID); err != nil {
+			return ApplicationErrInvalidID().Wrap(err)
+		}
 
-	return app, nil
+		aaProps.setApplication(app)
+
+		if !svc.ac.CanReadApplication(svc.ctx, app) {
+			return ApplicationErrNotAllowedToRead()
+		}
+
+		return nil
+	})
+
+	return app, svc.recordAction(svc.ctx, aaProps, ApplicationActionLookup, err)
 }
 
-func (svc *application) Find(f types.ApplicationFilter) (types.ApplicationSet, types.ApplicationFilter, error) {
-	f.IsReadable = svc.ac.FilterReadableApplications(svc.ctx)
+func (svc *application) Find(filter types.ApplicationFilter) (aa types.ApplicationSet, f types.ApplicationFilter, err error) {
+	var (
+		aaProps = &applicationActionProps{filter: &filter}
+	)
 
-	if f.Deleted > rh.FilterStateExcluded {
-		// If list with deleted applications is requested
-		// user must have access permissions to system (ie: is admin)
-		//
-		// not the best solution but ATM it allows us to have at least
-		// some kind of control over who can see deleted applications
-		if !svc.ac.CanAccess(svc.ctx) {
-			return nil, f, ErrNoPermissions.withStack()
+	err = svc.db.Transaction(func() error {
+		filter.IsReadable = svc.ac.FilterReadableApplications(svc.ctx)
+
+		if filter.Deleted > rh.FilterStateExcluded {
+			// If list with deleted applications is requested
+			// user must have access permissions to system (ie: is admin)
+			//
+			// not the best solution but ATM it allows us to have at least
+			// some kind of control over who can see deleted applications
+			if !svc.ac.CanAccess(svc.ctx) {
+				return ApplicationErrNotAllowedToListApplications()
+			}
 		}
-	}
 
-	return svc.application.Find(f)
+		aa, f, err = svc.application.Find(filter)
+		return err
+	})
+
+	return aa, f, svc.recordAction(svc.ctx, aaProps, ApplicationActionSearch, err)
 }
 
 func (svc *application) Create(new *types.Application) (app *types.Application, err error) {
-	if !svc.ac.CanCreateApplication(svc.ctx) {
-		return nil, ErrNoPermissions.withStack()
-	}
+	var (
+		aaProps = &applicationActionProps{new: new}
+	)
 
-	if err = svc.eventbus.WaitFor(svc.ctx, event.ApplicationBeforeCreate(new, nil)); err != nil {
-		return
-	}
+	err = svc.db.Transaction(func() (err error) {
+		if !svc.ac.CanCreateApplication(svc.ctx) {
+			return ApplicationErrNotAllowedToCreate()
+		}
 
-	if app, err = svc.application.Create(new); err != nil {
-		return
-	}
+		if err = svc.eventbus.WaitFor(svc.ctx, event.ApplicationBeforeCreate(new, nil)); err != nil {
+			return
+		}
 
-	defer svc.eventbus.Dispatch(svc.ctx, event.ApplicationAfterCreate(new, nil))
-	return
+		if app, err = svc.application.Create(new); err != nil {
+			return
+		}
 
+		aaProps.setApplication(app)
+
+		defer svc.eventbus.Dispatch(svc.ctx, event.ApplicationAfterCreate(new, nil))
+		return nil
+	})
+
+	return app, svc.recordAction(svc.ctx, aaProps, ApplicationActionCreate, err)
 }
 
 func (svc *application) Update(upd *types.Application) (app *types.Application, err error) {
-	if !svc.ac.CanUpdateApplication(svc.ctx, upd) {
-		return nil, ErrNoPermissions.withStack()
-	}
+	var (
+		aaProps = &applicationActionProps{update: upd}
+	)
 
-	return app, svc.db.Transaction(func() (err error) {
+	err = svc.db.Transaction(func() (err error) {
+		if upd.ID == 0 {
+			return ApplicationErrInvalidID()
+		}
+
 		if app, err = svc.application.FindByID(upd.ID); err != nil {
 			return
+		}
+
+		aaProps.setApplication(app)
+
+		if !svc.ac.CanUpdateApplication(svc.ctx, app) {
+			return ApplicationErrNotAllowedToUpdate()
 		}
 
 		if err = svc.eventbus.WaitFor(svc.ctx, event.ApplicationBeforeUpdate(upd, app)); err != nil {
@@ -145,38 +188,76 @@ func (svc *application) Update(upd *types.Application) (app *types.Application, 
 		defer svc.eventbus.Dispatch(svc.ctx, event.ApplicationAfterUpdate(upd, app))
 		return nil
 	})
+
+	return app, svc.recordAction(svc.ctx, aaProps, ApplicationActionUpdate, err)
 }
 
 func (svc *application) Delete(ID uint64) (err error) {
 	var (
-		app *types.Application
+		aaProps = &applicationActionProps{}
+		app     *types.Application
 	)
 
-	if app, err = svc.application.FindByID(ID); err != nil {
-		return
-	}
+	err = svc.db.Transaction(func() (err error) {
+		if ID == 0 {
+			return ApplicationErrInvalidID()
+		}
 
-	if !svc.ac.CanDeleteApplication(svc.ctx, app) {
-		return ErrNoPermissions.withStack()
-	}
-	if err = svc.eventbus.WaitFor(svc.ctx, event.ApplicationBeforeDelete(nil, app)); err != nil {
-		return
-	}
+		if app, err = svc.application.FindByID(ID); err != nil {
+			return
+		}
 
-	if err = svc.application.DeleteByID(ID); err != nil {
-		return
-	}
+		if !svc.ac.CanDeleteApplication(svc.ctx, app) {
+			return ApplicationErrNotAllowedToDelete()
+		}
 
-	defer svc.eventbus.Dispatch(svc.ctx, event.ApplicationAfterDelete(nil, app))
-	return
+		if err = svc.eventbus.WaitFor(svc.ctx, event.ApplicationBeforeDelete(nil, app)); err != nil {
+			return
+		}
+
+		if err = svc.application.DeleteByID(ID); err != nil {
+			return
+		}
+
+		defer svc.eventbus.Dispatch(svc.ctx, event.ApplicationAfterDelete(nil, app))
+		return nil
+	})
+
+	return svc.recordAction(svc.ctx, aaProps, ApplicationActionDelete, err)
 }
 
-func (svc *application) Undelete(ID uint64) error {
-	app := &types.Application{ID: ID}
+func (svc *application) Undelete(ID uint64) (err error) {
+	var (
+		aaProps = &applicationActionProps{}
+		app     *types.Application
+	)
 
-	if !svc.ac.CanDeleteApplication(svc.ctx, app) {
-		return ErrNoPermissions.withStack()
-	}
+	err = svc.db.Transaction(func() (err error) {
+		if ID == 0 {
+			return ApplicationErrInvalidID()
+		}
 
-	return svc.application.UndeleteByID(ID)
+		if app, err = svc.application.FindByID(ID); err != nil {
+			return
+		}
+
+		if !svc.ac.CanDeleteApplication(svc.ctx, app) {
+			return ApplicationErrNotAllowedToUndelete()
+		}
+
+		// @todo add event
+		//       if err = svc.eventbus.WaitFor(svc.ctx, event.ApplicationBeforeUndelete(nil, app)); err != nil {
+		//       	return
+		//       }
+
+		if err = svc.application.UndeleteByID(ID); err != nil {
+			return
+		}
+
+		// @todo add event
+		//       defer svc.eventbus.Dispatch(svc.ctx, event.ApplicationAfterUndelete(nil, app))
+		return nil
+	})
+
+	return svc.recordAction(svc.ctx, aaProps, ApplicationActionDelete, err)
 }
