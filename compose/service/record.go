ee431cea301d826ea55b099ede7bc6b805d78dd2
Denis Arh <denis.arh@gmail.com>
2020-05-29 18:08:29 +0200

    Add missing export action, fix import action log

--- compose/service/record.go
+++ compose/service/record.go
@@ -412,7 +412,7 @@ func (svc record) Import(ses *RecordImportSession, ssvc ImportSessionService) (e
 // @todo better value handling
 func (svc record) Export(filter types.RecordFilter, enc Encoder) (err error) {
 	var (
-		aProps = &recordActionProps{}
+		aProps = &recordActionProps{filter: &filter}
 	)
 
 	err = svc.db.Transaction(func() (err error) {
@@ -433,7 +433,7 @@ func (svc record) Export(filter types.RecordFilter, enc Encoder) (err error) {
 		return set.Walk(enc.Record)
 	})
 
-	return svc.recordAction(svc.ctx, aProps, RecordActionImport, err)
+	return svc.recordAction(svc.ctx, aProps, RecordActionExport, err)
 }
 
 // Bulk handles provided set of bulk record operations.
