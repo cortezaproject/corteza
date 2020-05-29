288e516986a0ceeca817a90b69c77b7364c38c7f
Denis Arh <denis.arh@gmail.com>
2020-05-29 15:22:58 +0200

    Fix bulk ops logic & (record value) error handling

--- tests/helpers/assert.go
+++ tests/helpers/assert.go
@@ -2,21 +2,36 @@ package helpers
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 
+	"github.com/davecgh/go-spew/spew"
 	"github.com/pkg/errors"
+
+	"github.com/cortezaproject/corteza-server/compose/types"
 )
 
 type (
 	assertFn func(*http.Response, *http.Request) error
 
-	StdErrorResponse struct{ Error struct{ Message string } }
+	StdErrorResponse struct {
+		Error struct {
+			Message string
+		}
+	}
+
+	RecordValueErrorSetResponse struct {
+		Error struct {
+			Message string
+			Details []types.RecordValueError
+		}
+	}
 )
 
 // decodes response body to given struct
-func decodeBody(rsp *http.Response, s interface{}) error {
+func DecodeBody(rsp *http.Response, s interface{}) error {
 	if err := json.NewDecoder(rsp.Body).Decode(&s); err != nil {
-		return errors.Wrap(err, "could not assert IsAuthorized")
+		return fmt.Errorf("could not decode body: %w", err)
 	}
 
 	return nil
@@ -47,15 +62,68 @@ func firstErr(ee ...interface{}) error {
 // AssertNoErrors ensures there are no errors in the response
 func AssertNoErrors(rsp *http.Response, _ *http.Request) (err error) {
 	tmp := StdErrorResponse{}
-	return firstErr(decodeBody(rsp, &tmp), tmp)
+	return firstErr(DecodeBody(rsp, &tmp), tmp)
+}
+
+// Asserts that all expected errors are returned
+//
+// Compares each error by Kind, Message and Meta.field
+//
+// Note: This assertion always expects errors!
+func AssertRecordValueError(exp ...*types.RecordValueError) assertFn {
+	return func(rsp *http.Response, _ *http.Request) (err error) {
+		rcvd := RecordValueErrorSetResponse{}
+		if err = DecodeBody(rsp, &rcvd); err != nil {
+			return err
+		}
+
+		if len(rcvd.Error.Details) == 0 {
+			return fmt.Errorf("expecting value errors, none received")
+		}
+
+	expLoop:
+		for _, e := range exp {
+			for _, r := range rcvd.Error.Details {
+				if e.Kind != r.Kind {
+					continue
+				}
+				if e.Message != r.Message {
+					continue
+				}
+				if e.Meta["field"] != r.Meta["field"] {
+					continue
+				}
+
+				// found expected error
+				continue expLoop
+			}
+
+			// did not find expected error
+			return fmt.Errorf("did not find expected error %v", e)
+		}
+
+		return nil
+	}
+}
+
+// Dump can be put into Assert()
+func Dump(rsp *http.Response, _ *http.Request) (err error) {
+	spew.Dump(rsp.Status)
+	spew.Dump(rsp.Header)
+	var payload interface{}
+	if err = DecodeBody(rsp, &payload); err != nil {
+		return err
+	}
+	spew.Dump(payload)
+	return nil
 }
 
 // AssertError ensures there are no errors in the response
 func AssertError(expectedError string) assertFn {
 	return func(rsp *http.Response, _ *http.Request) (err error) {
 		tmp := StdErrorResponse{}
-		if err = decodeBody(rsp, &tmp); err != nil {
-			return errors.Errorf("Could not decode body: %v", err)
+		if err = DecodeBody(rsp, &tmp); err != nil {
+			return err
 		}
 
 		if tmp.Error.Message == "" {
