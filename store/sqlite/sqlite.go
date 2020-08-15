3554d2e515c43eef60c38382724e841058685824
Denis Arh <denis.arh@gmail.com>
2020-08-15 18:46:44 +0200

    Implement new store infrastructure

--- store/sqlite/sqlite.go
+++ store/sqlite/sqlite.go
@@ -33,6 +33,26 @@ func New(ctx context.Context, dsn string) (s *Store, err error) {
 	return s, nil
 }
 
+func NewInMemory(ctx context.Context) (s *Store, err error) {
+	var (
+		dsn = "sqlite3://file::memory:?cache=shared"
+		cfg *rdbms.Config
+	)
+
+	if cfg, err = ProcDataSourceName(dsn); err != nil {
+		return nil, err
+	}
+
+	cfg.PlaceholderFormat = squirrel.Dollar
+
+	s = new(Store)
+	if s.Store, err = rdbms.New(ctx, cfg); err != nil {
+		return nil, err
+	}
+
+	return s, nil
+}
+
 func (s *Store) Upgrade(ctx context.Context, log *zap.Logger) (err error) {
 	if err = (&rdbms.Schema{}).Upgrade(ctx, NewUpgrader(log, s)); err != nil {
 		return fmt.Errorf("can not upgrade sqlite schema: %w", err)
