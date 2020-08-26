pandurd/corteza-server

Cleaner store API

store/credentials.gen.go
@@ -1,0 +1,76 @@
+package store
+
+// This file is auto-generated.
+//
+// Template:    pkg/codegen/assets/store_base.gen.go.tpl
+// Definitions: store/credentials.yaml
+//
+// Changes to this file may cause incorrect behavior and will be lost if
+// the code is regenerated.
+//
+// Definitions file that controls how this file is generated:
+//  - store/credentials.yaml
+
+import (
+	"context"
+	"github.com/cortezaproject/corteza-server/system/types"
+)
+
+type (
+	Credentials interface {
+		SearchCredentials(ctx context.Context, f types.CredentialsFilter) (types.CredentialsSet, types.CredentialsFilter, error)
+		LookupCredentialsByID(ctx context.Context, id uint64) (*types.Credentials, error)
+		CreateCredentials(ctx context.Context, rr ...*types.Credentials) error
+		UpdateCredentials(ctx context.Context, rr ...*types.Credentials) error
+		PartialCredentialsUpdate(ctx context.Context, onlyColumns []string, rr ...*types.Credentials) error
+		RemoveCredentials(ctx context.Context, rr ...*types.Credentials) error
+		RemoveCredentialsByID(ctx context.Context, ID uint64) error
+
+		TruncateCredentials(ctx context.Context) error
+
+		// Extra functions
+
+	}
+)
+
+// SearchCredentials returns all matching Credentials from store
+func SearchCredentials(ctx context.Context, s Credentials, f types.CredentialsFilter) (types.CredentialsSet, types.CredentialsFilter, error) {
+	return s.SearchCredentials(ctx, f)
+}
+
+// LookupCredentialsByID searches for credentials by ID
+//
+// It returns credentials even if deleted
+func LookupCredentialsByID(ctx context.Context, s Credentials, id uint64) (*types.Credentials, error) {
+	return s.LookupCredentialsByID(ctx, id)
+}
+
+// CreateCredentials creates one or more Credentials in store
+func CreateCredentials(ctx context.Context, s Credentials, rr ...*types.Credentials) error {
+	return s.CreateCredentials(ctx, rr...)
+}
+
+// UpdateCredentials updates one or more (existing) Credentials in store
+func UpdateCredentials(ctx context.Context, s Credentials, rr ...*types.Credentials) error {
+	return s.UpdateCredentials(ctx, rr...)
+}
+
+// PartialCredentialsUpdate updates one or more existing Credentials in store
+func PartialCredentialsUpdate(ctx context.Context, s Credentials, onlyColumns []string, rr ...*types.Credentials) error {
+	return s.PartialCredentialsUpdate(ctx, onlyColumns, rr...)
+}
+
+// RemoveCredentials removes one or more Credentials from store
+func RemoveCredentials(ctx context.Context, s Credentials, rr ...*types.Credentials) error {
+	return s.RemoveCredentials(ctx, rr...)
+}
+
+// RemoveCredentialsByID removes Credentials from store
+func RemoveCredentialsByID(ctx context.Context, s Credentials, ID uint64) error {
+	return s.RemoveCredentialsByID(ctx, ID)
+}
+
+// TruncateCredentials removes all Credentials from store
+func TruncateCredentials(ctx context.Context, s Credentials) error {
+	return s.TruncateCredentials(ctx)
+}

store/reminders.gen.go
@@ -1,0 +1,76 @@
+package store
+
+// This file is auto-generated.
+//
+// Template:    pkg/codegen/assets/store_base.gen.go.tpl
+// Definitions: store/reminders.yaml
+//
+// Changes to this file may cause incorrect behavior and will be lost if
+// the code is regenerated.
+//
+// Definitions file that controls how this file is generated:
+//  - store/reminders.yaml
+
+import (
+	"context"
+	"github.com/cortezaproject/corteza-server/system/types"
+)
+
+type (
+	Reminders interface {
+		SearchReminders(ctx context.Context, f types.ReminderFilter) (types.ReminderSet, types.ReminderFilter, error)
+		LookupReminderByID(ctx context.Context, id uint64) (*types.Reminder, error)
+		CreateReminder(ctx context.Context, rr ...*types.Reminder) error
+		UpdateReminder(ctx context.Context, rr ...*types.Reminder) error
+		PartialReminderUpdate(ctx context.Context, onlyColumns []string, rr ...*types.Reminder) error
+		RemoveReminder(ctx context.Context, rr ...*types.Reminder) error
+		RemoveReminderByID(ctx context.Context, ID uint64) error
+
+		TruncateReminders(ctx context.Context) error
+
+		// Extra functions
+
+	}
+)
+
+// SearchReminders returns all matching Reminders from store
+func SearchReminders(ctx context.Context, s Reminders, f types.ReminderFilter) (types.ReminderSet, types.ReminderFilter, error) {
+	return s.SearchReminders(ctx, f)
+}
+
+// LookupReminderByID searches for reminder by its ID
+//
+// It returns reminder even if deleted or suspended
+func LookupReminderByID(ctx context.Context, s Reminders, id uint64) (*types.Reminder, error) {
+	return s.LookupReminderByID(ctx, id)
+}
+
+// CreateReminder creates one or more Reminders in store
+func CreateReminder(ctx context.Context, s Reminders, rr ...*types.Reminder) error {
+	return s.CreateReminder(ctx, rr...)
+}
+
+// UpdateReminder updates one or more (existing) Reminders in store
+func UpdateReminder(ctx context.Context, s Reminders, rr ...*types.Reminder) error {
+	return s.UpdateReminder(ctx, rr...)
+}
+
+// PartialReminderUpdate updates one or more existing Reminders in store
+func PartialReminderUpdate(ctx context.Context, s Reminders, onlyColumns []string, rr ...*types.Reminder) error {
+	return s.PartialReminderUpdate(ctx, onlyColumns, rr...)
+}
+
+// RemoveReminder removes one or more Reminders from store
+func RemoveReminder(ctx context.Context, s Reminders, rr ...*types.Reminder) error {
+	return s.RemoveReminder(ctx, rr...)
+}
+
+// RemoveReminderByID removes Reminder from store
+func RemoveReminderByID(ctx context.Context, s Reminders, ID uint64) error {
+	return s.RemoveReminderByID(ctx, ID)
+}
+
+// TruncateReminders removes all Reminders from store
+func TruncateReminders(ctx context.Context, s Reminders) error {
+	return s.TruncateReminders(ctx)
+}

cortezaproject/corteza

store/credentials.gen.go
@@ -1,0 +1,83 @@
+package store
+
+// This file is auto-generated.
+//
+// Template:    pkg/codegen/assets/store_base.gen.go.tpl
+// Definitions: store/credentials.yaml
+//
+// Changes to this file may cause incorrect behavior and will be lost if
+// the code is regenerated.
+
+import (
+	"context"
+	"github.com/cortezaproject/corteza-server/system/types"
+)
+
+type (
+	Credentials interface {
+		SearchCredentials(ctx context.Context, f types.CredentialsFilter) (types.CredentialsSet, types.CredentialsFilter, error)
+		LookupCredentialsByID(ctx context.Context, id uint64) (*types.Credentials, error)
+
+		CreateCredentials(ctx context.Context, rr ...*types.Credentials) error
+
+		UpdateCredentials(ctx context.Context, rr ...*types.Credentials) error
+		PartialCredentialsUpdate(ctx context.Context, onlyColumns []string, rr ...*types.Credentials) error
+
+		UpsertCredentials(ctx context.Context, rr ...*types.Credentials) error
+
+		DeleteCredentials(ctx context.Context, rr ...*types.Credentials) error
+		DeleteCredentialsByID(ctx context.Context, ID uint64) error
+
+		TruncateCredentials(ctx context.Context) error
+	}
+)
+
+var _ *types.Credentials
+var _ context.Context
+
+// SearchCredentials returns all matching Credentials from store
+func SearchCredentials(ctx context.Context, s Credentials, f types.CredentialsFilter) (types.CredentialsSet, types.CredentialsFilter, error) {
+	return s.SearchCredentials(ctx, f)
+}
+
+// LookupCredentialsByID searches for credentials by ID
+//
+// It returns credentials even if deleted
+func LookupCredentialsByID(ctx context.Context, s Credentials, id uint64) (*types.Credentials, error) {
+	return s.LookupCredentialsByID(ctx, id)
+}
+
+// CreateCredentials creates one or more Credentials in store
+func CreateCredentials(ctx context.Context, s Credentials, rr ...*types.Credentials) error {
+	return s.CreateCredentials(ctx, rr...)
+}
+
+// UpdateCredentials updates one or more (existing) Credentials in store
+func UpdateCredentials(ctx context.Context, s Credentials, rr ...*types.Credentials) error {
+	return s.UpdateCredentials(ctx, rr...)
+}
+
+// PartialCredentialsUpdate updates one or more existing Credentials in store
+func PartialCredentialsUpdate(ctx context.Context, s Credentials, onlyColumns []string, rr ...*types.Credentials) error {
+	return s.PartialCredentialsUpdate(ctx, onlyColumns, rr...)
+}
+
+// UpsertCredentials creates new or updates existing one or more Credentials in store
+func UpsertCredentials(ctx context.Context, s Credentials, rr ...*types.Credentials) error {
+	return s.UpsertCredentials(ctx, rr...)
+}
+
+// DeleteCredentials Deletes one or more Credentials from store
+func DeleteCredentials(ctx context.Context, s Credentials, rr ...*types.Credentials) error {
+	return s.DeleteCredentials(ctx, rr...)
+}
+
+// DeleteCredentialsByID Deletes Credentials from store
+func DeleteCredentialsByID(ctx context.Context, s Credentials, ID uint64) error {
+	return s.DeleteCredentialsByID(ctx, ID)
+}
+
+// TruncateCredentials Deletes all Credentials from store
+func TruncateCredentials(ctx context.Context, s Credentials) error {
+	return s.TruncateCredentials(ctx)
+}

store/reminders.gen.go
@@ -1,0 +1,83 @@
+package store
+
+// This file is auto-generated.
+//
+// Template:    pkg/codegen/assets/store_base.gen.go.tpl
+// Definitions: store/reminders.yaml
+//
+// Changes to this file may cause incorrect behavior and will be lost if
+// the code is regenerated.
+
+import (
+	"context"
+	"github.com/cortezaproject/corteza-server/system/types"
+)
+
+type (
+	Reminders interface {
+		SearchReminders(ctx context.Context, f types.ReminderFilter) (types.ReminderSet, types.ReminderFilter, error)
+		LookupReminderByID(ctx context.Context, id uint64) (*types.Reminder, error)
+
+		CreateReminder(ctx context.Context, rr ...*types.Reminder) error
+
+		UpdateReminder(ctx context.Context, rr ...*types.Reminder) error
+		PartialReminderUpdate(ctx context.Context, onlyColumns []string, rr ...*types.Reminder) error
+
+		UpsertReminder(ctx context.Context, rr ...*types.Reminder) error
+
+		DeleteReminder(ctx context.Context, rr ...*types.Reminder) error
+		DeleteReminderByID(ctx context.Context, ID uint64) error
+
+		TruncateReminders(ctx context.Context) error
+	}
+)
+
+var _ *types.Reminder
+var _ context.Context
+
+// SearchReminders returns all matching Reminders from store
+func SearchReminders(ctx context.Context, s Reminders, f types.ReminderFilter) (types.ReminderSet, types.ReminderFilter, error) {
+	return s.SearchReminders(ctx, f)
+}
+
+// LookupReminderByID searches for reminder by its ID
+//
+// It returns reminder even if deleted or suspended
+func LookupReminderByID(ctx context.Context, s Reminders, id uint64) (*types.Reminder, error) {
+	return s.LookupReminderByID(ctx, id)
+}
+
+// CreateReminder creates one or more Reminders in store
+func CreateReminder(ctx context.Context, s Reminders, rr ...*types.Reminder) error {
+	return s.CreateReminder(ctx, rr...)
+}
+
+// UpdateReminder updates one or more (existing) Reminders in store
+func UpdateReminder(ctx context.Context, s Reminders, rr ...*types.Reminder) error {
+	return s.UpdateReminder(ctx, rr...)
+}
+
+// PartialReminderUpdate updates one or more existing Reminders in store
+func PartialReminderUpdate(ctx context.Context, s Reminders, onlyColumns []string, rr ...*types.Reminder) error {
+	return s.PartialReminderUpdate(ctx, onlyColumns, rr...)
+}
+
+// UpsertReminder creates new or updates existing one or more Reminders in store
+func UpsertReminder(ctx context.Context, s Reminders, rr ...*types.Reminder) error {
+	return s.UpsertReminder(ctx, rr...)
+}
+
+// DeleteReminder Deletes one or more Reminders from store
+func DeleteReminder(ctx context.Context, s Reminders, rr ...*types.Reminder) error {
+	return s.DeleteReminder(ctx, rr...)
+}
+
+// DeleteReminderByID Deletes Reminder from store
+func DeleteReminderByID(ctx context.Context, s Reminders, ID uint64) error {
+	return s.DeleteReminderByID(ctx, ID)
+}
+
+// TruncateReminders Deletes all Reminders from store
+func TruncateReminders(ctx context.Context, s Reminders) error {
+	return s.TruncateReminders(ctx)
+}