f4be53a04604d02e0dc568da92da29fbe4da838d
Denis Arh <denis.arh@gmail.com>
2020-09-02 21:45:23 +0200

    Improve healtcheck for mail

--- pkg/mail/healthcheck.go
+++ pkg/mail/healthcheck.go
@@ -7,12 +7,12 @@ import (
 
 // Healtcheck for (global) scheduler
 func Healthcheck(ctx context.Context) error {
-	if defaultDialer == nil {
-		return fmt.Errorf("uninitialized")
+	if defaultDialerError == nil {
+		return defaultDialerError
 	}
 
-	if defaultDialerError != nil {
-		return defaultDialerError
+	if defaultDialer == nil {
+		return fmt.Errorf("uninitialized")
 	}
 
 	return nil
