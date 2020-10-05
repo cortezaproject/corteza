74d4dab470580bf472516e839b21438ed387289f
Peter Grlica <peter.grlica@gmail.com>
2020-10-05 17:25:04 +0200

    Renamed module mapping rel_ fields

--- store/rdbms/federation_module_mappings.go
+++ store/rdbms/federation_module_mappings.go
@@ -9,11 +9,11 @@ func (s Store) convertFederationModuleMappingFilter(f types.ModuleMappingFilter)
 	query = s.federationModuleMappingsSelectBuilder()
 
 	if f.ComposeModuleID > 0 {
-		query = query.Where("cmd.compose_module_id = ?", f.ComposeModuleID)
+		query = query.Where("cmd.rel_compose_module = ?", f.ComposeModuleID)
 	}
 
 	if f.FederationModuleID > 0 {
-		query = query.Where("cmd.federation_module_id = ?", f.FederationModuleID)
+		query = query.Where("cmd.rel_federation_module = ?", f.FederationModuleID)
 	}
 
 	return
