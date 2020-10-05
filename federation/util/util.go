26e833d3a5e9847d1f4c86d7e0a73feb7de9e00d
Tomaž Jerman <tomaz.jerman121@gmail.com>
2020-10-05 11:47:16 +0200

    Implement federation node pairing flow

---
+++ federation/util/util.go
@@ -0,0 +1,71 @@
+package util
+
+import (
+	"errors"
+	"net/url"
+	"strconv"
+)
+
+var (
+	ErrorURIMissingToken = errors.New("uri: token missing")
+)
+
+const ()
+
+type (
+	NodeURIParams struct {
+		Name string
+	}
+
+	DecodedURI struct {
+		Domain string
+		Token  string
+		NodeID uint64
+		Params NodeURIParams
+	}
+)
+
+func DecodeURI(i string) (du *DecodedURI, err error) {
+	du = &DecodedURI{
+		Params: NodeURIParams{},
+	}
+
+	u, err := url.Parse(i)
+	if err != nil {
+		return
+	}
+
+	du.Domain = u.Host
+	nodeS := u.User.Username()
+	if nodeS == "" {
+		return nil, ErrorURIMissingToken
+	}
+	nodeID, err := strconv.ParseUint(nodeS, 10, 64)
+	if err != nil {
+		return nil, err
+	}
+	du.NodeID = nodeID
+
+	token, has := u.User.Password()
+	if !has {
+		return nil, ErrorURIMissingToken
+	}
+	du.Token = token
+
+	params := u.Query()
+	if name, has := params["name"]; has && len(name) > 0 {
+		du.Params.Name = name[0]
+	}
+
+	return
+}
+
+func EncodeURI(ott, domain string, nodeID uint64) string {
+	u := url.URL{
+		Scheme: "corteza",
+		User:   url.UserPassword(strconv.FormatUint(nodeID, 10), ott),
+		Host:   domain,
+	}
+
+	return u.String()
+}
