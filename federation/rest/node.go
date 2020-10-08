7b716d2d42119ca1765e41d902dbaf37bb90e2a6
Denis Arh <denis.arh@gmail.com>
2020-10-08 21:18:09 +0200

    Improve & finalize fed. node REST & integ. tests

--- federation/rest/node.go
+++ federation/rest/node.go
@@ -2,21 +2,37 @@ package rest
 
 import (
 	"context"
-	"errors"
-
 	"github.com/cortezaproject/corteza-server/federation/rest/request"
 	"github.com/cortezaproject/corteza-server/federation/service"
 	"github.com/cortezaproject/corteza-server/federation/types"
+	"github.com/titpetric/factory/resputil"
 )
 
 type (
-	Node struct {
-		svcNode service.NodeService
-	}
-)
+	nodeServicer interface {
+		Search(ctx context.Context, f types.NodeFilter) (types.NodeSet, types.NodeFilter, error)
+		Create(ctx context.Context, n *types.Node) (*types.Node, error)
+		CreateFromPairingURI(ctx context.Context, uri string) (*types.Node, error)
 
-var (
-	ErrInvalidNodeCreateParams = errors.New("create node: missing or invalid parameters")
+		Pair(ctx context.Context, nodeID uint64) error
+		HandshakeConfirm(ctx context.Context, nodeID uint64) error
+		HandshakeComplete(ctx context.Context, nodeID uint64, token string) error
+
+		RegenerateNodeURI(ctx context.Context, nodeID uint64) (string, error)
+	}
+
+	Node struct {
+		svcNode nodeServicer
+	}
+
+	moduleSetPayload struct {
+		Filter types.NodeFilter `json:"filter"`
+		Set    []*nodePayload   `json:"set"`
+	}
+
+	nodePayload struct {
+		*types.Node
+	}
 )
 
 func (Node) New() *Node {
@@ -25,36 +41,64 @@ func (Node) New() *Node {
 	}
 }
 
+func (ctrl Node) Search(ctx context.Context, r *request.NodeSearch) (interface{}, error) {
+	set, f, err := ctrl.svcNode.Search(ctx, types.NodeFilter{
+		Query:  r.Query,
+		Status: r.Status,
+	})
+
+	return ctrl.makeFilterPayload(ctx, set, f, err)
+}
+
 func (ctrl Node) Create(ctx context.Context, r *request.NodeCreate) (interface{}, error) {
-	if r.NodeURI == "" && (r.Domain == "" || r.Name == "") {
-		return nil, ErrInvalidNodeCreateParams
+	if r.PairingURI != "" {
+		return ctrl.svcNode.CreateFromPairingURI(ctx, r.PairingURI)
+	} else {
+		n := &types.Node{
+			BaseURL: r.BaseURL,
+			Name:    r.Name,
+		}
+
+		return ctrl.svcNode.Create(ctx, n)
 	}
+}
 
-	if r.NodeURI != "" {
-		return ctrl.svcNode.CreateFromURI(ctx, r.NodeURI, r.MyDomain)
-	} else if r.Domain != "" && r.Name != "" {
-		n := &types.Node{}
-		n.Domain = r.Domain
-		n.Name = r.Name
-		n.Status = types.NodeStatusPending
-
-		return ctrl.svcNode.Create(ctx, n, r.MyDomain)
-	}
-
-	return nil, ErrInvalidNodeCreateParams
+func (ctrl Node) GenerateURI(ctx context.Context, r *request.NodeGenerateURI) (interface{}, error) {
+	return ctrl.svcNode.RegenerateNodeURI(ctx, r.NodeID)
 }
 
 func (ctrl Node) Pair(ctx context.Context, r *request.NodePair) (interface{}, error) {
-	err := ctrl.svcNode.Pair(ctx, r.NodeID)
-	return nil, err
+	return resputil.OK(), ctrl.svcNode.Pair(ctx, r.NodeID)
 }
 
 func (ctrl Node) HandshakeConfirm(ctx context.Context, r *request.NodeHandshakeConfirm) (interface{}, error) {
-	err := ctrl.svcNode.HandshakeConfirm(ctx, r.NodeID)
-	return nil, err
+	return resputil.OK(), ctrl.svcNode.HandshakeConfirm(ctx, r.NodeID)
 }
 
 func (ctrl Node) HandshakeComplete(ctx context.Context, r *request.NodeHandshakeComplete) (interface{}, error) {
-	err := ctrl.svcNode.HandshakeComplete(ctx, r.NodeID, r.TokenA)
-	return nil, err
+	return resputil.OK(), ctrl.svcNode.HandshakeComplete(ctx, r.NodeID, r.TokenA)
+}
+
+func (ctrl Node) makePayload(ctx context.Context, m *types.Node, err error) (*nodePayload, error) {
+	if err != nil || m == nil {
+		return nil, err
+	}
+
+	return &nodePayload{
+		Node: m,
+	}, nil
+}
+
+func (ctrl Node) makeFilterPayload(ctx context.Context, nn types.NodeSet, f types.NodeFilter, err error) (*moduleSetPayload, error) {
+	if err != nil {
+		return nil, err
+	}
+
+	msp := &moduleSetPayload{Filter: f, Set: make([]*nodePayload, len(nn))}
+
+	for i := range nn {
+		msp.Set[i], _ = ctrl.makePayload(ctx, nn[i], nil)
+	}
+
+	return msp, nil
 }
---
+++ tests/federation/node_pairing_test.go
@@ -0,0 +1,206 @@
+package federation
+
+import (
+	"context"
+	"fmt"
+	"github.com/cortezaproject/corteza-server/federation/service"
+	"github.com/cortezaproject/corteza-server/federation/types"
+	"github.com/cortezaproject/corteza-server/store"
+	"github.com/cortezaproject/corteza-server/tests/helpers"
+	jsonpath "github.com/steinfletcher/apitest-jsonpath"
+	"net/http"
+	"strconv"
+	"testing"
+)
+
+type (
+	// node handshake mocker, allows custom functions
+	mockNodeHandshake struct {
+		init     func(context.Context, *types.Node, string) error
+		complete func(context.Context, *types.Node, string) error
+	}
+)
+
+func (h mockNodeHandshake) Init(ctx context.Context, n *types.Node, t string) error {
+	return h.init(ctx, n, t)
+}
+
+func (h mockNodeHandshake) Complete(ctx context.Context, n *types.Node, t string) error {
+	return h.complete(ctx, n, t)
+}
+
+func (h helper) clearNodes() {
+	h.noError(store.TruncateFederationNodes(context.Background(), service.DefaultStore))
+}
+
+func (h helper) lookupNodeByID(ID uint64) *types.Node {
+	n, err := service.DefaultStore.LookupFederationNodeByID(context.Background(), ID)
+	h.a.NoError(err)
+	return n
+}
+
+func TestSuccessfulNodePairing(t *testing.T) {
+	var (
+		h        = newHelper(t)
+		aNodeID  uint64
+		aNodeURI string
+		bNodeID  uint64
+
+		rspWithNode struct{ Response *types.Node }
+		rspWithURI  struct{ Response string }
+
+		checkNodeStatus = func(ID uint64, status string) {
+			n := h.lookupNodeByID(aNodeID)
+			h.a.NotNil(n)
+			h.a.Equal(n.Status, status)
+		}
+	)
+
+	service.DefaultNode.SetHandshaker(nil)
+
+	h.clearNodes()
+
+	{
+		// #############################################################################################################
+		t.Log("Step #1, create node on server A")
+
+		h.apiInit().
+			//Debug().
+			Post("/nodes").
+			FormData("name", "Server B").
+			FormData("baseURL", "https://api.server-b.tld/federation").
+			Expect(t).
+			Status(http.StatusOK).
+			Assert(helpers.AssertNoErrors).
+			End().
+			JSON(&rspWithNode)
+
+		// response ok?
+		h.a.NotNil(rspWithNode.Response)
+		aNodeID = rspWithNode.Response.ID
+		checkNodeStatus(aNodeID, types.NodeStatusPending)
+	}
+
+	{
+		// #############################################################################################################
+		t.Log("Step #2, request pairing URI")
+
+		h.apiInit().
+			//Debug().
+			Post(fmt.Sprintf("/nodes/%d/uri", aNodeID)).
+			Expect(t).
+			Status(http.StatusOK).
+			Assert(helpers.AssertNoErrors).
+			End().
+			JSON(&rspWithURI)
+
+		// response ok?
+		h.a.NotNil(rspWithNode.Response)
+		aNodeURI = rspWithURI.Response
+
+		checkNodeStatus(aNodeID, types.NodeStatusPending)
+	}
+
+	{
+		// #############################################################################################################
+		t.Log("Step #3, use pairing URI to create node on the 2nd server")
+
+		h.apiInit().
+			//Debug().
+			Post("/nodes").
+			FormData("pairingURI", aNodeURI).
+			Expect(t).
+			Status(http.StatusOK).
+			Assert(helpers.AssertNoErrors).
+			End().
+			JSON(&rspWithNode)
+
+		// Node stored.
+		h.a.NotNil(rspWithNode.Response)
+		bNodeID = rspWithNode.Response.ID
+
+		checkNodeStatus(aNodeID, types.NodeStatusPending)
+		checkNodeStatus(bNodeID, types.NodeStatusPending)
+	}
+
+	{
+		// #############################################################################################################
+		t.Log("Step #4, admin of 2nd server requests list of nodes")
+
+		h.apiInit().
+			//Debug().
+			Get("/nodes").
+			Query("status", types.NodeStatusPending).
+			Expect(t).
+			Status(http.StatusOK).
+			Assert(helpers.AssertNoErrors).
+			Assert(jsonpath.Present(fmt.Sprintf(`$.response.set[? @.sharedNodeID=="%d"]`, aNodeID))).
+			Assert(jsonpath.Present(fmt.Sprintf(`$.response.set[? @.nodeID=="%d"]`, bNodeID))).
+			End()
+	}
+
+	{
+		// #############################################################################################################
+		t.Log("Step #5, request pairing procedure on 2nd server")
+
+		// fake remote call with local/direct change
+		service.DefaultNode.SetHandshaker(&mockNodeHandshake{
+			init: func(ctx context.Context, n *types.Node, authToken string) error {
+				h.apiInit().
+					//Debug().
+					Post(fmt.Sprintf("/nodes/%d/handshake", n.SharedNodeID)).
+					FormData("pairToken", n.PairToken).
+					FormData("authToken", authToken).
+					FormData("sharedNodeID", strconv.FormatUint(n.ID, 10)).
+					Expect(t).
+					Status(http.StatusOK).
+					Assert(helpers.AssertNoErrors).
+					End()
+				return nil
+			},
+		})
+
+		h.apiInit().
+			//Debug().
+			Post(fmt.Sprintf("/nodes/%d/pair", bNodeID)).
+			Expect(t).
+			Status(http.StatusOK).
+			Assert(helpers.AssertNoErrors).
+			End()
+
+		checkNodeStatus(aNodeID, types.NodeStatusPairRequested)
+		checkNodeStatus(bNodeID, types.NodeStatusPairRequested)
+	}
+
+	{
+		// #############################################################################################################
+		t.Log("Step #6, handshake confirmation on 1st server")
+
+		// fake remote call with local/direct change
+		service.DefaultNode.SetHandshaker(&mockNodeHandshake{
+			complete: func(ctx context.Context, n *types.Node, authToken string) error {
+				h.apiInit().
+					//Debug().
+					Post(fmt.Sprintf("/nodes/%d/handshake-complete", n.SharedNodeID)).
+					FormData("authToken", authToken).
+					Expect(t).
+					Status(http.StatusOK).
+					Assert(helpers.AssertNoErrors).
+					End()
+
+				return nil
+			},
+		})
+
+		h.apiInit().
+			//Debug().
+			Post(fmt.Sprintf("/nodes/%d/handshake-confirm", aNodeID)).
+			Expect(t).
+			Status(http.StatusOK).
+			Assert(helpers.AssertNoErrors).
+			End()
+
+		checkNodeStatus(aNodeID, types.NodeStatusPaired)
+		checkNodeStatus(bNodeID, types.NodeStatusPaired)
+	}
+}
