d5ad6ec5565ef19fdc8bf7f0420b7989d50a473a
Denis Arh <denis.arh@gmail.com>
2020-10-12 15:22:35 +0200

    Fix compose module tests

--- compose/service/module_test.go
+++ compose/service/module_test.go
@@ -12,7 +12,7 @@ import (
 	"testing"
 )
 
-func TestCharts(t *testing.T) {
+func TestModules(t *testing.T) {
 	var (
 		ctx    = context.Background()
 		s, err = sqlite3.ConnectInMemory(ctx)
@@ -55,44 +55,44 @@ func TestCharts(t *testing.T) {
 			ac:    AccessControl(&rbac.ServiceAllowAll{}),
 		}
 		res, err := svc.Create(&types.Chart{Name: "My first chart", NamespaceID: namespaceID})
-		req.NoError(unwrapChartInternal(err))
+		req.NoError(unwrapModuleInternal(err))
 		req.NotNil(res)
 
 		res, err = svc.FindByID(namespaceID, res.ID)
-		req.NoError(unwrapChartInternal(err))
+		req.NoError(unwrapModuleInternal(err))
 		req.NotNil(res)
 
 		res, err = svc.FindByHandle(namespaceID, res.Handle)
-		req.NoError(unwrapChartInternal(err))
+		req.NoError(unwrapModuleInternal(err))
 		req.NotNil(res)
 
 		res.Name = "Changed"
 		res, err = svc.Update(res)
-		req.NoError(unwrapChartInternal(err))
+		req.NoError(unwrapModuleInternal(err))
 		req.NotNil(res)
 		req.NotNil(res.UpdatedAt)
 		req.Equal(res.Name, "Changed")
 
 		res, err = svc.FindByID(namespaceID, res.ID)
-		req.NoError(unwrapChartInternal(err))
+		req.NoError(unwrapModuleInternal(err))
 		req.NotNil(res)
 		req.Equal(res.Name, "Changed")
 
 		err = svc.DeleteByID(namespaceID, res.ID)
-		req.NoError(unwrapChartInternal(err))
+		req.NoError(unwrapModuleInternal(err))
 		req.NotNil(res)
 
 		// this works because we're allowed to do everything
 		res, err = svc.FindByID(namespaceID, res.ID)
-		req.NoError(unwrapChartInternal(err))
+		req.NoError(unwrapModuleInternal(err))
 		req.NotNil(res)
 		req.NotNil(res.DeletedAt)
 
 	})
 }
 
-func unwrapChartInternal(err error) error {
-	g := ChartErrGeneric()
+func unwrapModuleInternal(err error) error {
+	g := ModuleErrGeneric()
 	for {
 		if errors.Is(err, g) {
 			err = errors.Unwrap(err)
