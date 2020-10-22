8258e1941be1c74286811179269404addd6f6689
Denis Arh <denis.arh@gmail.com>
2020-10-22 23:12:05 +0200

    Add support for decoding roles from yaml def

---
+++ pkg/envoy/yaml/role_test.go
@@ -0,0 +1,47 @@
+package yaml
+
+import (
+	"github.com/stretchr/testify/require"
+	"gopkg.in/yaml.v3"
+	"testing"
+)
+
+func TestRole_UnmarshalYAML(t *testing.T) {
+	var (
+		parseString = func(src string) (*role, error) {
+			w := &role{}
+			return w, yaml.Unmarshal([]byte(src), w)
+		}
+	)
+
+	t.Run("empty", func(t *testing.T) {
+		req := require.New(t)
+
+		w, err := parseString(``)
+		req.NoError(err)
+		req.NotNil(w)
+		req.Nil(w.res)
+	})
+
+	t.Run("simple name", func(t *testing.T) {
+		req := require.New(t)
+
+		w, err := parseString(`{ name: Test }`)
+		req.NoError(err)
+		req.NotNil(w)
+		req.NotNil(w.res)
+		req.NotEmpty(w.res.Name)
+	})
+
+	t.Run("role 1", func(t *testing.T) {
+		req := require.New(t)
+
+		doc, err := parseDocument("role_1")
+		req.NoError(err)
+		req.NotNil(doc)
+		req.Len(doc.roles, 2)
+		req.NotNil(doc.roles[1])
+		req.Equal("Role2", doc.roles[1].res.Name)
+		req.Equal("r2", doc.roles[1].res.Handle)
+	})
+}
