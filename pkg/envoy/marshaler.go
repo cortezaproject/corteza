55f56d50d570fca622e51c7d8497bc932ad35057
Tomaž Jerman <tomaz.jerman121@gmail.com>
2020-11-02 15:00:55 +0100

    Tweak encoder, marshler so it doesn't fail to compile

--- pkg/envoy/marshaler.go
+++ pkg/envoy/marshaler.go
@@ -2,22 +2,24 @@ package envoy
 
 import (
 	"fmt"
+
+	"github.com/cortezaproject/corteza-server/pkg/envoy/resource"
 )
 
 type (
 	Marshaller interface {
-		MarshalEnvoy() ([]Node, error)
+		MarshalEnvoy() ([]resource.Interface, error)
 	}
 )
 
 // MarshalMerge takes one or more nodes and Marshals and merges all
-func CollectNodes(ii ...interface{}) (nn []Node, err error) {
+func CollectNodes(ii ...interface{}) (nn []resource.Interface, err error) {
 	for _, i := range ii {
 		switch c := i.(type) {
-		case NodeSet:
-			nn = append(nn, c...)
-		case Node:
-			nn = append(nn, c)
+		// case NodeSet:
+		// 	nn = append(nn, c...)
+		// case Node:
+		// 	nn = append(nn, c)
 
 		case Marshaller:
 			if tmp, err := c.MarshalEnvoy(); err != nil {
