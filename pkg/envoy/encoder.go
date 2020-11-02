d928063a591357b70c748823f1f346962ce4ea10
Tomaž Jerman <tomaz.jerman121@gmail.com>
2020-11-02 15:00:55 +0100

    Tweak encoder, marshler so it doesn't fail to compile

--- pkg/envoy/encoder.go
+++ pkg/envoy/encoder.go
@@ -6,41 +6,26 @@ import (
 
 type (
 	Encoder interface {
-		Encode(context.Context, ...Node) error
+		Encode(ctx context.Context, ss ...*ExecState) error
 	}
 
 	Provider interface {
-		Next(context.Context) (node Node, parentNodes []Node, childNodes []Node, err error)
+		Next(ctx context.Context) (*ExecState, error)
 	}
 )
 
 func Encode(ctx context.Context, p Provider, ee ...Encoder) error {
 	for {
-		node, pp, _, err := p.Next(ctx)
-
-		// If both node AND error are provided, that means its a dep. conflict
-		if node != nil && err != nil {
-			// @todo handle dep. conflicts
-			continue
-		} else if err != nil {
+		state, err := p.Next(ctx)
+		if err != nil {
 			return err
-		}
-
-		if node == nil {
-			// No more nodes...
+		} else if state == nil {
 			return nil
 		}
 
-		// Upgradable nodes need to be processed based on their parent nodes
-		if un, is := node.(NodeUpdater); is {
-			un.Update(pp...)
-		}
-
 		for _, e := range ee {
-			err = e.Encode(ctx, node)
-			if err != nil {
-				return err
-			}
+			// Any dep conflicts should be handled by the Encoder
+			err = e.Encode(ctx, state)
 		}
 	}
 }
