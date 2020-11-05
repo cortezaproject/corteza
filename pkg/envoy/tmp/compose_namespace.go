eb8057517655e51f61ad8ab6b023820094826e1d
Tomaž Jerman <tomaz.jerman121@gmail.com>
2020-11-05 20:21:11 +0100

    Cleanup store encoder

--- pkg/envoy/tmp/compose_namespace.go
+++ pkg/envoy/tmp/compose_namespace.go
@@ -6,9 +6,75 @@ import (
 	"time"
 
 	"github.com/cortezaproject/corteza-server/compose/types"
+	"github.com/cortezaproject/corteza-server/pkg/envoy"
 	"github.com/cortezaproject/corteza-server/pkg/envoy/resource"
 	"github.com/cortezaproject/corteza-server/store"
 )
+
+type (
+	composeNamespacePreproc struct {
+		es *encoderState
+		s  store.Storer
+	}
+)
+
+func NewComposeNamespacePreproc(es *encoderState, s store.Storer) envoy.Processor {
+	return &composeNamespacePreproc{
+		es: es,
+		s:  s,
+	}
+}
+
+func (p *composeNamespacePreproc) Process(ctx context.Context, state *envoy.ExecState) error {
+	res, is := state.Res.(*resource.ComposeNamespace)
+	if !is {
+		return nil
+	}
+
+	// Check if the given namespace exists
+	// Namespaces are top-level resources, so they don't depend on anything;
+	// no other checks are needed
+
+	ns, err := loadComposeNamespace(ctx, p.s, namespaceFilterFromGeneric(GenericFilter(res.Identifiers())))
+	if err != nil {
+		return err
+	}
+
+	if ns != nil {
+		p.es.SetExists(res)
+		p.es.Set(res, res.ResourceType(), ns.ID, res.Identifiers().StringSlice()...)
+	}
+
+	return nil
+}
+
+func encodeComposeNamespace(ctx context.Context, ectx *encodingContext, s store.Storer, state resRefs, res *resource.ComposeNamespace) (resRefs, error) {
+	var (
+		ns     = res.Res
+		rState = make(resRefs)
+	)
+
+	ns.ID = state.Get(res)
+	if ns.ID <= 0 {
+		ns.ID = nextID()
+	}
+	if ns.CreatedAt.IsZero() {
+		ns.CreatedAt = time.Now()
+	}
+
+	rState.Set(resource.COMPOSE_NAMESPACE_RESOURCE_TYPE, ns.ID, res.Identifiers().StringSlice()...)
+
+	if !ectx.partial && !ectx.exists {
+		err := store.CreateComposeNamespace(ctx, s, ns)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return rState, nil
+}
+
+// Utils
 
 func namespaceFilterFromGeneric(gf genericFilter) types.NamespaceFilter {
 	f := types.NamespaceFilter{
@@ -22,22 +88,29 @@ func namespaceFilterFromGeneric(gf genericFilter) types.NamespaceFilter {
 	return f
 }
 
-func encodeComposeNamespace(ctx context.Context, s store.Storer, ns *resource.ComposeNamespace) (uint64, error) {
-	var (
-		res = ns.Res
-	)
+func findNamespace(ctx context.Context, s store.Storer, rr []resource.Interface, ii resource.Identifiers) (ns *types.Namespace, err error) {
+	// Try to find it in the parent resources
+	var nsRes *resource.ComposeNamespace
+	walkResources(rr, func(r resource.Interface) error {
+		nsR, ok := r.(*resource.ComposeNamespace)
+		if !ok {
+			return nil
+		}
 
-	res.ID = nextID()
-	if res.CreatedAt.IsZero() {
-		res.CreatedAt = time.Now()
+		if nsR.Identifiers().HasAny(r.Identifiers().StringSlice()...) {
+			nsRes = nsR
+		}
+		return nil
+	})
+
+	// Found it
+	if nsRes != nil {
+		return nsRes.Res, nil
 	}
 
-	// err := store.CreateComposeNamespace(ctx, s, res)
-	// if err != nil {
-	// 	return nil, err
-	// }
-
-	return res.ID, nil
+	// Go in the store
+	f := namespaceFilterFromGeneric(GenericFilter(ii))
+	return loadComposeNamespace(ctx, s, f)
 }
 
 func loadComposeNamespace(ctx context.Context, s store.Storer, f types.NamespaceFilter) (*types.Namespace, error) {
