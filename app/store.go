1978156cdae2b752ae323585304c21fd77f22d58
Denis Arh <denis.arh@gmail.com>
2020-11-08 18:23:54 +0100

    Enable sqlite3 RDBMS store backaned

--- app/store.go
+++ app/store.go
@@ -1,9 +1,8 @@
 package app
 
 // Registers all supported store backends
-//
-// SQLite is intentionally ignored here
 import (
 	_ "github.com/cortezaproject/corteza-server/store/mysql"
 	_ "github.com/cortezaproject/corteza-server/store/postgres"
+	_ "github.com/cortezaproject/corteza-server/store/sqlite3"
 )
