d95f7fdb3bde3fe0a7037ff6956c1b73fe394035
Tomaž Jerman <tomaz.jerman121@gmail.com>
2020-11-17 15:38:37 +0100

    Define settings yaml decoder

--- pkg/envoy/yaml/settings.go
+++ pkg/envoy/yaml/settings.go
@@ -1,26 +1,15 @@
 package yaml
 
+import (
+	"github.com/cortezaproject/corteza-server/pkg/envoy/resource"
+)
+
 type (
 	settings map[string]interface{}
 )
 
-//func (wrap settings) MarshalEnvoy() ([]envoy.Node, error) {
-//	nn := make([]envoy.Node, 0, 1+len(wrap.modules))
-//	nn = append(nn, &envoy.SettingsNode{Ns: wrap.res})
-//
-//	if tmp, err := wrap.modules.MarshalEnvoy(); err != nil {
-//		return nil, err
-//	} else {
-//		nn = append(nn, tmp...)
-//	}
-//
-//	// @todo rbac
-//
-//	//if tmp, err := wrap.rules.MarshalEnvoy(); err != nil {
-//	//	return nil, err
-//	//} else {
-//	//	nn = append(nn, tmp...)
-//	//}
-//
-//	return nn, nil
-//}
+func (wrap settings) MarshalEnvoy() (nn []resource.Interface, err error) {
+	n := resource.NewSettings(wrap)
+
+	return []resource.Interface{n}, nil
+}
