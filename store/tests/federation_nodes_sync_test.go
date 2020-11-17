cbf28bba8606b79fb0cdd4bbb6e47734fa296ec1
Peter Grlica <peter.grlica@gmail.com>
2020-11-17 14:33:12 +0100

    Nodes sync test file, fixed node pair tests

---
+++ store/tests/federation_nodes_sync_test.go
@@ -0,0 +1,11 @@
+package tests
+
+import (
+	"testing"
+
+	"github.com/cortezaproject/corteza-server/store"
+)
+
+func testFederationNodesSync(t *testing.T, s store.FederationNodesSyncs) {
+	// @todo
+}
