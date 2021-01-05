Rushit/corteza-server

Workflow foundation

pkg/workflow/activity_test.go
@@ -1,0 +1,49 @@
+package workflow
+
+import (
+	"context"
+	"github.com/cortezaproject/corteza-server/pkg/expr"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestActivities_NoArgs(t *testing.T) {
+	var (
+		req = require.New(t)
+	)
+
+	{
+		a := Activity(
+			func(ctx context.Context, variables Variables) (Variables, error) {
+				return Variables{"foo": "42"}, nil
+			},
+			nil,
+			nil,
+		)
+
+		r, err := a.Exec(context.Background(), &ExecRequest{})
+		req.NoError(err)
+		req.IsType(Variables{}, r)
+		req.Empty(r)
+	}
+
+	{
+		results := Expressions(expr.Parser())
+		results.Set("bar", "foo")
+
+		a := Activity(
+			func(ctx context.Context, variables Variables) (Variables, error) {
+				return Variables{"foo": 42}, nil
+			},
+			nil,
+			results,
+		)
+
+		r, err := a.Exec(context.Background(), &ExecRequest{})
+		req.NoError(err)
+		req.NotNil(r)
+		req.Contains(r, "bar")
+		req.NotContains(r, "foo")
+		req.Equal(42, r.(Variables)["bar"])
+	}
+}

pkg/workflow/expressions_test.go
@@ -1,0 +1,26 @@
+package workflow
+
+import (
+	"context"
+	"github.com/cortezaproject/corteza-server/pkg/expr"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestExpressions(t *testing.T) {
+	var (
+		req = require.New(t)
+		ee  = Expressions(expr.Parser())
+	)
+
+	req.NoError(ee.Set("foo", `40`))
+	req.NoError(ee.Set("bar", `foo + 2`))
+
+	r, err := ee.Exec(context.Background(), &ExecRequest{})
+	req.NoError(err)
+	req.NotNil(r)
+	req.Contains(r, "foo")
+	req.Contains(r, "bar")
+	req.Equal(40, r.(Variables).Int("foo", 0))
+	req.Equal(42, r.(Variables).Int("bar", 0))
+}

pkg/workflow/gateways.go
@@ -1,0 +1,175 @@
+package workflow
+
+import (
+	"context"
+	"fmt"
+	"github.com/PaesslerAG/gval"
+	"sync"
+)
+
+// gatewayPath structure is used by subset of gateway nodes
+//
+// It allows to evaluate test expression to help determine the
+// gateway if a certain path should be used or not
+type (
+	GatewayPaths []*gatewayPath
+	gatewayPath  struct {
+		eval gval.Evaluable
+		to   Step
+	}
+)
+
+// GatewayPath validates expression and returns initialized gatewayPath
+func GatewayPath(s Step, expr string) (gwp *gatewayPath, err error) {
+	gwp = &gatewayPath{to: s}
+
+	if len(expr) > 0 {
+		if gwp.eval, err = gval.Full().NewEvaluable(expr); err != nil {
+			return nil, fmt.Errorf("can not parse gateway test expression %s: %w", expr, err)
+		}
+	}
+
+	return
+}
+
+// joinGateway handles merging/joining of multiple paths into
+// a single path forward
+type joinGateway struct {
+	paths  Steps
+	scopes map[Step]Variables
+	l      sync.Mutex
+}
+
+// JoinGateway fn initializes join gateway with all paths that are expected to be joined
+func JoinGateway(ss ...Step) *joinGateway {
+	return &joinGateway{
+		paths:  ss,
+		scopes: make(map[Step]Variables),
+	}
+}
+
+// Exec fn on join gateway can be called multiple times, even multiple times caller the same caller
+//
+// Func will override the collected caller's Variables.
+//
+// Join gateways is ready to continue with the workflow when all configured paths are ready to be joined
+// When all paths are merged (ie Exec was called at least once per caller)
+func (gw *joinGateway) Exec(_ context.Context, r *ExecRequest) (ExecResponse, error) {
+	gw.l.Lock()
+	defer gw.l.Unlock()
+
+	if !gw.paths.Contains(r.Caller) {
+		return nil, fmt.Errorf("unknown caller for join gateway")
+	}
+
+	gw.scopes[r.Caller] = r.Scope
+	if len(gw.scopes) < len(gw.paths) {
+		return &Joined{}, nil
+	}
+
+	// All collected, merge scope caller all paths in the defined order
+	var merged = Variables{}
+	for _, p := range gw.paths {
+		merged = merged.Merge(gw.scopes[p])
+	}
+
+	return merged, nil
+}
+
+// forkGateway handles forking to multiple paths
+type forkGateway struct{}
+
+// ForkGateway fn initializes fork gateway
+// No arguments are required; workflow graph config is used to
+// determine all possible fork paths on the fly
+func ForkGateway() *forkGateway {
+	return &forkGateway{}
+}
+
+// Exec fn on fork gateway always returns empty Steps slice
+// This signals workflow executor to collect child nodes directly caller workflow
+func (gw forkGateway) Exec(context.Context, *ExecRequest) (ExecResponse, error) {
+	return Steps{}, nil
+}
+
+// inclGateway is an inclusive gateway that can return one or more paths
+type inclGateway struct {
+	paths []*gatewayPath
+}
+
+// InclGateway fn initializes inclusive gateway
+func InclGateway(pp ...*gatewayPath) (*inclGateway, error) {
+	if len(pp) < 2 {
+		return nil, fmt.Errorf("expecting at least two paths for incusive gateway")
+	}
+
+	for _, p := range pp {
+		if p.eval == nil {
+			return nil, fmt.Errorf("all inclusve gateway paths must have valid test expression")
+		}
+	}
+
+	return &inclGateway{paths: pp}, nil
+}
+
+// Exec fn on inclGateway uses current scope to test all configured paths
+//
+// One or more matched paths can be returned!
+func (gw inclGateway) Exec(ctx context.Context, r *ExecRequest) (ExecResponse, error) {
+
+	var paths Steps
+	for _, p := range gw.paths {
+		if result, err := p.eval.EvalBool(ctx, r.Scope); err != nil {
+			return nil, err
+		} else if result {
+			paths = append(paths, p.to)
+		}
+	}
+
+	if len(paths) == 0 {
+		return nil, fmt.Errorf("inclusive gateway must match at least one condition")
+	}
+
+	return paths, nil
+}
+
+// exclGateway is an exclusive gateway that can return exactly one path
+type exclGateway struct {
+	paths []*gatewayPath
+}
+
+// ExclGateway fn initializes exclusive gateway
+func ExclGateway(pp ...*gatewayPath) (*exclGateway, error) {
+	t := len(pp)
+	if t < 2 {
+		return nil, fmt.Errorf("expecting at least two paths for exclusive gateway")
+	}
+
+	for i, p := range pp {
+		if p.eval == nil && i != t-1 {
+			return nil, fmt.Errorf("all exclusive gateway paths must have valid test expression")
+		}
+	}
+
+	return &exclGateway{paths: pp}, nil
+}
+
+// Exec fn on exclGateway uses current scope to test all configured paths
+//
+// Exactly one matched path can be returned.
+func (gw exclGateway) Exec(ctx context.Context, r *ExecRequest) (ExecResponse, error) {
+	for _, p := range gw.paths {
+		if p.eval == nil {
+			// empty & last; treat it as else part of the if condition
+			return p.to, nil
+		}
+
+		if result, err := p.eval.EvalBool(ctx, r.Scope); err != nil {
+			return nil, err
+		} else if result {
+			return p.to, nil
+		}
+	}
+
+	return nil, fmt.Errorf("exclusive gateway must match one condition")
+}

pkg/workflow/gateways_test.go
@@ -1,0 +1,123 @@
+package workflow
+
+import (
+	"context"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+type (
+	gwTestStep struct{}
+)
+
+func (*gwTestStep) Exec(context.Context, *ExecRequest) (ExecResponse, error) {
+	return nil, nil
+}
+
+func TestGatewayPath(t *testing.T) {
+	var (
+		req = require.New(t)
+		gwp *gatewayPath
+		err error
+	)
+
+	gwp, err = GatewayPath(nil, "")
+	req.NoError(err)
+	req.NotNil(gwp)
+
+	gwp, err = GatewayPath(nil, "a > 1")
+	req.NoError(err)
+	req.NotNil(gwp)
+
+	gwp, err = GatewayPath(nil, "<>")
+	req.Error(err)
+}
+
+func TestJoinGateway(t *testing.T) {
+	var (
+		req        = require.New(t)
+		p1, p2, p3 = &wfTestStep{name: "p1"}, &wfTestStep{name: "p2"}, &wfTestStep{name: "p3"}
+		gw         = JoinGateway(p1, p2, p3)
+
+		r   ExecResponse
+		err error
+	)
+
+	r, err = gw.Exec(nil, &ExecRequest{Caller: p1})
+	req.NoError(err)
+	req.Equal(&Joined{}, r)
+
+	r, err = gw.Exec(nil, &ExecRequest{Caller: p2})
+	req.NoError(err)
+	req.Equal(&Joined{}, r)
+
+	r, err = gw.Exec(nil, &ExecRequest{Caller: p3})
+	req.NoError(err)
+	req.IsType(Variables{}, r)
+}
+
+func TestForkGateway(t *testing.T) {
+	var (
+		req = require.New(t)
+		gw  = ForkGateway()
+	)
+
+	r, err := gw.Exec(nil, nil)
+	req.NoError(err)
+	req.Equal(Steps{}, r)
+	req.Empty(r)
+}
+
+func TestInclGateway(t *testing.T) {
+	var (
+		req = require.New(t)
+
+		s1, s2, s3 = &wfTestStep{name: "s1"}, &wfTestStep{name: "s2"}, &wfTestStep{name: "s3"}
+		gwp1, _    = GatewayPath(s1, "a > 10")
+		gwp2, _    = GatewayPath(s2, "a > 5")
+		gwp3, _    = GatewayPath(s3, "a > 0")
+
+		gw, err = InclGateway(gwp1, gwp2, gwp3)
+	)
+
+	r, err := gw.Exec(context.Background(), &ExecRequest{Scope: Variables{"a": 11}})
+	req.NoError(err)
+	req.Equal(Steps{s1, s2, s3}, r)
+
+	r, err = gw.Exec(context.Background(), &ExecRequest{Scope: Variables{"a": 6}})
+	req.NoError(err)
+	req.Equal(Steps{s2, s3}, r)
+
+	r, err = gw.Exec(context.Background(), &ExecRequest{Scope: Variables{"a": 1}})
+	req.NoError(err)
+	req.Equal(Steps{s3}, r)
+
+	r, err = gw.Exec(context.Background(), &ExecRequest{Scope: Variables{"a": 0}})
+	req.Error(err)
+	req.Nil(r)
+}
+
+func TestExclGateway(t *testing.T) {
+	var (
+		req = require.New(t)
+
+		s1, s2, s3 = &wfTestStep{name: "s1"}, &wfTestStep{name: "s2"}, &wfTestStep{name: "s3"}
+		gwp1, _    = GatewayPath(s1, "a > 10")
+		gwp2, _    = GatewayPath(s2, "a > 5")
+		gwp3, _    = GatewayPath(s3, "")
+
+		gw, err = ExclGateway(gwp1, gwp2, gwp3)
+	)
+
+	r, err := gw.Exec(context.Background(), &ExecRequest{Scope: Variables{"a": 11}})
+	req.NoError(err)
+	req.Equal(s1, r)
+
+	r, err = gw.Exec(context.Background(), &ExecRequest{Scope: Variables{"a": 6}})
+	req.NoError(err)
+	req.Equal(s2, r)
+
+	r, err = gw.Exec(context.Background(), &ExecRequest{Scope: Variables{"a": 1}})
+	req.NoError(err)
+	req.Equal(s3, r)
+}

pkg/workflow/session_test.go
@@ -1,0 +1,168 @@
+package workflow
+
+import (
+	"context"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/atomic"
+	"testing"
+	"time"
+)
+
+type (
+	sesTestStep struct {
+		name string
+		exec func(context.Context, *ExecRequest) (ExecResponse, error)
+	}
+
+	sesTestTemporal struct {
+		delay time.Duration
+		until time.Time
+	}
+)
+
+var (
+	// used for testing to produce lower numbers that are easier to inspect and compare
+	testID = atomic.NewUint64(0)
+)
+
+func init() {
+	nextID = func() uint64 {
+		return testID.Inc()
+	}
+}
+
+func (s *sesTestStep) Exec(ctx context.Context, r *ExecRequest) (ExecResponse, error) {
+	if s.exec != nil {
+		return s.exec(ctx, r)
+	}
+
+	return Variables{
+		"counter": r.Scope.Int("counter", 0) + 1,
+		"path":    r.Scope.String("path", "") + "/" + s.name,
+		s.name:    "executed",
+	}, nil
+}
+
+func (s *sesTestTemporal) Exec(ctx context.Context, r *ExecRequest) (ExecResponse, error) {
+	if s.until.IsZero() {
+		s.until = time.Now().Add(s.delay)
+	}
+
+	if s.until.After(time.Now()) {
+		return DelayExecution(s.until), nil
+	}
+
+	return Variables{
+		"waitForMoment": "executed",
+	}, nil
+}
+
+func TestSession_TwoStepWorkflow(t *testing.T) {
+	var (
+		ctx = context.Background()
+		req = require.New(t)
+		wf  = Workflow()
+		ses = Session(ctx, wf)
+
+		s1 = &sesTestStep{name: "s1"}
+		s2 = &sesTestStep{name: "s2"}
+	)
+
+	wf.AddStep(s1, s2) // 1st execute s1 then s2
+	ses.Exec(ctx, s1, Variables{"two": 1, "three": 1})
+	ses.Wait(ctx)
+	req.NoError(ses.FinalError())
+	req.NotNil(ses.Result())
+	req.Equal("/s1/s2", ses.Result().String("path", ""))
+}
+
+func TestSession_SplitAndMerge(t *testing.T) {
+	var (
+		ctx = context.Background()
+		req = require.New(t)
+		wf  = Workflow()
+		ses = Session(ctx, wf)
+
+		start  = &sesTestStep{name: "start"}
+		split1 = &sesTestStep{name: "split1"}
+		split2 = &sesTestStep{name: "split2"}
+		split3 = &sesTestStep{name: "split3"}
+
+		end = JoinGateway(split1, split2, split3)
+	)
+
+	wf.AddStep(start, split1, split2, split3)
+	wf.AddStep(split1, end)
+	wf.AddStep(split2, end)
+	wf.AddStep(split3, end)
+	ses.Exec(ctx, start, nil)
+	ses.Wait(ctx)
+	req.True(ses.Idle())
+	req.NoError(ses.FinalError())
+	req.NotNil(ses.Result())
+	// split3 only!
+	req.Equal("/start/split3", ses.Result().String("path", ""))
+	req.Contains(ses.Result(), "split1")
+	req.Contains(ses.Result(), "split2")
+	req.Contains(ses.Result(), "split3")
+}
+
+func TestSession_Delays(t *testing.T) {
+	var (
+		// how fast we want to go (lower = faster)
+		//
+		unit = 500 * time.Microsecond
+
+		ctx = context.Background()
+		req = require.New(t)
+		wf  = Workflow()
+		ses = Session(ctx, wf,
+			// for testing we need much shorter worker intervals
+			SetWorkerInterval(unit),
+		)
+
+		start = &sesTestStep{name: "start"}
+
+		waitForInputStateId atomic.Uint64
+		waitForInput        = &sesTestStep{name: "waitForInput", exec: func(ctx context.Context, r *ExecRequest) (ExecResponse, error) {
+			if !r.Scope.Has("input") {
+				waitForInputStateId.Store(r.StateID)
+				return WaitForInput(), nil
+			}
+
+			return Variables{
+				"input":        r.Scope["input"],
+				"waitForInput": "executed",
+			}, nil
+		}}
+		waitForMoment = &sesTestTemporal{delay: 3 * unit}
+	)
+
+	wf.AddStep(start, waitForMoment)
+	wf.AddStep(waitForMoment, waitForInput)
+
+	req.NoError(ses.Exec(ctx, start, nil))
+
+	// wait-for-moment step needs to be executed before we can resume wait-for-input
+	time.Sleep(10 * unit)
+	req.NotZero(waitForInputStateId.Load())
+
+	// should not be completed yet...
+	req.True(ses.Idle())
+	req.True(ses.Suspended())
+
+	// push in the input
+	req.NoError(ses.Resume(ctx, waitForInputStateId.Load(), Variables{"input": "foo"}))
+
+	req.False(ses.Suspended())
+	ses.Wait(ctx)
+	time.Sleep(10 * unit)
+
+	// should not be completed yet...
+	req.True(ses.Idle())
+	req.NoError(ses.FinalError())
+	req.NotNil(ses.Result())
+	req.Contains(ses.Result(), "waitForMoment")
+	req.Contains(ses.Result(), "waitForInput")
+	req.Equal("foo", ses.Result().String("input", ""))
+}

cortezaproject/corteza

pkg/workflow/activity_test.go
@@ -1,0 +1,49 @@
+package workflow
+
+import (
+	"context"
+	"github.com/cortezaproject/corteza-server/pkg/expr"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestActivities_NoArgs(t *testing.T) {
+	var (
+		req = require.New(t)
+	)
+
+	{
+		a := Activity(
+			func(ctx context.Context, variables Variables) (Variables, error) {
+				return Variables{"foo": "42"}, nil
+			},
+			nil,
+			nil,
+		)
+
+		r, err := a.Exec(context.Background(), &ExecRequest{})
+		req.NoError(err)
+		req.IsType(Variables{}, r)
+		req.Empty(r)
+	}
+
+	{
+		results := NewExpressions(expr.Parser())
+		results.Set("bar", "foo")
+
+		a := Activity(
+			func(ctx context.Context, variables Variables) (Variables, error) {
+				return Variables{"foo": 42}, nil
+			},
+			nil,
+			results,
+		)
+
+		r, err := a.Exec(context.Background(), &ExecRequest{})
+		req.NoError(err)
+		req.NotNil(r)
+		req.Contains(r, "bar")
+		req.NotContains(r, "foo")
+		req.Equal(42, r.(Variables)["bar"])
+	}
+}

pkg/workflow/expressions_test.go
@@ -1,0 +1,26 @@
+package workflow
+
+import (
+	"context"
+	"github.com/cortezaproject/corteza-server/pkg/expr"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestExpressions(t *testing.T) {
+	var (
+		req = require.New(t)
+		ee  = NewExpressions(expr.Parser())
+	)
+
+	req.NoError(ee.Set("foo", `40`))
+	req.NoError(ee.Set("bar", `foo + 2`))
+
+	r, err := ee.Exec(context.Background(), &ExecRequest{})
+	req.NoError(err)
+	req.NotNil(r)
+	req.Contains(r, "foo")
+	req.Contains(r, "bar")
+	req.Equal(40, r.(Variables).Int("foo", 0))
+	req.Equal(42, r.(Variables).Int("bar", 0))
+}

pkg/workflow/gateways.go
@@ -1,0 +1,175 @@
+package workflow
+
+import (
+	"context"
+	"fmt"
+	"github.com/PaesslerAG/gval"
+	"sync"
+)
+
+// GatewayPath structure is used by subset of gateway nodes
+//
+// It allows to evaluate test Expression to help determine the
+// gateway if a certain path should be used or not
+type (
+	GatewayPaths []*GatewayPath
+	GatewayPath  struct {
+		eval gval.Evaluable
+		to   Step
+	}
+)
+
+// NewGatewayPath validates Expression and returns initialized GatewayPath
+func NewGatewayPath(s Step, expr string) (gwp *GatewayPath, err error) {
+	gwp = &GatewayPath{to: s}
+
+	if len(expr) > 0 {
+		if gwp.eval, err = gval.Full().NewEvaluable(expr); err != nil {
+			return nil, fmt.Errorf("can not parse gateway test Expression %s: %w", expr, err)
+		}
+	}
+
+	return
+}
+
+// joinGateway handles merging/joining of multiple paths into
+// a single path forward
+type joinGateway struct {
+	paths  Steps
+	scopes map[Step]Variables
+	l      sync.Mutex
+}
+
+// JoinGateway fn initializes join gateway with all paths that are expected to be joined
+func JoinGateway(ss ...Step) *joinGateway {
+	return &joinGateway{
+		paths:  ss,
+		scopes: make(map[Step]Variables),
+	}
+}
+
+// Exec fn on join gateway can be called multiple times, even multiple times caller the same caller
+//
+// Func will override the collected caller's Variables.
+//
+// Join gateways is ready to continue with the Graph when all configured paths are ready to be joined
+// When all paths are merged (ie Exec was called at least once per caller)
+func (gw *joinGateway) Exec(_ context.Context, r *ExecRequest) (ExecResponse, error) {
+	gw.l.Lock()
+	defer gw.l.Unlock()
+
+	if !gw.paths.Contains(r.Caller) {
+		return nil, fmt.Errorf("unknown caller for join gateway")
+	}
+
+	gw.scopes[r.Caller] = r.Scope
+	if len(gw.scopes) < len(gw.paths) {
+		return &Joined{}, nil
+	}
+
+	// All collected, merge scope caller all paths in the defined order
+	var merged = Variables{}
+	for _, p := range gw.paths {
+		merged = merged.Merge(gw.scopes[p])
+	}
+
+	return merged, nil
+}
+
+// forkGateway handles forking to multiple paths
+type forkGateway struct{}
+
+// ForkGateway fn initializes fork gateway
+// No arguments are required; Graph Graph config is used to
+// determine all possible fork paths on the fly
+func ForkGateway() *forkGateway {
+	return &forkGateway{}
+}
+
+// Exec fn on fork gateway always returns empty Steps slice
+// This signals Graph executor to collect child nodes directly caller Graph
+func (gw forkGateway) Exec(context.Context, *ExecRequest) (ExecResponse, error) {
+	return Steps{}, nil
+}
+
+// inclGateway is an inclusive gateway that can return one or more paths
+type inclGateway struct {
+	paths []*GatewayPath
+}
+
+// InclGateway fn initializes inclusive gateway
+func InclGateway(pp ...*GatewayPath) (*inclGateway, error) {
+	if len(pp) < 2 {
+		return nil, fmt.Errorf("expecting at least two paths for incusive gateway")
+	}
+
+	for _, p := range pp {
+		if p.eval == nil {
+			return nil, fmt.Errorf("all inclusve gateway paths must have valid test Expression")
+		}
+	}
+
+	return &inclGateway{paths: pp}, nil
+}
+
+// Exec fn on inclGateway uses current scope to test all configured paths
+//
+// One or more matched paths can be returned!
+func (gw inclGateway) Exec(ctx context.Context, r *ExecRequest) (ExecResponse, error) {
+
+	var paths Steps
+	for _, p := range gw.paths {
+		if result, err := p.eval.EvalBool(ctx, r.Scope); err != nil {
+			return nil, err
+		} else if result {
+			paths = append(paths, p.to)
+		}
+	}
+
+	if len(paths) == 0 {
+		return nil, fmt.Errorf("inclusive gateway must match at least one condition")
+	}
+
+	return paths, nil
+}
+
+// exclGateway is an exclusive gateway that can return exactly one path
+type exclGateway struct {
+	paths []*GatewayPath
+}
+
+// ExclGateway fn initializes exclusive gateway
+func ExclGateway(pp ...*GatewayPath) (*exclGateway, error) {
+	t := len(pp)
+	if t < 2 {
+		return nil, fmt.Errorf("expecting at least two paths for exclusive gateway")
+	}
+
+	for i, p := range pp {
+		if p.eval == nil && i != t-1 {
+			return nil, fmt.Errorf("all exclusive gateway paths must have valid test Expression")
+		}
+	}
+
+	return &exclGateway{paths: pp}, nil
+}
+
+// Exec fn on exclGateway uses current scope to test all configured paths
+//
+// Exactly one matched path can be returned.
+func (gw exclGateway) Exec(ctx context.Context, r *ExecRequest) (ExecResponse, error) {
+	for _, p := range gw.paths {
+		if p.eval == nil {
+			// empty & last; treat it as else part of the if condition
+			return p.to, nil
+		}
+
+		if result, err := p.eval.EvalBool(ctx, r.Scope); err != nil {
+			return nil, err
+		} else if result {
+			return p.to, nil
+		}
+	}
+
+	return nil, fmt.Errorf("exclusive gateway must match one condition")
+}

pkg/workflow/gateways_test.go
@@ -1,0 +1,123 @@
+package workflow
+
+import (
+	"context"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+type (
+	gwTestStep struct{}
+)
+
+func (*gwTestStep) Exec(context.Context, *ExecRequest) (ExecResponse, error) {
+	return nil, nil
+}
+
+func TestGatewayPath(t *testing.T) {
+	var (
+		req = require.New(t)
+		gwp *GatewayPath
+		err error
+	)
+
+	gwp, err = NewGatewayPath(nil, "")
+	req.NoError(err)
+	req.NotNil(gwp)
+
+	gwp, err = NewGatewayPath(nil, "a > 1")
+	req.NoError(err)
+	req.NotNil(gwp)
+
+	gwp, err = NewGatewayPath(nil, "<>")
+	req.Error(err)
+}
+
+func TestJoinGateway(t *testing.T) {
+	var (
+		req        = require.New(t)
+		p1, p2, p3 = &wfTestStep{name: "p1"}, &wfTestStep{name: "p2"}, &wfTestStep{name: "p3"}
+		gw         = JoinGateway(p1, p2, p3)
+
+		r   ExecResponse
+		err error
+	)
+
+	r, err = gw.Exec(nil, &ExecRequest{Caller: p1})
+	req.NoError(err)
+	req.Equal(&Joined{}, r)
+
+	r, err = gw.Exec(nil, &ExecRequest{Caller: p2})
+	req.NoError(err)
+	req.Equal(&Joined{}, r)
+
+	r, err = gw.Exec(nil, &ExecRequest{Caller: p3})
+	req.NoError(err)
+	req.IsType(Variables{}, r)
+}
+
+func TestForkGateway(t *testing.T) {
+	var (
+		req = require.New(t)
+		gw  = ForkGateway()
+	)
+
+	r, err := gw.Exec(nil, nil)
+	req.NoError(err)
+	req.Equal(Steps{}, r)
+	req.Empty(r)
+}
+
+func TestInclGateway(t *testing.T) {
+	var (
+		req = require.New(t)
+
+		s1, s2, s3 = &wfTestStep{name: "s1"}, &wfTestStep{name: "s2"}, &wfTestStep{name: "s3"}
+		gwp1, _    = NewGatewayPath(s1, "a > 10")
+		gwp2, _    = NewGatewayPath(s2, "a > 5")
+		gwp3, _    = NewGatewayPath(s3, "a > 0")
+
+		gw, err = InclGateway(gwp1, gwp2, gwp3)
+	)
+
+	r, err := gw.Exec(context.Background(), &ExecRequest{Scope: Variables{"a": 11}})
+	req.NoError(err)
+	req.Equal(Steps{s1, s2, s3}, r)
+
+	r, err = gw.Exec(context.Background(), &ExecRequest{Scope: Variables{"a": 6}})
+	req.NoError(err)
+	req.Equal(Steps{s2, s3}, r)
+
+	r, err = gw.Exec(context.Background(), &ExecRequest{Scope: Variables{"a": 1}})
+	req.NoError(err)
+	req.Equal(Steps{s3}, r)
+
+	r, err = gw.Exec(context.Background(), &ExecRequest{Scope: Variables{"a": 0}})
+	req.Error(err)
+	req.Nil(r)
+}
+
+func TestExclGateway(t *testing.T) {
+	var (
+		req = require.New(t)
+
+		s1, s2, s3 = &wfTestStep{name: "s1"}, &wfTestStep{name: "s2"}, &wfTestStep{name: "s3"}
+		gwp1, _    = NewGatewayPath(s1, "a > 10")
+		gwp2, _    = NewGatewayPath(s2, "a > 5")
+		gwp3, _    = NewGatewayPath(s3, "")
+
+		gw, err = ExclGateway(gwp1, gwp2, gwp3)
+	)
+
+	r, err := gw.Exec(context.Background(), &ExecRequest{Scope: Variables{"a": 11}})
+	req.NoError(err)
+	req.Equal(s1, r)
+
+	r, err = gw.Exec(context.Background(), &ExecRequest{Scope: Variables{"a": 6}})
+	req.NoError(err)
+	req.Equal(s2, r)
+
+	r, err = gw.Exec(context.Background(), &ExecRequest{Scope: Variables{"a": 1}})
+	req.NoError(err)
+	req.Equal(s3, r)
+}

pkg/workflow/session_test.go
@@ -1,0 +1,168 @@
+package workflow
+
+import (
+	"context"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/atomic"
+	"testing"
+	"time"
+)
+
+type (
+	sesTestStep struct {
+		name string
+		exec func(context.Context, *ExecRequest) (ExecResponse, error)
+	}
+
+	sesTestTemporal struct {
+		delay time.Duration
+		until time.Time
+	}
+)
+
+var (
+	// used for testing to produce lower numbers that are easier to inspect and compare
+	testID = atomic.NewUint64(0)
+)
+
+func init() {
+	nextID = func() uint64 {
+		return testID.Inc()
+	}
+}
+
+func (s *sesTestStep) Exec(ctx context.Context, r *ExecRequest) (ExecResponse, error) {
+	if s.exec != nil {
+		return s.exec(ctx, r)
+	}
+
+	return Variables{
+		"counter": r.Scope.Int("counter", 0) + 1,
+		"path":    r.Scope.String("path", "") + "/" + s.name,
+		s.name:    "executed",
+	}, nil
+}
+
+func (s *sesTestTemporal) Exec(ctx context.Context, r *ExecRequest) (ExecResponse, error) {
+	if s.until.IsZero() {
+		s.until = time.Now().Add(s.delay)
+	}
+
+	if s.until.After(time.Now()) {
+		return DelayExecution(s.until), nil
+	}
+
+	return Variables{
+		"waitForMoment": "executed",
+	}, nil
+}
+
+func TestSession_TwoStepWorkflow(t *testing.T) {
+	var (
+		ctx = context.Background()
+		req = require.New(t)
+		wf  = NewGraph()
+		ses = Session(ctx, wf)
+
+		s1 = &sesTestStep{name: "s1"}
+		s2 = &sesTestStep{name: "s2"}
+	)
+
+	wf.AddStep(s1, s2) // 1st execute s1 then s2
+	ses.Exec(ctx, s1, Variables{"two": 1, "three": 1})
+	ses.Wait(ctx)
+	req.NoError(ses.FinalError())
+	req.NotNil(ses.Result())
+	req.Equal("/s1/s2", ses.Result().String("path", ""))
+}
+
+func TestSession_SplitAndMerge(t *testing.T) {
+	var (
+		ctx = context.Background()
+		req = require.New(t)
+		wf  = NewGraph()
+		ses = Session(ctx, wf)
+
+		start  = &sesTestStep{name: "start"}
+		split1 = &sesTestStep{name: "split1"}
+		split2 = &sesTestStep{name: "split2"}
+		split3 = &sesTestStep{name: "split3"}
+
+		end = JoinGateway(split1, split2, split3)
+	)
+
+	wf.AddStep(start, split1, split2, split3)
+	wf.AddStep(split1, end)
+	wf.AddStep(split2, end)
+	wf.AddStep(split3, end)
+	ses.Exec(ctx, start, nil)
+	ses.Wait(ctx)
+	req.True(ses.Idle())
+	req.NoError(ses.FinalError())
+	req.NotNil(ses.Result())
+	// split3 only!
+	req.Equal("/start/split3", ses.Result().String("path", ""))
+	req.Contains(ses.Result(), "split1")
+	req.Contains(ses.Result(), "split2")
+	req.Contains(ses.Result(), "split3")
+}
+
+func TestSession_Delays(t *testing.T) {
+	var (
+		// how fast we want to go (lower = faster)
+		//
+		unit = 500 * time.Microsecond
+
+		ctx = context.Background()
+		req = require.New(t)
+		wf  = NewGraph()
+		ses = Session(ctx, wf,
+			// for testing we need much shorter worker intervals
+			SetWorkerInterval(unit),
+		)
+
+		start = &sesTestStep{name: "start"}
+
+		waitForInputStateId atomic.Uint64
+		waitForInput        = &sesTestStep{name: "waitForInput", exec: func(ctx context.Context, r *ExecRequest) (ExecResponse, error) {
+			if !r.Scope.Has("input") {
+				waitForInputStateId.Store(r.StateID)
+				return WaitForInput(), nil
+			}
+
+			return Variables{
+				"input":        r.Scope["input"],
+				"waitForInput": "executed",
+			}, nil
+		}}
+		waitForMoment = &sesTestTemporal{delay: 3 * unit}
+	)
+
+	wf.AddStep(start, waitForMoment)
+	wf.AddStep(waitForMoment, waitForInput)
+
+	req.NoError(ses.Exec(ctx, start, nil))
+
+	// wait-for-moment step needs to be executed before we can resume wait-for-input
+	time.Sleep(10 * unit)
+	req.NotZero(waitForInputStateId.Load())
+
+	// should not be completed yet...
+	req.True(ses.Idle())
+	req.True(ses.Suspended())
+
+	// push in the input
+	req.NoError(ses.Resume(ctx, waitForInputStateId.Load(), Variables{"input": "foo"}))
+
+	req.False(ses.Suspended())
+	ses.Wait(ctx)
+	time.Sleep(10 * unit)
+
+	// should not be completed yet...
+	req.True(ses.Idle())
+	req.NoError(ses.FinalError())
+	req.NotNil(ses.Result())
+	req.Contains(ses.Result(), "waitForMoment")
+	req.Contains(ses.Result(), "waitForInput")
+	req.Equal("foo", ses.Result().String("input", ""))
+}