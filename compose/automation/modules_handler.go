7ddcd402ab3e840b258a380191ae2e62af0c16e8
Denis Arh <denis.arh@gmail.com>
2021-02-01 20:18:57 +0100

    Add basic automation functions for compose

--- compose/automation/modules_handler.go
+++ compose/automation/modules_handler.go
@@ -2,157 +2,82 @@ package automation
 
 import (
 	"context"
+	"fmt"
 	"github.com/cortezaproject/corteza-server/compose/types"
 )
 
 type (
-	recordService interface {
-		FindByID(ctx context.Context, namespaceID, moduleID, recordID uint64) (*types.Record, error)
-		Find(ctx context.Context, filter types.RecordFilter) (set types.RecordSet, f types.RecordFilter, err error)
-
-		Create(ctx context.Context, record *types.Record) (*types.Record, error)
-		Update(ctx context.Context, record *types.Record) (*types.Record, error)
-		Bulk(ctx context.Context, oo ...*types.RecordBulkOperation) (types.RecordSet, error)
-
-		DeleteByID(ctx context.Context, namespaceID, moduleID uint64, recordID ...uint64) error
-	}
-
 	moduleService interface {
 		FindByID(ctx context.Context, namespaceID, moduleID uint64) (*types.Module, error)
 		FindByHandle(ctx context.Context, namespaceID uint64, handle string) (*types.Module, error)
+		Find(ctx context.Context, filter types.ModuleFilter) (set types.ModuleSet, f types.ModuleFilter, err error)
+
+		Create(ctx context.Context, module *types.Module) (*types.Module, error)
+		Update(ctx context.Context, module *types.Module) (*types.Module, error)
+
+		DeleteByID(ctx context.Context, namespaceID uint64, moduleID uint64) error
 	}
 
-	namespaceService interface {
-		FindByID(ctx context.Context, namespaceID uint64) (*types.Namespace, error)
-		FindByHandle(ctx context.Context, handle string) (*types.Namespace, error)
-	}
-
-	recordsHandlers struct {
-		reg recordsHandlerRegistry
+	modulesHandler struct {
+		reg modulesHandlerRegistry
 		ns  namespaceService
 		mod moduleService
-		rec recordService
+	}
+
+	moduleLookup interface {
+		GetModule() (bool, uint64, string, *types.Module)
 	}
 )
 
-func RecordsHandlers(reg recordsHandlerRegistry, ns namespaceService, mod moduleService, rec recordService) *recordsHandlers {
-	h := &recordsHandlers{
+func ModulesHandler(reg modulesHandlerRegistry, ns namespaceService, mod moduleService) *modulesHandler {
+	h := &modulesHandler{
 		reg: reg,
 		ns:  ns,
 		mod: mod,
-		rec: rec,
 	}
 
 	h.register()
 	return h
 }
 
-func resolveNamespace(ctx context.Context, svc namespaceService, id *uint64, handle string, res *types.Namespace) (err error) {
-	if *id == 0 {
-		if len(handle) > 0 {
-			if res, err = svc.FindByHandle(ctx, handle); err != nil {
-				return
-			}
-		}
-
-		if res != nil {
-			*id = res.ID
-		}
-	}
-
+func (h modulesHandler) lookup(ctx context.Context, args *modulesLookupArgs) (results *modulesLookupResults, err error) {
+	results = &modulesLookupResults{}
+	results.Module, err = lookupModule(ctx, h.ns, h.mod, args)
 	return
 }
 
-func resolveModule(ctx context.Context, svc moduleService, namespaceID uint64, id *uint64, handle string, res *types.Module) (err error) {
-	if *id == 0 {
-		if len(handle) > 0 {
-			if res, err = svc.FindByHandle(ctx, namespaceID, handle); err != nil {
-				return
-			}
-		}
-
-		if res != nil {
-			*id = res.ID
-		}
+func getModuleID(ctx context.Context, nsSvc namespaceService, modSvc moduleService, args moduleLookup) (namespaceID uint64, moduleID uint64, err error) {
+	namespaceID, err = getNamespaceID(ctx, nsSvc, args.(namespaceLookup))
+	if err != nil {
+		return
 	}
 
-	return
+	var mod *types.Module
+	if _, moduleID, _, _ = args.GetModule(); moduleID > 0 {
+		return
+	} else if mod, err = lookupModule(ctx, nsSvc, modSvc, args); err != nil {
+		return
+	} else {
+		return namespaceID, mod.ID, nil
+	}
 }
 
-func (h recordsHandlers) lookupByID(ctx context.Context, args *recordsLookupByIDArgs) (results *recordsLookupByIDResults, err error) {
-	results = &recordsLookupByIDResults{}
-
-	if err = resolveNamespace(ctx, h.ns, &args.namespaceID, args.namespaceHandle, args.namespaceRes); err != nil {
-		return nil, err
+func lookupModule(ctx context.Context, nsSvc namespaceService, modSvc moduleService, args moduleLookup) (*types.Module, error) {
+	namespaceID, err := getNamespaceID(ctx, nsSvc, args.(namespaceLookup))
+	if err != nil {
+		return nil, fmt.Errorf("could not load namespace: %w", err)
 	}
 
-	if err = resolveModule(ctx, h.mod, args.namespaceID, &args.moduleID, args.moduleHandle, args.moduleRes); err != nil {
-		return nil, err
+	_, ID, handle, module := args.GetModule()
+
+	switch {
+	case module != nil:
+		return module, nil
+	case ID > 0:
+		return modSvc.FindByID(ctx, namespaceID, ID)
+	case len(handle) > 0:
+		return modSvc.FindByHandle(ctx, namespaceID, handle)
 	}
 
-	results.Record, err = h.rec.FindByID(ctx, args.namespaceID, args.moduleID, args.RecordID)
-	return
-}
-
-func (h recordsHandlers) create(ctx context.Context, args *recordsCreateArgs) (results *recordsCreateResults, err error) {
-	results = &recordsCreateResults{}
-
-	if err = resolveNamespace(ctx, h.ns, &args.namespaceID, args.namespaceHandle, args.namespaceRes); err != nil {
-		return nil, err
-	}
-
-	if err = resolveModule(ctx, h.mod, args.namespaceID, &args.moduleID, args.moduleHandle, args.moduleRes); err != nil {
-		return nil, err
-	}
-
-	rec := &types.Record{
-		ModuleID:    args.moduleID,
-		NamespaceID: args.namespaceID,
-		Values:      args.Values,
-		Labels:      args.Labels,
-		OwnedBy:     args.OwnedBy,
-	}
-
-	results.Record, err = h.rec.Create(ctx, rec)
-	return
-}
-
-func (h recordsHandlers) save(ctx context.Context, args *recordsSaveArgs) (results *recordsSaveResults, err error) {
-	results = &recordsSaveResults{}
-	results.Record, err = h.rec.Update(ctx, args.Record)
-	return
-}
-
-func (h recordsHandlers) update(ctx context.Context, args *recordsUpdateArgs) (results *recordsUpdateResults, err error) {
-	results = &recordsUpdateResults{}
-	if err = resolveNamespace(ctx, h.ns, &args.namespaceID, args.namespaceHandle, args.namespaceRes); err != nil {
-		return nil, err
-	}
-
-	if err = resolveModule(ctx, h.mod, args.namespaceID, &args.moduleID, args.moduleHandle, args.moduleRes); err != nil {
-		return nil, err
-	}
-
-	rec := &types.Record{
-		ModuleID:    args.moduleID,
-		NamespaceID: args.namespaceID,
-		Values:      args.Values,
-		Labels:      args.Labels,
-		OwnedBy:     args.OwnedBy,
-	}
-
-	results.Record, err = h.rec.Update(ctx, rec)
-	return
-}
-
-func (h recordsHandlers) delete(ctx context.Context, args *recordsDeleteArgs) (err error) {
-	if err = resolveNamespace(ctx, h.ns, &args.namespaceID, args.namespaceHandle, args.namespaceRes); err != nil {
-		return err
-	}
-
-	if err = resolveModule(ctx, h.mod, args.namespaceID, &args.moduleID, args.moduleHandle, args.moduleRes); err != nil {
-		return err
-	}
-
-	return h.rec.DeleteByID(ctx, args.namespaceID, args.moduleID, args.RecordID)
+	return nil, fmt.Errorf("empty module lookup params")
 }
---
+++ compose/automation/namespaces_handler.gen.go
@@ -0,0 +1,128 @@
+package automation
+
+// This file is auto-generated.
+//
+// Changes to this file may cause incorrect behavior and will be lost if
+// the code is regenerated.
+//
+// Definitions file that controls how this file is generated:
+// compose/automation/namespaces_handler.yaml
+
+import (
+	"context"
+	atypes "github.com/cortezaproject/corteza-server/automation/types"
+	"github.com/cortezaproject/corteza-server/compose/types"
+	"github.com/cortezaproject/corteza-server/pkg/expr"
+	"github.com/cortezaproject/corteza-server/pkg/wfexec"
+)
+
+var _ wfexec.ExecResponse
+
+type (
+	namespacesHandlerRegistry interface {
+		AddFunctions(ff ...*atypes.Function)
+		Type(ref string) expr.Type
+	}
+)
+
+func (h namespacesHandler) register() {
+	h.reg.AddFunctions(
+		h.Lookup(),
+	)
+}
+
+type (
+	namespacesLookupArgs struct {
+		hasNamespace    bool
+		Namespace       interface{}
+		namespaceID     uint64
+		namespaceHandle string
+		namespaceRes    *types.Namespace
+	}
+
+	namespacesLookupResults struct {
+		Namespace *types.Namespace
+	}
+)
+
+func (a namespacesLookupArgs) GetNamespace() (bool, uint64, string, *types.Namespace) {
+	return a.hasNamespace, a.namespaceID, a.namespaceHandle, a.namespaceRes
+}
+
+// Lookup function Lookup for compose namespace by ID
+//
+// expects implementation of lookup function:
+// func (h namespacesHandler) lookup(ctx context.Context, args *namespacesLookupArgs) (results *namespacesLookupResults, err error) {
+//    return
+// }
+func (h namespacesHandler) Lookup() *atypes.Function {
+	return &atypes.Function{
+		Ref:  "composeNamespacesLookup",
+		Kind: "function",
+		Meta: &atypes.FunctionMeta{
+			Short: "Lookup for compose namespace by ID",
+		},
+
+		Parameters: []*atypes.Param{
+			{
+				Name:  "namespace",
+				Types: []string{"ID", "Handle", "ComposeNamespace"}, Required: true,
+			},
+		},
+
+		Results: []*atypes.Param{
+
+			{
+				Name:  "namespace",
+				Types: []string{"ComposeNamespace"},
+			},
+		},
+
+		Handler: func(ctx context.Context, in *expr.Vars) (out *expr.Vars, err error) {
+			var (
+				args = &namespacesLookupArgs{
+					hasNamespace: in.Has("namespace"),
+				}
+			)
+
+			if err = in.Decode(args); err != nil {
+				return
+			}
+
+			// Converting Namespace argument
+			if args.hasNamespace {
+				aux := expr.Must(expr.Select(in, "namespace"))
+				switch aux.Type() {
+				case h.reg.Type("ID").Type():
+					args.namespaceID = aux.Get().(uint64)
+				case h.reg.Type("Handle").Type():
+					args.namespaceHandle = aux.Get().(string)
+				case h.reg.Type("ComposeNamespace").Type():
+					args.namespaceRes = aux.Get().(*types.Namespace)
+				}
+			}
+
+			var results *namespacesLookupResults
+			if results, err = h.lookup(ctx, args); err != nil {
+				return
+			}
+
+			out = &expr.Vars{}
+
+			{
+				// converting results.Namespace (*types.Namespace) to ComposeNamespace
+				var (
+					tval expr.TypedValue
+				)
+
+				if tval, err = h.reg.Type("ComposeNamespace").Cast(results.Namespace); err != nil {
+					return
+				} else if err = expr.Assign(out, "namespace", tval); err != nil {
+					return
+				}
+			}
+
+			return
+		},
+	}
+}
