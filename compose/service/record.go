c14668584fe7bb7c34117b095cb4b10ebb11cf87
Tomaž Jerman <tomaz.jerman121@gmail.com>
2021-02-15 15:24:00 +0100

    Fix import failed counter lapsus

--- compose/service/record.go
+++ compose/service/record.go
@@ -376,6 +376,7 @@ func (svc record) Import(ses *recordImportSession) (err error) {
 		ses.Progress.FinishedAt = now()
 		if err != nil {
 			ses.Progress.FailReason = err.Error()
+			ses.Progress.Failed++
 			return err
 		}
 
