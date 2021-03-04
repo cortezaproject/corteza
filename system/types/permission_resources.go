4aef3ae5217709748694b1621cd3405c25b54f5f
Denis Arh <denis.arh@gmail.com>
2021-03-04 08:06:08 +0100

    Remove org resource RBAC type

--- system/types/permission_resources.go
+++ system/types/permission_resources.go
@@ -7,7 +7,6 @@ import (
 const SystemRBACResource = rbac.Resource("system")
 const ApplicationRBACResource = rbac.Resource("system:application:")
 const TemplateRBACResource = rbac.Resource("system:template:")
-const OrganisationRBACResource = rbac.Resource("system:organisation:")
 const UserRBACResource = rbac.Resource("system:user:")
 const RoleRBACResource = rbac.Resource("system:role:")
 const AuthClientRBACResource = rbac.Resource("system:auth-client:")
