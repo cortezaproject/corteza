96de5934359b0693f8fbba4173757ae1d4e8db62
Peter Grlica <peter.grlica@gmail.com>
2021-03-17 17:30:39 +0100

    Added time, array, num and generic gval functions

--- pkg/expr/func_str.go
+++ pkg/expr/func_str.go
@@ -2,9 +2,11 @@ package expr
 
 import (
 	"fmt"
+	"regexp"
 	"strings"
 
 	"github.com/PaesslerAG/gval"
+	valid "github.com/asaskevich/govalidator"
 )
 
 func StringFunctions() []gval.Language {
@@ -18,6 +20,21 @@ func StringFunctions() []gval.Language {
 		gval.Function("shortest", shortest),
 		gval.Function("longest", longest),
 		gval.Function("format", fmt.Sprintf),
+		gval.Function("title", title),
+		gval.Function("untitle", untitle),
+		gval.Function("repeat", strings.Repeat),
+		gval.Function("replace", strings.Replace),
+		gval.Function("isUrl", valid.IsURL),
+		gval.Function("isEmail", valid.IsEmail),
+		gval.Function("split", strings.Split),
+		gval.Function("join", strings.Join),
+		gval.Function("hasSubstring", hasSubstring),
+		gval.Function("substring", substring),
+		gval.Function("hasPrefix", strings.HasPrefix),
+		gval.Function("hasSuffix", strings.HasSuffix),
+		gval.Function("shorten", shorten),
+		gval.Function("camelize", camelize),
+		gval.Function("snakify", snakify),
 	}
 }
 
@@ -43,4 +60,88 @@ func longest(f string, aa ...string) string {
 
 func length(s string) int {
 	return len(s)
+}
+
+// title works similarly as strings.ToTitle, with the expception
+// of uppercasing only the first word in line
+func title(s string) string {
+	split := strings.Split(s, " ")
+	return fmt.Sprintf("%s %s", strings.Title(split[0]), strings.Join(split[1:], " "))
+}
+
+// untitle works only on the first word in line
+func untitle(s string) string {
+	split := strings.Split(s, " ")
+	first := strings.ToLower(split[0][:1]) + split[0][1:]
+
+	return fmt.Sprintf("%s %s", first, strings.Join(split[1:], " "))
+}
+
+// hasSubstring checks if a substring exists in original string
+// use watchCase if need case sensitivity
+func hasSubstring(s, substring string, watchCase bool) bool {
+	if watchCase {
+		return strings.Contains(s, substring)
+	}
+
+	return strings.Contains(strings.ToLower(s), strings.ToLower(substring))
+}
+
+// substring extracts a substring from original string
+// specifying end value will not match till end of string
+func substring(s string, start, end int) string {
+	if end == -1 {
+		end = len(s)
+	}
+
+	if start >= len(s) {
+		return ""
+	}
+
+	end++
+	if end > len(s) {
+		end = len(s)
+	}
+
+	return s[start:end]
+}
+
+// shorten trims by whole words or only chars by
+// the specified amount
+func shorten(s, t string, count int) string {
+	var joined string
+
+	if t == "char" {
+		if count > len(s) {
+			return ""
+		}
+
+		joined = s[:count]
+	} else {
+		fields := strings.Fields(s)
+
+		if len(fields) == 0 {
+			return ""
+		}
+
+		joined = strings.Join(fields[:count], " ")
+	}
+
+	reg, err := regexp.Compile("[^a-zA-Z0-9]$")
+
+	if err != nil {
+		return ""
+	}
+
+	joined = reg.ReplaceAllString(joined, "")
+
+	return joined + " …"
+}
+
+func camelize(s string) string {
+	return untitle(strings.Replace(strings.Title(s), " ", "", -1))
+}
+
+func snakify(s string) string {
+	return strings.ToLower(strings.Replace(strings.Title(s), " ", "_", -1))
 }
--- pkg/expr/func_str_test.go
+++ pkg/expr/func_str_test.go
@@ -42,3 +42,150 @@ func Example_longest() {
 	// output:
 	// foobar
 }
+
+func Example_title() {
+	eval(`title("foo bAR")`, nil)
+
+	// output:
+	// Foo bAR
+}
+
+func Example_untitle() {
+	eval(`untitle("Foo Bar")`, nil)
+
+	// output:
+	// foo Bar
+}
+
+func Example_repeat() {
+	eval(`repeat("duran ", c)`, map[string]interface{}{"c": 2})
+
+	// output:
+	// duran duran
+}
+
+func Example_replace_all() {
+	eval(`replace("foobar baz", "ba", "BA", c)`, map[string]interface{}{"c": -1})
+
+	// output:
+	// fooBAr BAz
+}
+
+func Example_replace_first() {
+	eval(`replace("foobar baz", "ba", "BA", c)`, map[string]interface{}{"c": 1})
+
+	// output:
+	// fooBAr baz
+}
+
+func Example_isUrl() {
+	eval(`isUrl("http:/example.tld")`, nil)
+
+	// output:
+	// false
+}
+
+func Example_isEmail() {
+	eval(`isUrl("example.user+valid@example.tld")`, nil)
+
+	// output:
+	// true
+}
+
+func Example_split() {
+	eval(`split("This will be split in:2-parts", ":")`, nil)
+
+	// output:
+	// [This will be split in 2-parts]
+}
+
+func Example_join() {
+	eval(`join(exploded, ",")`, map[string][]string{"exploded": {"One", "two", "three"}})
+
+	// output:
+	// One,two,three
+}
+
+func Example_hasSubstring_caseS() {
+	eval(`hasSubstring("foo BAR", "o b", true)`, nil)
+
+	// output:
+	// false
+}
+
+func Example_hasSubstring_caseI() {
+	eval(`hasSubstring("foo BAR", "o b", false)`, nil)
+
+	// output:
+	// true
+}
+
+func Example_hasSuffix() {
+	eval(`hasSuffix("foo bar", "ar")`, nil)
+
+	// output:
+	// true
+}
+
+func Example_hasPrefix() {
+	eval(`hasPrefix("foo bar", "foo ")`, nil)
+
+	// output:
+	// true
+}
+
+func Example_shorten_word() {
+	eval(`shorten("foo bar 1337, this is. test one - or three", "word", c)`, map[string]int{"c": 3})
+
+	// output:
+	// foo bar 1337 …
+}
+
+func Example_shorten_char() {
+	eval(`shorten("foo bar 1337, this is. test one - or three", "char", c)`, map[string]int{"c": 22})
+
+	// output:
+	// foo bar 1337, this is …
+}
+
+func Example_camelize() {
+	eval(`camelize("foo bar baz_test")`, nil)
+
+	// output:
+	// fooBarBaz_test
+}
+
+func Example_snakify() {
+	eval(`snakify("foo bar baz_test")`, nil)
+
+	// output:
+	// foo_bar_baz_test
+}
+
+func Example_substring() {
+	eval(`substring("foo bar baz_test", start, end)`, map[string]interface{}{"start": 2, "end": -1})
+
+	// output:
+	// o bar baz_test
+}
+
+func Example_substring_highStart() {
+	eval(`substring("foo", start, end)`, map[string]interface{}{"start": 3, "end": -1})
+
+	// output:
+	//
+}
+
+func Example_substring_withEnd() {
+	eval(`substring("foo bar baz_test", start, end)`, map[string]interface{}{"start": 2, "end": 4})
+
+	// output:
+	// o b
+}
+
+func Example_substring_endOverflow() {
+	eval(`substring("foo bar baz_test", start, end)`, map[string]interface{}{"start": 2, "end": 100})
+
+	// output:
+	// o bar baz_test
+}
