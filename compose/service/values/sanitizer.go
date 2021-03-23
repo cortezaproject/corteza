0117896231f61f9e790d0ae25acfea5f1e77de92
Denis Arh <denis.arh@gmail.com>
2021-03-23 20:41:22 +0100

    Fix datetime field type value manipulation

--- compose/service/values/sanitizer.go
+++ compose/service/values/sanitizer.go
@@ -175,16 +175,7 @@ func sDatetime(v interface{}, onlyDate, onlyTime bool) string {
 		datetime = fmt.Sprintf("%v", v)
 	)
 
-	if onlyDate {
-		internalFormat = datetimeInternalFormatDate
-		inputFormats = []string{
-			datetimeInternalFormatDate,
-			"02 Jan 06",
-			"Monday, 02-Jan-06",
-			"Mon, 02 Jan 2006",
-			"2006/_1/_2",
-		}
-	} else if onlyTime {
+	if onlyTime {
 		internalFormat = datetimeIntenralFormatTime
 		inputFormats = []string{
 			datetimeIntenralFormatTime,
@@ -198,29 +189,44 @@ func sDatetime(v interface{}, onlyDate, onlyTime bool) string {
 			time.Kitchen,
 		}
 	} else {
-		internalFormat = datetimeInternalFormatFull
+		if onlyDate {
+			// In case only date is used, make sure we format it properly
+			internalFormat = datetimeInternalFormatDate
+		} else {
+			internalFormat = datetimeInternalFormatFull
+		}
+
 		// date & time
 		inputFormats = []string{
 			datetimeInternalFormatFull,
+			"2006-01-02T15:04:05", // iso8601 without timezone
 			time.RFC1123Z,
 			time.RFC1123,
-			time.RFC850,
 			time.RFC822Z,
 			time.RFC822,
-			time.RubyDate,
-			time.UnixDate,
+			time.RFC850,
 			time.ANSIC,
-			"2006/_1/_2 15:04:05",
-			"2006/_1/_2 15:04",
-		}
-
-		// if string looks like a RFC 3330 (ISO 8601), see if we need to suffix it with Z
-		if isoDaty.MatchString(datetime) && !hasTimezone.MatchString(datetime) {
-			// No timezone, add Z to satisfy parser
-			datetime = datetime + "Z"
-
-			// Simplifiy list of rules
-			inputFormats = []string{time.RFC3339}
+			time.UnixDate,
+			time.RubyDate,
+			"2006-01-02 15:04:05.999999999 -0700 MST", // Time.String()
+			"2006-01-02",
+			"02 Jan 2006",
+			"2006-01-02T15:04:05-0700", // RFC3339 without timezone hh:mm colon
+			"2006-01-02 15:04:05 -07:00",
+			"2006-01-02 15:04:05 -0700",
+			"2006-01-02 15:04:05Z07:00", // RFC3339 without T
+			"2006-01-02 15:04:05Z0700",  // RFC3339 without T or timezone hh:mm colon
+			"2006-01-02 15:04:05",
+			time.Kitchen,
+			time.Stamp,
+			time.StampMilli,
+			time.StampMicro,
+			time.StampNano,
+			datetimeInternalFormatDate,
+			"02 Jan 06",
+			"Monday, 02-Jan-06",
+			"Mon, 02 Jan 2006",
+			"2006/_1/_2",
 		}
 	}
 
