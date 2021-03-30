d49f4fbbad3c9ee766b232ec6360976d7b7c310a
Denis Arh <denis.arh@gmail.com>
2021-03-30 20:07:20 +0200

    Additional tests & bool handling

--- pkg/expr/expr_types_test.go
+++ pkg/expr/expr_types_test.go
@@ -1,11 +1,59 @@
 package expr
 
 import (
+	"context"
 	"github.com/stretchr/testify/require"
 	"net/http"
 	"net/url"
 	"testing"
 )
+
+func TestTypedValueOperations(t *testing.T) {
+	scope := RVars{
+		"xUint":   Must(NewUnsignedInteger(1)),
+		"xInt":    Must(NewInteger(1)),
+		"xBoolT":  Must(NewBoolean(true)),
+		"xBoolF":  Must(NewBoolean(false)),
+		"xString": Must(NewString("foo")),
+	}.Vars()
+
+	tcc := []struct {
+		expects interface{}
+		expr    string
+	}{
+		// sanity check
+		{true, "xBoolT"},
+		{false, "xBoolF"},
+
+		// uint ops
+		{true, "xUint == 1"},
+		{uint64(1), "xUint"},
+
+		// uint ops
+		{true, "xInt == 1"},
+		{int64(1), "xInt"},
+
+		// string ops
+		{true, `xString == "foo"`},
+		{"foo", "xString"},
+	}
+
+	for _, tc := range tcc {
+		t.Run(tc.expr, func(t *testing.T) {
+			var (
+				req       = require.New(t)
+				parser    = NewParser()
+				eval, err = parser.Parse(tc.expr)
+			)
+
+			req.NoError(err)
+			result, err := eval.Eval(context.Background(), scope)
+			req.NoError(err)
+			req.Equal(tc.expects, UntypedValue(result))
+		})
+	}
+
+}
 
 func TestKV_Set(t *testing.T) {
 	var (
@@ -44,6 +92,37 @@ func TestKVV_Set(t *testing.T) {
 	req.NoError(kvv.Assign(url.Values{"foo": []string{"bar"}}))
 	req.Contains(kvv.value, "foo")
 	req.Equal([]string{"bar"}, kvv.value["foo"])
+}
+
+func TestCastEmptyString(t *testing.T) {
+	var (
+		req = require.New(t)
+	)
+	{
+		f, err := CastToFloat("")
+		req.NoError(err)
+		req.Zero(f)
+	}
+	{
+		i, err := CastToInteger("")
+		req.NoError(err)
+		req.Zero(i)
+	}
+	{
+		u, err := CastToUnsignedInteger("")
+		req.NoError(err)
+		req.Zero(u)
+	}
+	{
+		u, err := CastToDuration("")
+		req.NoError(err)
+		req.Zero(u)
+	}
+	{
+		u, err := CastToBoolean("")
+		req.NoError(err)
+		req.False(u)
+	}
 }
 
 func TestCastToArray(t *testing.T) {
