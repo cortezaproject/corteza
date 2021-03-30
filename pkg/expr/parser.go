333b2f344fec2e68e5570166c2bf69c7941fa499
Denis Arh <denis.arh@gmail.com>
2021-03-30 15:52:53 +0200

    More improvements on expr vars & rec values

--- pkg/expr/parser.go
+++ pkg/expr/parser.go
@@ -66,11 +66,13 @@ func (p *gvalParser) ParseEvaluators(ee ...Evaluator) error {
 }
 
 func (e *gvalEval) Eval(ctx context.Context, scope *Vars) (interface{}, error) {
-	return e.evaluable(ctx, scope.Dict())
+	return e.evaluable(ctx, scope)
+	//return e.evaluable(ctx, scope.Dict())
 }
 
 func (e *gvalEval) Test(ctx context.Context, scope *Vars) (bool, error) {
-	r, err := e.evaluable(ctx, scope.Dict())
+	r, err := e.evaluable(ctx, scope)
+	//r, err := e.evaluable(ctx, scope.Dict())
 	if err != nil {
 		return false, err
 	}
