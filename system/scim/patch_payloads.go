83bd36bcd457ef44ebfe658b926158d3afc202da
Vivek Patel <ervicpatelwork@gmail.com>
2021-05-10 18:14:46 +0530

    Extends SCIM implementation to support role membership management
    - Updates request payload for patching group
    - Updates tests

--- system/scim/patch_payloads.go
+++ system/scim/patch_payloads.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"regexp"
 )
 
 const (
@@ -22,15 +23,27 @@ type (
 	operationRequest struct {
 		Operation string `json:"op"`
 		Path      string `json:"path"`
-		Value     []struct {
-			Value string `json:"value"`
-		} `json:"value"`
+		Value     map[string]string
 	}
 )
 
 func (req *operationsRequest) decodeJSON(r io.Reader) error {
 	if err := json.NewDecoder(r).Decode(req); err != nil {
 		return fmt.Errorf("could not decode operations payload: %w", err)
+	}
+
+	// Compiles req.Path and updates req.Value
+	for i, op := range req.Operations {
+		r := regexp.MustCompile(`([a-z]\w+)\[value eq \"([\w]{8}-[\w]{4}-[\w]{4}-[\w]{4}-[\w]{12})\"\]`)
+		strings := r.FindStringSubmatch(op.Path)
+		if len(strings) == 3 {
+			key := strings[1]
+			if op.Value == nil {
+				op.Value = make(map[string]string)
+			}
+			op.Value[key] = strings[2]
+			req.Operations[i].Value = op.Value
+		}
 	}
 
 	return nil
