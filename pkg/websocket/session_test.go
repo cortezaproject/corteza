f9a7124f49ec56a866f80e86c46224bfd639e51e
Vivek Patel <ervicpatelwork@gmail.com>
2021-05-14 16:44:52 +0530

    Fixes websocket server and session tests

--- pkg/websocket/session_test.go
+++ pkg/websocket/session_test.go
@@ -2,8 +2,10 @@ package websocket
 
 import (
 	"github.com/cortezaproject/corteza-server/pkg/auth"
+	"github.com/cortezaproject/corteza-server/pkg/logger"
 	"github.com/cortezaproject/corteza-server/pkg/options"
 	"github.com/stretchr/testify/require"
+	"go.uber.org/zap"
 	"testing"
 	"time"
 )
@@ -17,37 +19,40 @@ func TestSession_procRawMessage(t *testing.T) {
 		userID uint64 = 123
 	)
 
+	if testing.Verbose() {
+		s.logger = logger.MakeDebugLogger()
+	} else {
+		s.logger = zap.NewNop()
+	}
+
 	req.NoError(err)
 	s.server.accessToken = jwtHandler
 
 	jwt := jwtHandler.Encode(auth.NewIdentity(userID, 456, 789))
 
-	req.EqualError(s.procRawMessage([]byte("{}")), "empty payload")
+	req.EqualError(s.procRawMessage([]byte("{}")), "unauthenticated session")
 	req.Nil(s.identity)
 
-	req.EqualError(s.procRawMessage([]byte(`{"auth":{}}`)), "unauthorized: token contains an invalid number of segments")
+	req.EqualError(s.procRawMessage([]byte(`{"@type": "credentials", "@value": {"accessToken": ""}}`)), "unauthorized: token contains an invalid number of segments")
 	req.Nil(s.identity)
 
-	req.EqualError(s.procRawMessage([]byte(`{"auth":{"access_token": ""}}`)), "unauthorized: token contains an invalid number of segments")
-	req.Nil(s.identity)
-
-	req.NoError(s.procRawMessage([]byte(`{"auth":{"access_token": "` + jwt + `"}}`)))
+	req.NoError(s.procRawMessage([]byte(`{"@type": "credentials", "@value": {"accessToken": "` + jwt + `"}}`)))
 	req.NotNil(s.identity)
 	req.Equal(userID, s.identity.Identity())
 
-	req.EqualError(s.procRawMessage([]byte("{}")), "empty payload")
+	req.EqualError(s.procRawMessage([]byte("{}")), "unknown message type ''")
 	req.Equal(userID, s.identity.Identity())
 
 	// Repeat with the same user
 	jwt = jwtHandler.Encode(auth.NewIdentity(userID, 456, 789))
 
-	req.NoError(s.procRawMessage([]byte(`{"auth":{"access_token": "` + jwt + `"}}`)))
+	req.NoError(s.procRawMessage([]byte(`{"@type": "credentials", "@value": {"accessToken": "` + jwt + `"}}`)))
 	req.NotNil(s.identity)
 	req.Equal(userID, s.identity.Identity())
 
 	// Try to authenticate on an existing authenticated session as a different user
 	jwt = jwtHandler.Encode(auth.NewIdentity(userID+1, 456, 789))
 
-	req.EqualError(s.procRawMessage([]byte(`{"auth":{"access_token": "`+jwt+`"}}`)), "unauthorized: identity does not match")
+	req.EqualError(s.procRawMessage([]byte(`{"@type": "credentials", "@value": {"accessToken": "`+jwt+`"}}`)), "unauthorized: identity does not match")
 
 }
