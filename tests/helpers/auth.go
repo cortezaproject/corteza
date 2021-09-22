92f225654c7da41eeb92d168ddc941c0c53c02d6
Denis Arh <denis.arh@gmail.com>
2021-09-22 21:23:42 +0200

    Fix access token handling in integration test

--- tests/helpers/auth.go
+++ tests/helpers/auth.go
@@ -7,7 +7,6 @@ import (
 	"github.com/steinfletcher/apitest"
 
 	"github.com/cortezaproject/corteza-server/pkg/auth"
-	"github.com/cortezaproject/corteza-server/system/types"
 )
 
 func BindAuthMiddleware(r chi.Router) {
@@ -15,16 +14,6 @@ func BindAuthMiddleware(r chi.Router) {
 		auth.DefaultJwtHandler.HttpVerifier(),
 		auth.DefaultJwtHandler.HttpAuthenticator(),
 	)
-}
-
-func ReqHeaderAuthBearer(user *types.User) apitest.Intercept {
-	return func(req *http.Request) {
-		if user == nil {
-			req.Header.Del("Authorization")
-		} else {
-			req.Header.Set("Authorization", "Bearer "+auth.DefaultJwtHandler.Encode(user))
-		}
-	}
 }
 
 func ReqHeaderRawAuthBearer(token string) apitest.Intercept {
