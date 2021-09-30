d8d5758ba4196bb2d6a4b25ffff4d433c8e4acf6
Vivek Patel <ervicpatelwork@gmail.com>
2021-09-30 15:21:32 +0530

    Fixes apigw prefilter for string with hyphen

--- tests/apigw/prefilter_query_passing_test.go
+++ tests/apigw/prefilter_query_passing_test.go
@@ -17,4 +17,12 @@ func Test_prefilter_query_passing(t *testing.T) {
 		Expect(t).
 		Status(http.StatusOK).
 		End()
+
+	h.apiInit().
+		Get("/test-hyphen").
+		Query("foo-bar", "encrypted-string").
+		Header("Accept", "application/json").
+		Expect(t).
+		Status(http.StatusOK).
+		End()
 }
