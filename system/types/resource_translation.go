4619fa65b3ce0151fd1e938273bfd753a895af76
Vivek Patel <ervicpatelwork@gmail.com>
2021-10-07 19:54:08 +0530

    Removes html from translation message string

--- system/types/resource_translation.go
+++ system/types/resource_translation.go
@@ -4,6 +4,7 @@ import (
 	"database/sql/driver"
 	"strings"
 	"time"
+	"unicode/utf8"
 
 	"github.com/cortezaproject/corteza-server/pkg/filter"
 	"github.com/cortezaproject/corteza-server/pkg/locale"
@@ -86,7 +87,7 @@ outer:
 			Lang:     Lang{Tag: language.Make(b.Lang)},
 			Resource: b.Resource,
 			K:        b.Key,
-			Message:  b.Msg,
+			Message:  sanitizeHtml(b.Msg),
 		})
 	}
 	return
@@ -97,7 +98,7 @@ func (set ResourceTranslationSet) Old(bb locale.ResourceTranslationSet) (out Res
 		for _, a := range set {
 			// It's not new
 			if a.Compare(b) {
-				a.Message = b.Msg
+				a.Message = sanitizeHtml(b.Msg)
 				out = append(out, a)
 				break
 			}
@@ -117,4 +118,51 @@ func FromLocale(ll locale.ResourceTranslationSet) (out ResourceTranslationSet) {
 	}
 
 	return out
+}
+
+// sanitizeHtml Aggressively strips HTML tags from a string.
+// It will only keep anything between `>` and `<`.
+func sanitizeHtml(s string) string {
+	const (
+		htmlTagStart = 60 // Unicode `<`
+		htmlTagEnd   = 62 // Unicode `>`
+	)
+
+	// Set up a string builder and allocate enough memory for the new string.
+	var builder strings.Builder
+	builder.Grow(len(s) + utf8.UTFMax)
+
+	in := false // True if we are inside an HTML tag.
+	start := 0  // The index of the previous start tag character `<`
+	end := 0    // The index of the previous end tag character `>`
+
+	for i, c := range s {
+		// If this is the last character, and we are not in an HTML tag, save it.
+		if (i+1) == len(s) && end >= start {
+			builder.WriteString(s[end:])
+		}
+
+		// Keep going if the character is not `<` or `>`
+		if c != htmlTagStart && c != htmlTagEnd {
+			continue
+		}
+
+		if c == htmlTagStart {
+			// Only update the start if we are not in a tag.
+			// This make sure we strip `<<br>` not just `<br>`
+			if !in {
+				start = i
+			}
+			in = true
+
+			// Write the valid string between the close and start of the two tags.
+			builder.WriteString(s[end:start])
+			continue
+		}
+		// else c == htmlTagEnd
+		in = false
+		end = i + 1
+	}
+	s = builder.String()
+	return s
 }
