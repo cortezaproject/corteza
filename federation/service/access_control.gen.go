0564fe7190539d9c14ad54ad13aa8b248b82a909
Vivek Patel <ervicpatelwork@gmail.com>
2021-11-26 21:15:43 +0530

    Add support for role permission cloning
    It clones all RBAC rules from one role to another, but also removes all existing rules from role.

--- federation/service/access_control.gen.go
+++ federation/service/access_control.gen.go
@@ -30,6 +30,7 @@ type (
 			Can(rbac.Session, string, rbac.Resource) bool
 			Grant(context.Context, ...*rbac.Rule) error
 			FindRulesByRoleID(roleID uint64) (rr rbac.RuleSet)
+			CloneRulesByRoleID(ctx context.Context, fromRoleID uint64, toRoleID ...uint64) error
 		}
 	}
 )
@@ -169,6 +170,17 @@ func (svc accessControl) FindRulesByRoleID(ctx context.Context, roleID uint64) (
 	}
 
 	return svc.rbac.FindRulesByRoleID(roleID), nil
+}
+
+// CloneRulesByRoleID clone all rules of a Role S to a specific Role T
+//
+// This function is auto-generated
+func (svc accessControl) CloneRulesByRoleID(ctx context.Context, fromRoleID uint64, toRoleID ...uint64) error {
+	if !svc.CanGrant(ctx) {
+		return AccessControlErrNotAllowedToSetPermissions()
+	}
+
+	return svc.rbac.CloneRulesByRoleID(ctx, fromRoleID, toRoleID...)
 }
 
 // CanManageExposedModule checks if current user can manage shared module
--- tests/system/permissions_test.go
+++ tests/system/permissions_test.go
@@ -3,6 +3,7 @@ package system
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 	"testing"
 
 	"github.com/cortezaproject/corteza-server/pkg/rbac"
@@ -96,4 +97,124 @@ func TestPermissionsDelete(t *testing.T) {
 	for _, r := range p.FindRulesByRoleID(permDelRole) {
 		h.a.True(r.Access == rbac.Inherit)
 	}
+}
+
+func TestPermissionsCloneToSingleRole(t *testing.T) {
+	h := newHelper(t)
+	p := rbac.Global()
+
+	// Make sure our user can grant
+	helpers.AllowMe(h, types.ComponentRbacResource(), "grant")
+
+	// New role.
+	roleS := h.roleID + 1
+	roleT := h.roleID + 2
+
+	h.a.Len(rbac.Global().FindRulesByRoleID(roleS), 0)
+	h.a.Len(rbac.Global().FindRulesByRoleID(roleT), 0)
+
+	// Set up a few fake rules for new role
+	helpers.Grant(rbac.AllowRule(roleS, types.ComponentRbacResource(), "user.create"))
+
+	helpers.Grant(rbac.AllowRule(roleT, types.ComponentRbacResource(), "user.update"))
+	helpers.Grant(rbac.AllowRule(roleT, types.ComponentRbacResource(), "user.delete"))
+
+	h.a.Len(p.FindRulesByRoleID(roleS), 1)
+	h.a.Len(p.FindRulesByRoleID(roleT), 2)
+
+	h.apiInit().
+		Post(fmt.Sprintf("/permissions/%d/rules/clone", roleS)).
+		Query("cloneToRoleID", strconv.FormatUint(roleT, 10)).
+		Header("Accept", "application/json").
+		Expect(t).
+		Status(http.StatusOK).
+		Assert(helpers.AssertNoErrors).
+		End()
+
+	// Make sure all rules for role S are intact
+	h.a.Len(p.FindRulesByRoleID(roleS), 1)
+	// Make sure all rules for role T are cloned from role S
+	h.a.Len(p.FindRulesByRoleID(roleT), 1)
+}
+
+func TestPermissionsCloneToMultipleRole(t *testing.T) {
+	h := newHelper(t)
+	p := rbac.Global()
+
+	// Make sure our user can grant
+	helpers.AllowMe(h, types.ComponentRbacResource(), "grant")
+
+	// New role.
+	roleS := h.roleID + 1
+	roleT := h.roleID + 2
+	roleY := h.roleID + 3
+
+	fmt.Println("roleS: ", roleS)
+	fmt.Println("roleS: ", roleT)
+	fmt.Println("roleS: ", roleY)
+
+	h.a.Len(rbac.Global().FindRulesByRoleID(roleS), 0)
+	h.a.Len(rbac.Global().FindRulesByRoleID(roleT), 0)
+	h.a.Len(rbac.Global().FindRulesByRoleID(roleY), 0)
+
+	// Set up a few fake rules for new role
+	helpers.Grant(rbac.AllowRule(roleS, types.ComponentRbacResource(), "user.create"))
+
+	helpers.Grant(rbac.AllowRule(roleT, types.ComponentRbacResource(), "user.update"))
+	helpers.Grant(rbac.AllowRule(roleT, types.ComponentRbacResource(), "user.delete"))
+
+	helpers.Grant(rbac.AllowRule(roleY, types.ComponentRbacResource(), "user.create"))
+	helpers.Grant(rbac.AllowRule(roleY, types.ComponentRbacResource(), "user.update"))
+	helpers.Grant(rbac.AllowRule(roleY, types.ComponentRbacResource(), "user.delete"))
+
+	h.a.Len(p.FindRulesByRoleID(roleS), 1)
+	h.a.Len(p.FindRulesByRoleID(roleT), 2)
+	h.a.Len(p.FindRulesByRoleID(roleY), 3)
+
+	h.apiInit().
+		Post(fmt.Sprintf("/permissions/%d/rules/clone", roleS)).
+		Query("cloneToRoleID", strconv.FormatUint(roleT, 10)).
+		Query("cloneToRoleID", strconv.FormatUint(roleY, 10)).
+		Header("Accept", "application/json").
+		Expect(t).
+		Status(http.StatusOK).
+		Assert(helpers.AssertNoErrors).
+		End()
+
+	// Make sure all rules for role S are intact
+	h.a.Len(p.FindRulesByRoleID(roleS), 1)
+	// Make sure all rules for role T are cloned from role S
+	h.a.Len(p.FindRulesByRoleID(roleT), 1)
+	// Make sure all rules for role Y are cloned from role S
+	h.a.Len(p.FindRulesByRoleID(roleY), 1)
+}
+
+func TestPermissionsCloneNotAllowed(t *testing.T) {
+	h := newHelper(t)
+	p := rbac.Global()
+
+	// New role.
+	roleS := h.roleID + 1
+	roleT := h.roleID + 2
+
+	h.a.Len(rbac.Global().FindRulesByRoleID(roleS), 0)
+	h.a.Len(rbac.Global().FindRulesByRoleID(roleT), 0)
+
+	// Set up a few fake rules for new role
+	helpers.Grant(rbac.AllowRule(roleS, types.ComponentRbacResource(), "user.create"))
+
+	helpers.Grant(rbac.AllowRule(roleT, types.ComponentRbacResource(), "user.update"))
+	helpers.Grant(rbac.AllowRule(roleT, types.ComponentRbacResource(), "user.delete"))
+
+	h.a.Len(p.FindRulesByRoleID(roleS), 1)
+	h.a.Len(p.FindRulesByRoleID(roleT), 2)
+
+	h.apiInit().
+		Post(fmt.Sprintf("/permissions/%d/rules/clone", roleS)).
+		Header("Accept", "application/json").
+		FormData("cloneToRoleID", strconv.FormatUint(roleT, 10)).
+		Expect(t).
+		Status(http.StatusOK).
+		Assert(helpers.AssertError("accessControl.errors.notAllowedToSetPermissions")).
+		End()
 }
