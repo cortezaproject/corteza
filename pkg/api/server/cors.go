120ff81ab72d100bc69840914f3a4d1b4b852bd4
Denis Arh <denis.arh@gmail.com>
2022-01-22 21:47:51 +0100

    Tweaked CORS settings

--- pkg/api/server/cors.go
+++ pkg/api/server/cors.go
@@ -9,9 +9,24 @@ import (
 // Sets up default CORS rules to use as a middleware
 func handleCORS(next http.Handler) http.Handler {
 	return cors.New(cors.Options{
-		AllowedOrigins:   []string{"*"},
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"},
-		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-ID"},
+		AllowedOrigins: []string{
+			"http://*",
+			"https://*",
+		},
+		AllowedMethods: []string{
+			http.MethodHead,
+			http.MethodGet,
+			http.MethodPost,
+			http.MethodPut,
+			http.MethodPatch,
+			http.MethodDelete,
+		},
+		AllowedHeaders: []string{
+			"Accept",
+			"Authorization",
+			"Content-Type",
+			"X-CSRF-ID",
+		},
 		AllowCredentials: true,
 		MaxAge:           300, // Maximum value not ignored by any of major browsers
 	}).Handler(next)
