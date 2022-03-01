8c0a62284d05d19da7a32c38fd491113e4510aa9
Denis Arh <denis.arh@gmail.com>
2022-03-01 09:13:37 +0100

    Add back-link sanitization on logout page

--- auth/handlers/links.go
+++ auth/handlers/links.go
@@ -1,6 +1,9 @@
 package handlers
 
-import "strings"
+import (
+	"regexp"
+	"strings"
+)
 
 type (
 	Links struct {
@@ -47,7 +50,10 @@ type (
 	}
 )
 
-var BasePath string = "/"
+var (
+	invalidLinkChars        = regexp.MustCompile(`[^-A-Za-z0-9+&@#/%?=~_|!:,.;\\(\\)]`)
+	BasePath         string = "/"
+)
 
 func GetLinks() Links {
 	var b = strings.TrimSuffix(BasePath, "/") + "/"
@@ -100,4 +106,8 @@ func tbp(s string) string {
 	}
 
 	return s
+}
+
+func sanitizeLink(l string) string {
+	return invalidLinkChars.ReplaceAllString(l, "")
 }
