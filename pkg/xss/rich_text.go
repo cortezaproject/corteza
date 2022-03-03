d95eec997378f7f1650b050c4803b291663d34de
Tomaž Jerman <tomaz.jerman121@gmail.com>
2022-03-03 15:12:56 +0100

    Make resource translation sanitization looser
    Reuse record value configuration to allow rich text from
    content page blocks.

---
+++ pkg/xss/rich_text.go
@@ -0,0 +1,26 @@
+package xss
+
+import (
+	"html"
+	"regexp"
+
+	"github.com/microcosm-cc/bluemonday"
+)
+
+// RichText assures safe HTML content
+func RichText(in string) string {
+	// use standard html escaping policy
+	p := bluemonday.UGCPolicy()
+
+	// match only colors for html editor elements on style attr
+	p.AllowAttrs("style").OnElements("span", "p")
+	p.AllowStyles("color").Matching(regexp.MustCompile("(?i)^#([0-9a-f]{3,4}|[0-9a-f]{6}|[0-9a-f]{8})$")).Globally()
+	p.AllowStyles("background-color").Matching(regexp.MustCompile("(?i)^#([0-9a-f]{3,4}|[0-9a-f]{6}|[0-9a-f]{8})$")).Globally()
+
+	sanitized := p.Sanitize(in)
+
+	// handle escaped strings and unescape them
+	// all the dangerous chars should have been stripped
+	// by now
+	return html.UnescapeString(sanitized)
+}
