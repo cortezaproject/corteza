62a653fb08c4c3735903239cb72087285847176a
Denis Arh <denis.arh@gmail.com>
2022-03-22 12:16:00 +0100

    Fix crash on unexisting module
    Module proc() called with nil for module.

--- compose/service/module.go
+++ compose/service/module.go
@@ -446,14 +446,14 @@ func (svc module) lookup(ctx context.Context, namespaceID uint64, lookup func(*m
 			return ModuleErrNotAllowedToRead()
 		}
 
+		svc.proc(ctx, m)
+
 		if err = loadModuleLabels(ctx, svc.store, m); err != nil {
 			return err
 		}
 
 		return loadModuleFields(ctx, svc.store, m)
 	}()
-
-	svc.proc(ctx, m)
 
 	return m, svc.recordAction(ctx, aProps, ModuleActionLookup, err)
 }
