86431fe82f222d2369cd522ca63cba36338074b3
Vivek Patel <ervicpatelwork@gmail.com>
2022-04-26 16:40:58 +0530

    Add rest endpoints for chart translations
    It allows saving/updating yAxis label and metric label translations

--- compose/types/chart.go
+++ compose/types/chart.go
@@ -3,6 +3,8 @@ package types
 import (
 	"database/sql/driver"
 	"encoding/json"
+	"github.com/cortezaproject/corteza-server/pkg/locale"
+	"strings"
 	"time"
 
 	"github.com/cortezaproject/corteza-server/pkg/filter"
@@ -31,6 +33,7 @@ type (
 	}
 
 	ChartConfigReport struct {
+		ReportID   uint64                   `json:"reportID,string,omitempty"`
 		Filter     string                   `json:"filter"`
 		ModuleID   uint64                   `json:"moduleID,string,omitempty"`
 		Metrics    []map[string]interface{} `json:"metrics,omitempty"`
@@ -64,6 +67,76 @@ type (
 		filter.Paging
 	}
 )
+
+func (c Chart) decodeTranslations(tt locale.ResourceTranslationIndex) {
+	var aux *locale.ResourceTranslation
+
+	for i, report := range c.Config.Reports {
+		if aux = tt.FindByKey(LocaleKeyChartYAxisLabel.Path); aux != nil {
+			c.Config.Reports[i].YAxis["label"] = aux.Msg
+		}
+
+		for j, metric := range report.Metrics {
+			if metricID, ok := metric["metricID"]; ok {
+				mID, is := metricID.(string)
+				if !is {
+					continue
+				}
+				mpl := strings.NewReplacer(
+					"{{metricID}}", mID,
+				)
+
+				if aux = tt.FindByKey(mpl.Replace(LocaleKeyChartMetricsMetricIDLabel.Path)); aux != nil {
+					c.Config.Reports[i].Metrics[j]["label"] = aux.Msg
+				}
+			}
+		}
+	}
+}
+
+func (c Chart) encodeTranslations() (out locale.ResourceTranslationSet) {
+	out = make(locale.ResourceTranslationSet, 0, 12)
+
+	for _, report := range c.Config.Reports {
+		if mLabel, ok := report.YAxis["label"]; ok {
+			ml, is := mLabel.(string)
+			if !is {
+				continue
+			}
+			out = append(out, &locale.ResourceTranslation{
+				Resource: c.ResourceTranslation(),
+				Key:      LocaleKeyChartYAxisLabel.Path,
+				Msg:      ml,
+			})
+		}
+
+		for _, metric := range report.Metrics {
+			if metricID, ok := metric["metricID"]; ok {
+				mID, is := metricID.(string)
+				if !is {
+					continue
+				}
+				mpl := strings.NewReplacer(
+					"{{metricID}}", mID,
+				)
+
+				if mLabel, ok := metric["label"]; ok {
+					ml, is := mLabel.(string)
+					if !is {
+						continue
+					}
+					out = append(out, &locale.ResourceTranslation{
+						Resource: c.ResourceTranslation(),
+						Key:      mpl.Replace(LocaleKeyChartMetricsMetricIDLabel.Path),
+						Msg:      ml,
+					})
+				}
+			}
+		}
+	}
+
+	return
+}
 
 // FindByHandle finds chart by it's handle
 func (set ChartSet) FindByHandle(handle string) *Chart {
