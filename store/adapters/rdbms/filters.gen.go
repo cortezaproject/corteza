d187f6439993fa7d8fbab007fedbd9af40f2d484
Tomaž Jerman <tomaz.jerman121@gmail.com>
2022-05-23 15:13:21 +0200

    Add missing module filter to module field search

--- store/adapters/rdbms/filters.gen.go
+++ store/adapters/rdbms/filters.gen.go
@@ -575,6 +575,10 @@ func ComposeModuleFieldFilter(f composeType.ModuleFieldFilter) (ee []goqu.Expres
 		ee = append(ee, expr)
 	}
 
+	if len(f.ModuleID) > 0 {
+		ee = append(ee, goqu.C("rel_module").In(f.ModuleID))
+	}
+
 	return ee, f, err
 }
 
