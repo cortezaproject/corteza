2a71dc80b16b61194f860594af0b85bf07849db4
Tomaž Jerman <tomaz.jerman121@gmail.com>
2022-07-15 16:21:41 +0200

    Add missing store tests for privacy resources

--- store/tests/dal_connections.go
+++ store/tests/dal_connections.go
@@ -1,10 +1,142 @@
 package tests
 
 import (
-	"github.com/cortezaproject/corteza-server/store"
+	"context"
 	"testing"
+	"time"
+
+	"github.com/cortezaproject/corteza-server/pkg/filter"
+	"github.com/cortezaproject/corteza-server/pkg/id"
+	"github.com/cortezaproject/corteza-server/pkg/rand"
+	"github.com/cortezaproject/corteza-server/store"
+	"github.com/cortezaproject/corteza-server/system/types"
+	"github.com/stretchr/testify/require"
 )
 
 func testDalConnections(t *testing.T, s store.DalConnections) {
-	// @todo
+	var (
+		ctx = context.Background()
+		req = require.New(t)
+
+		makeNew = func(handle string) *types.DalConnection {
+			// minimum data set for new dalConnection
+			return &types.DalConnection{
+				ID:        id.Next(),
+				CreatedAt: time.Now(),
+				Handle:    handle,
+			}
+		}
+
+		truncAndCreate = func(t *testing.T) (*require.Assertions, *types.DalConnection) {
+			req := require.New(t)
+			req.NoError(s.TruncateDalConnections(ctx))
+			res := makeNew(string(rand.Bytes(10)))
+			req.NoError(s.CreateDalConnection(ctx, res))
+			return req, res
+		}
+	)
+
+	t.Run("create", func(t *testing.T) {
+		dalConnection := makeNew("DalConnectionCRUD")
+		req.NoError(s.CreateDalConnection(ctx, dalConnection))
+	})
+
+	t.Run("lookup by ID", func(t *testing.T) {
+		req, dalConnection := truncAndCreate(t)
+		fetched, err := s.LookupDalConnectionByID(ctx, dalConnection.ID)
+		req.NoError(err)
+		req.Equal(dalConnection.Handle, fetched.Handle)
+		req.Equal(dalConnection.ID, fetched.ID)
+		req.NotNil(fetched.CreatedAt)
+		req.Nil(fetched.UpdatedAt)
+		req.Nil(fetched.DeletedAt)
+	})
+
+	t.Run("update", func(t *testing.T) {
+		req, dalConnection := truncAndCreate(t)
+		dalConnection.Handle = "DalConnectionCRUD-2"
+
+		req.NoError(s.UpdateDalConnection(ctx, dalConnection))
+
+		updated, err := s.LookupDalConnectionByID(ctx, dalConnection.ID)
+		req.NoError(err)
+		req.Equal(dalConnection.Handle, updated.Handle)
+	})
+
+	t.Run("upsert", func(t *testing.T) {
+		t.Run("existing", func(t *testing.T) {
+			req, dalConnection := truncAndCreate(t)
+			dalConnection.Handle = "DalConnectionCRUD-2"
+
+			req.NoError(s.UpsertDalConnection(ctx, dalConnection))
+
+			updated, err := s.LookupDalConnectionByID(ctx, dalConnection.ID)
+			req.NoError(err)
+			req.Equal(dalConnection.Handle, updated.Handle)
+		})
+
+		t.Run("new", func(t *testing.T) {
+			dalConnection := makeNew("upsert me")
+			dalConnection.Handle = "ComposeChartCRUD-2"
+
+			req.NoError(s.UpsertDalConnection(ctx, dalConnection))
+
+			upserted, err := s.LookupDalConnectionByID(ctx, dalConnection.ID)
+			req.NoError(err)
+			req.Equal(dalConnection.Handle, upserted.Handle)
+		})
+	})
+
+	t.Run("delete", func(t *testing.T) {
+		t.Run("by DalConnection", func(t *testing.T) {
+			req, dalConnection := truncAndCreate(t)
+			req.NoError(s.DeleteDalConnection(ctx, dalConnection))
+			_, err := s.LookupDalConnectionByID(ctx, dalConnection.ID)
+			req.EqualError(err, store.ErrNotFound.Error())
+		})
+
+		t.Run("by ID", func(t *testing.T) {
+			req, dalConnection := truncAndCreate(t)
+			req.NoError(s.DeleteDalConnectionByID(ctx, dalConnection.ID))
+			_, err := s.LookupDalConnectionByID(ctx, dalConnection.ID)
+			req.EqualError(err, store.ErrNotFound.Error())
+		})
+	})
+
+	t.Run("search", func(t *testing.T) {
+		prefill := []*types.DalConnection{
+			makeNew("/one-one"),
+			makeNew("/one-two"),
+			makeNew("/two-one"),
+			makeNew("/two-two"),
+			makeNew("/two-deleted"),
+		}
+
+		count := len(prefill)
+
+		prefill[4].DeletedAt = &prefill[4].CreatedAt
+		valid := count - 1
+
+		req.NoError(s.TruncateDalConnections(ctx))
+		req.NoError(s.CreateDalConnection(ctx, prefill...))
+
+		// search for all valid
+		set, _, err := s.SearchDalConnections(ctx, types.DalConnectionFilter{})
+		req.NoError(err)
+		req.Len(set, valid) // we've deleted one
+
+		// search for ALL
+		set, _, err = s.SearchDalConnections(ctx, types.DalConnectionFilter{Deleted: filter.StateInclusive})
+		req.NoError(err)
+		req.Len(set, count) // we've deleted one
+
+		// search for deleted only
+		set, _, err = s.SearchDalConnections(ctx, types.DalConnectionFilter{Deleted: filter.StateExclusive})
+		req.NoError(err)
+		req.Len(set, 1) // we've deleted one
+
+		set, _, err = s.SearchDalConnections(ctx, types.DalConnectionFilter{Handle: "/two-one"})
+		req.NoError(err)
+		req.Len(set, 1)
+	})
 }
--- store/tests/data_privacy_requests.go
+++ store/tests/data_privacy_requests.go
@@ -1,10 +1,130 @@
 package tests
 
 import (
-	"github.com/cortezaproject/corteza-server/store"
+	"context"
 	"testing"
+	"time"
+
+	"github.com/cortezaproject/corteza-server/pkg/id"
+	"github.com/cortezaproject/corteza-server/pkg/rand"
+	"github.com/cortezaproject/corteza-server/store"
+	"github.com/cortezaproject/corteza-server/system/types"
+	"github.com/stretchr/testify/require"
 )
 
 func testDataPrivacyRequests(t *testing.T, s store.DataPrivacyRequests) {
-	// @todo
+	var (
+		ctx = context.Background()
+		req = require.New(t)
+
+		makeNew = func(handle string) *types.DataPrivacyRequest {
+			// minimum data set for new dataPrivacyRequest
+			return &types.DataPrivacyRequest{
+				ID:        id.Next(),
+				CreatedAt: time.Now(),
+				Kind:      types.RequestKindCorrect,
+			}
+		}
+
+		truncAndCreate = func(t *testing.T) (*require.Assertions, *types.DataPrivacyRequest) {
+			req := require.New(t)
+			req.NoError(s.TruncateDataPrivacyRequests(ctx))
+			res := makeNew(string(rand.Bytes(10)))
+			req.NoError(s.CreateDataPrivacyRequest(ctx, res))
+			return req, res
+		}
+	)
+
+	t.Run("create", func(t *testing.T) {
+		dataPrivacyRequest := makeNew("DataPrivacyRequestCRUD")
+		req.NoError(s.CreateDataPrivacyRequest(ctx, dataPrivacyRequest))
+	})
+
+	t.Run("lookup by ID", func(t *testing.T) {
+		req, dataPrivacyRequest := truncAndCreate(t)
+		fetched, err := s.LookupDataPrivacyRequestByID(ctx, dataPrivacyRequest.ID)
+		req.NoError(err)
+		req.Equal(dataPrivacyRequest.Kind, fetched.Kind)
+		req.Equal(dataPrivacyRequest.ID, fetched.ID)
+		req.NotNil(fetched.CreatedAt)
+		req.Nil(fetched.UpdatedAt)
+		req.Nil(fetched.DeletedAt)
+	})
+
+	t.Run("update", func(t *testing.T) {
+		req, dataPrivacyRequest := truncAndCreate(t)
+		dataPrivacyRequest.Kind = types.RequestKindDelete
+
+		req.NoError(s.UpdateDataPrivacyRequest(ctx, dataPrivacyRequest))
+
+		updated, err := s.LookupDataPrivacyRequestByID(ctx, dataPrivacyRequest.ID)
+		req.NoError(err)
+		req.Equal(dataPrivacyRequest.Kind, updated.Kind)
+	})
+
+	t.Run("upsert", func(t *testing.T) {
+		t.Run("existing", func(t *testing.T) {
+			req, dataPrivacyRequest := truncAndCreate(t)
+			dataPrivacyRequest.Kind = types.RequestKindDelete
+
+			req.NoError(s.UpsertDataPrivacyRequest(ctx, dataPrivacyRequest))
+
+			updated, err := s.LookupDataPrivacyRequestByID(ctx, dataPrivacyRequest.ID)
+			req.NoError(err)
+			req.Equal(dataPrivacyRequest.Kind, updated.Kind)
+		})
+
+		t.Run("new", func(t *testing.T) {
+			dataPrivacyRequest := makeNew("upsert me")
+			dataPrivacyRequest.Kind = "ComposeChartCRUD-2"
+
+			req.NoError(s.UpsertDataPrivacyRequest(ctx, dataPrivacyRequest))
+
+			upserted, err := s.LookupDataPrivacyRequestByID(ctx, dataPrivacyRequest.ID)
+			req.NoError(err)
+			req.Equal(dataPrivacyRequest.Kind, upserted.Kind)
+		})
+	})
+
+	t.Run("delete", func(t *testing.T) {
+		t.Run("by DataPrivacyRequest", func(t *testing.T) {
+			req, dataPrivacyRequest := truncAndCreate(t)
+			req.NoError(s.DeleteDataPrivacyRequest(ctx, dataPrivacyRequest))
+			_, err := s.LookupDataPrivacyRequestByID(ctx, dataPrivacyRequest.ID)
+			req.EqualError(err, store.ErrNotFound.Error())
+		})
+
+		t.Run("by ID", func(t *testing.T) {
+			req, dataPrivacyRequest := truncAndCreate(t)
+			req.NoError(s.DeleteDataPrivacyRequestByID(ctx, dataPrivacyRequest.ID))
+			_, err := s.LookupDataPrivacyRequestByID(ctx, dataPrivacyRequest.ID)
+			req.EqualError(err, store.ErrNotFound.Error())
+		})
+	})
+
+	t.Run("search", func(t *testing.T) {
+		prefill := []*types.DataPrivacyRequest{
+			makeNew("/one-one"),
+			makeNew("/one-two"),
+			makeNew("/two-one"),
+			makeNew("/two-two"),
+		}
+
+		prefill[3].Kind = types.RequestKindDelete
+
+		count := len(prefill)
+		valid := count
+
+		req.NoError(s.TruncateDataPrivacyRequests(ctx))
+		req.NoError(s.CreateDataPrivacyRequest(ctx, prefill...))
+
+		// search for all valid
+		set, _, err := s.SearchDataPrivacyRequests(ctx, types.DataPrivacyRequestFilter{})
+		req.NoError(err)
+		req.Len(set, valid) // we've deleted one
+
+		set, _, err = s.SearchDataPrivacyRequests(ctx, types.DataPrivacyRequestFilter{Kind: []string{"delete"}})
+		req.NoError(err)
+		req.Len(set, 1)
+	})
 }
