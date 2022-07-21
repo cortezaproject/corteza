2f21d06bd5673653e6b5fbba1b0651a533edcc4a
Vivek Patel <ervicpatelwork@gmail.com>
2022-07-21 17:10:49 +0530

    Fix sensitive level check for data privacy modules
    A module have one or more module field with have a sensitivity level then it will consider as private module.

--- compose/types/data_privacy.go
+++ compose/types/data_privacy.go
@@ -7,16 +7,28 @@ import (
 
 type (
 	PrivacyModule struct {
-		ID     uint64 `json:"moduleID,string"`
-		Name   string `json:"name"`
-		Handle string `json:"handle"`
+		Module    PrivacyModuleMeta    `json:"module"`
+		Namespace PrivacyNamespaceMeta `json:"namespace"`
 
 		ConnectionID uint64                  `json:"-"`
 		Connection   *sysTypes.DalConnection `json:"connection"`
 	}
 
+	PrivacyModuleMeta struct {
+		ID     uint64         `json:"moduleID,string"`
+		Name   string         `json:"name"`
+		Handle string         `json:"handle"`
+		Fields ModuleFieldSet `json:"fields"`
+	}
+
+	PrivacyNamespaceMeta struct {
+		ID   uint64 `json:"namespaceID,string"`
+		Slug string `json:"slug"`
+		Name string `json:"name"`
+	}
+
 	PrivacyModuleFilter struct {
-		NamespaceID  uint64   `json:"namespaceID,string"`
+		NamespaceID  uint64   `json:"-"`
 		ConnectionID []uint64 `json:"connectionID,string"`
 
 		// Standard helpers for paging and sorting
