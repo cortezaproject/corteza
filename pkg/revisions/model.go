e8004911456cd79ab3a770dc7bcd9baddf5c5120
Denis Arh <denis.arh@gmail.com>
2022-07-27 20:16:44 +0200

    Implement generic package for revisions

---
+++ pkg/revisions/model.go
@@ -0,0 +1,40 @@
+package revisions
+
+import (
+	"github.com/cortezaproject/corteza-server/pkg/dal"
+	"github.com/cortezaproject/corteza-server/pkg/dal/capabilities"
+	"github.com/cortezaproject/corteza-server/pkg/id"
+)
+
+const (
+	RevisionResourceType = "corteza::system:revision"
+)
+
+// Model returns generic dal.Model for storing revisions
+//
+// Returns only basic
+func Model() *dal.Model {
+	// make revision model
+	return &dal.Model{
+		ResourceID: id.Next(),
+
+		Ident: "revisions",
+
+		ResourceType: RevisionResourceType,
+		Attributes: dal.AttributeSet{
+			&dal.Attribute{Ident: "id", PrimaryKey: true, Store: &dal.CodecPlain{}, Type: &dal.TypeID{}},
+			&dal.Attribute{Ident: "ts", Store: &dal.CodecPlain{}, Type: &dal.TypeTimestamp{}},
+			&dal.Attribute{Ident: "revision", Store: &dal.CodecPlain{}, Type: &dal.TypeNumber{}},
+			&dal.Attribute{Ident: "operation", Store: &dal.CodecPlain{}, Type: &dal.TypeNumber{}},
+			&dal.Attribute{Ident: "rel_resource", Store: &dal.CodecPlain{}, Type: &dal.TypeID{}},
+			&dal.Attribute{Ident: "rel_user", Store: &dal.CodecPlain{}, Type: &dal.TypeID{}},
+			&dal.Attribute{Ident: "delta", Store: &dal.CodecPlain{}, Type: &dal.TypeJSON{}},
+			&dal.Attribute{Ident: "comment", Store: &dal.CodecPlain{}, Type: &dal.TypeText{}},
+		},
+
+		Capabilities: capabilities.Set{
+			capabilities.Create,
+			capabilities.Search,
+		},
+	}
+}
