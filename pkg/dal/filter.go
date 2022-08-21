daf294d5728231ce68310d0ec2bc5bb629fe1470
Tomaž Jerman <tomaz.jerman121@gmail.com>
2022-08-21 16:53:59 +0200

    Add an internalFilter wrapping struct for filter.Filter
    The struct simplifies filter manipulation when performing
    analysis and optimization.

---
+++ pkg/dal/filter.go
@@ -0,0 +1,144 @@
+package dal
+
+import (
+	"fmt"
+
+	"github.com/cortezaproject/corteza-server/pkg/filter"
+	"github.com/cortezaproject/corteza-server/pkg/ql"
+)
+
+type (
+	// internalFilter is a wrap to simplify interactions for cases where we might
+	// want to interact with the received filters; i.e. when optimizing pipeline steps
+	internalFilter struct {
+		constraints      map[string][]any
+		stateConstraints map[string]filter.State
+		expression       string
+		expParsed        *ql.ASTNode
+		orderBy          filter.SortExprSet
+		limit            uint
+		cursor           *filter.PagingCursor
+	}
+)
+
+func (f internalFilter) Constraints() map[string][]any             { return f.constraints }
+func (f internalFilter) StateConstraints() map[string]filter.State { return f.stateConstraints }
+func (f internalFilter) Expression() string                        { return f.expression }
+func (f internalFilter) OrderBy() filter.SortExprSet               { return f.orderBy }
+func (f internalFilter) Limit() uint                               { return f.limit }
+func (f internalFilter) Cursor() *filter.PagingCursor              { return f.cursor }
+
+// toInternalFilter converts filter.Filter to internalFilter for easier manipulation
+//
+// nil filters are returned as zeroed-out internalFilters.
+func toInternalFilter(f filter.Filter) (out internalFilter, err error) {
+	if f == nil {
+		return
+	}
+
+	// Base
+	out = internalFilter{
+		constraints:      f.Constraints(),
+		stateConstraints: f.StateConstraints(),
+		expression:       f.Expression(),
+		orderBy:          f.OrderBy(),
+		limit:            f.Limit(),
+		cursor:           f.Cursor(),
+	}
+
+	// Parse expression foe later use
+	if out.expression != "" {
+		out.expParsed, err = ql.NewParser().Parse(out.expression)
+		if err != nil {
+			return
+		}
+	}
+
+	return
+}
+
+// mergeFilters returns a new filter based on a overwritten by values from b
+func (a internalFilter) mergeFilters(b internalFilter) (c internalFilter) {
+	c = a
+
+	// expression
+	if b.expParsed != nil {
+		if c.expParsed == nil {
+			c.expParsed = b.expParsed
+			c.expression = b.expression
+		} else {
+			parsedA := a.expParsed
+			rawA := a.expression
+			if parsedA.Ref != "group" {
+				parsedA = &ql.ASTNode{
+					Ref: "group",
+					Args: []*ql.ASTNode{
+						parsedA,
+					},
+				}
+				rawA = fmt.Sprintf("(%s)", rawA)
+			}
+
+			parsedB := b.expParsed
+			rawB := b.expression
+			if parsedB.Ref != "group" {
+				parsedB = &ql.ASTNode{
+					Ref: "group",
+					Args: []*ql.ASTNode{
+						parsedB,
+					},
+				}
+				rawB = fmt.Sprintf("(%s)", rawB)
+			}
+
+			c.expParsed = &ql.ASTNode{
+				Ref:  "and",
+				Args: ql.ASTNodeSet{parsedA, parsedB},
+			}
+			c.expression = fmt.Sprintf("%s && %s", rawA, rawB)
+		}
+
+	}
+
+	// constraints
+	if b.constraints != nil {
+		if c.constraints == nil {
+			c.constraints = make(map[string][]any)
+		}
+
+		for k, v := range b.constraints {
+			c.constraints[k] = append(c.constraints[k], v...)
+		}
+	}
+
+	// state constraints
+	if b.stateConstraints != nil {
+		if c.stateConstraints == nil {
+			c.stateConstraints = make(map[string]filter.State)
+		}
+		for k, v := range b.stateConstraints {
+			c.stateConstraints[k] = v
+		}
+	}
+
+	// order by
+	if b.orderBy != nil {
+		c.orderBy = append(c.orderBy, b.orderBy...)
+	}
+
+	// cursor
+	// always use the latest paging cursor
+	c.cursor = b.cursor
+
+	return
+}
+
+// empty reports if the filter is completely empty or not
+func (f internalFilter) empty() bool {
+	return f.constraints == nil &&
+		f.stateConstraints == nil &&
+		f.expression == "" &&
+		f.orderBy == nil &&
+		f.limit == 0 &&
+		f.cursor == nil
+}
