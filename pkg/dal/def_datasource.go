01c6f7cc6e958f03f11461ff01638a3c58ff6bf8
Tomaž Jerman <tomaz.jerman121@gmail.com>
2022-10-10 16:18:51 +0200

    Add base support for offloading pipeline steps to the data source level

--- pkg/dal/def_datasource.go
+++ pkg/dal/def_datasource.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/cortezaproject/corteza-server/pkg/expr"
 	"github.com/cortezaproject/corteza-server/pkg/filter"
+	"github.com/cortezaproject/corteza-server/pkg/ql"
 )
 
 type (
@@ -18,13 +20,21 @@ type (
 
 		OutAttributes []AttributeMapping
 
-		analysis stepAnalysis
+		analysis   map[string]OpAnalysis
+		connection *ConnectionWrap
+		model      *Model
+
+		// clobbered lists all of the steps that are offloaded into the datasource.
+		// The list is provided in order; the first step is the first step which should execute
+		// in the report pipeline.
+		//
+		// @todo change to generic step; doing this because I can for now
+		clobbered []*Aggregate
 
 		// provided in the init step so we can omit some code in the exec step
+		// @todo consider removing this
 		auxIter Iterator
 	}
-
-	iterProvider func(ctx context.Context, mf ModelRef, f filter.Filter) (iter Iterator, model *Model, err error)
 )
 
 func (def *Datasource) Identifier() string {
@@ -36,22 +46,40 @@ func (def *Datasource) Sources() []string {
 }
 
 func (def *Datasource) Attributes() [][]AttributeMapping {
-	return [][]AttributeMapping{def.OutAttributes}
+	if len(def.clobbered) > 0 {
+		return def.offloadedAttributes()
+	}
+
+	return def.ownAttributes()
 }
 
 func (def *Datasource) Analyze(ctx context.Context) (err error) {
-	// @todo probe datasource; for now, RDBMS only so all is cheap
-	def.analysis = stepAnalysis{
-		scanCost:   costUnknown,
-		searchCost: costUnknown,
-		filterCost: costUnknown,
-		sortCost:   costUnknown,
-		outputSize: sizeUnknown,
+	a, err := def.connection.connection.Analyze(ctx, def.model)
+	if err != nil {
+		return
 	}
+
+	def.analysis = map[string]OpAnalysis{
+		OpAnalysisIterate: {
+			ScanCost:   CostUnknown,
+			SearchCost: CostUnknown,
+			FilterCost: CostUnknown,
+			SortCost:   CostUnknown,
+			OutputSize: SizeUnknown,
+		},
+	}
+
+	if _, ok := a[OpAnalysisAggregate]; ok {
+		def.analysis[OpAnalysisAggregate] = a[OpAnalysisAggregate]
+	}
+	if _, ok := a[OpAnalysisJoin]; ok {
+		def.analysis[OpAnalysisJoin] = a[OpAnalysisJoin]
+	}
+
 	return
 }
 
-func (def *Datasource) Analysis() stepAnalysis {
+func (def *Datasource) Analysis() map[string]OpAnalysis {
 	return def.analysis
 }
 
@@ -59,7 +87,14 @@ func (def *Datasource) Optimize(req internalFilter) (res internalFilter, err err
 	return internalFilter{}, fmt.Errorf("optimization not implemented")
 }
 
-func (def *Datasource) init(ctx context.Context, s iterProvider) (err error) {
+func (def *Datasource) init(ctx context.Context) (err error) {
+	if def.model == nil {
+		return fmt.Errorf("cannot initialize datasource: model not set")
+	}
+	if def.connection == nil {
+		return fmt.Errorf("cannot initialize datasource: connection not set")
+	}
+
 	if def.Filter != nil {
 		def.filter, err = toInternalFilter(def.Filter)
 		if err != nil {
@@ -67,17 +102,8 @@ func (def *Datasource) init(ctx context.Context, s iterProvider) (err error) {
 		}
 	}
 
-	// Do the first init to get the model.
-	// For now, this is a ~free operation, but it should change when we allow things
-	// like nesting reports, etc.
-	// @todo refactor datasource descriptors to avoid this
-	_, model, err := s(ctx, def.ModelRef, def.filter)
-	if err != nil {
-		return
-	}
-
 	if len(def.OutAttributes) == 0 {
-		def.OutAttributes = def.outAttrsFromModel(model)
+		def.OutAttributes = def.outAttrsFromModel(def.model)
 	}
 
 	pp := make([]string, 0, len(def.OutAttributes)/2+1)
@@ -94,18 +120,36 @@ func (def *Datasource) init(ctx context.Context, s iterProvider) (err error) {
 		return
 	}
 
-	// Get the iterator for actual use
-	def.auxIter, _, err = s(ctx, def.ModelRef, def.filter)
-	if err != nil {
-		return
-	}
-
+	// Firstly validate the base
 	err = def.validate()
 	if err != nil {
 		return
 	}
 
-	return nil
+	if len(def.clobbered) > 0 {
+		// @todo currently, we can only do one; change this when we tweak the DB
+		//       offloading code.
+		ag := def.clobbered[0]
+
+		// Invoke the aggregation's init to perform the validation and preparation logic
+		var wa *aggregate
+		wa, err = ag.init(ctx, nil)
+		if err != nil {
+			return
+		}
+
+		// Preprocess the filters to conform to connection API
+		f, having, err := def.getAggregationFilters(def.filter, wa.filter)
+		if err != nil {
+			return err
+		}
+
+		def.auxIter, err = def.connection.connection.Aggregate(ctx, def.model, f, wa.groupDefs, wa.aggregateDefs, having)
+		return err
+	}
+
+	def.auxIter, err = def.connection.connection.Search(ctx, def.model, def.filter)
+	return err
 }
 
 func (def *Datasource) exec(ctx context.Context) (out Iterator, err error) {
@@ -141,12 +185,177 @@ func (def *Datasource) outAttrsFromModel(model *Model) (attrs []AttributeMapping
 			Ident: attr.Ident,
 			Src:   attr.Ident,
 			Props: MapProperties{
-				IsPrimary: attr.PrimaryKey,
-				IsSystem:  attr.System,
-				Nullable:  attr.Type.IsNullable(),
-				Type:      attr.Type,
+				IsPrimary:    attr.PrimaryKey,
+				IsSystem:     attr.System,
+				IsFilterable: attr.Filterable,
+				IsSortable:   attr.Sortable,
+				Nullable:     attr.Type.IsNullable(),
+				Type:         attr.Type,
 			},
 		})
+	}
+
+	return
+}
+
+func (def *Datasource) shouldClobberAggregation() bool {
+	costs, ok := def.analysis[OpAnalysisAggregate]
+	if !ok {
+		return false
+	}
+
+	// @todo more to it; check and compare costs; for now we know that all rdbms
+	//       offloads will be faster.
+
+	_ = costs
+
+	return true
+}
+
+func (def *Datasource) ownAttributes() [][]AttributeMapping {
+	return [][]AttributeMapping{def.OutAttributes}
+}
+
+func (def *Datasource) offloadedAttributes() [][]AttributeMapping {
+	// The last offloaded step's attributes is what this step's iterator returns
+	return def.clobbered[len(def.clobbered)-1].Attributes()
+}
+
+func (def *Datasource) clobber(s PipelineStep) (ok bool) {
+	switch cs := s.(type) {
+	case *Aggregate:
+		if !def.shouldClobberAggregation() {
+			return false
+		}
+
+		def.clobbered = append(def.clobbered, cs)
+		return true
+	}
+
+	return
+}
+
+// getAggregationFilters transforms the base and the aggregation filters to conform
+// to the store/dal's model.Aggregate API
+//
+// - The full filter.Filter return parameter is applied to the base dataset
+// - The QL node return parameter is applied only to the aggregated dataset
+//
+// @todo should we change the store's API to accept 2x filter.Filter? I think
+//       that would allow the underlaying driver to decide how to handle them
+//       instead of relying on what SQL does.
+func (def *Datasource) getAggregationFilters(base, agg internalFilter) (filter internalFilter, having *ql.ASTNode, err error) {
+	var typedV expr.TypedValue
+	filter = base
+
+	// Move the aggregation's order and limit to the base filter because those
+	// are applied to the final output (per SQL)
+	//
+	// Leave base filtering parameters (filter, constraints, ...) as those are apploed
+	// to the dataset before aggregation (which is correct).
+	filter.orderBy = agg.orderBy
+	filter.limit = agg.limit
+
+	// Convert the rest of the filtering parameters defined on the aggregation's filter
+	// to the QL node.
+	// ALl of that is applied after the aggregation which is correct.
+	var nConstraints *ql.ASTNode
+	if len(agg.constraints) > 0 {
+		nConstraints = &ql.ASTNode{
+			Ref: "and",
+		}
+		for k, c := range agg.constraints {
+			arg := &ql.ASTNode{
+				Ref: "or",
+			}
+
+			for _, v := range c {
+				typedV, err = expr.Typify(v)
+				if err != nil {
+					return
+				}
+				arg.Args = append(arg.Args, &ql.ASTNode{
+					Symbol: k,
+					Value:  ql.WrapValue(typedV),
+				})
+			}
+
+			nConstraints.Args = append(nConstraints.Args, &ql.ASTNode{
+				Ref:  "group",
+				Args: ql.ASTNodeSet{arg},
+			})
+		}
+	}
+
+	var nStateConstraints *ql.ASTNode
+	if len(agg.stateConstraints) > 0 {
+		nStateConstraints = &ql.ASTNode{
+			Ref: "and",
+		}
+		for k, c := range agg.stateConstraints {
+			typedV, err = expr.Typify(c)
+			if err != nil {
+				return
+			}
+
+			nStateConstraints.Args = append(nStateConstraints.Args, &ql.ASTNode{
+				Symbol: k,
+				Value:  ql.WrapValue(typedV),
+			})
+		}
+	}
+
+	var nMetaConstraints *ql.ASTNode
+	if len(agg.stateConstraints) > 0 {
+		nMetaConstraints = &ql.ASTNode{
+			Ref: "and",
+		}
+		for k, c := range agg.stateConstraints {
+			typedV, err = expr.Typify(c)
+			if err != nil {
+				return
+			}
+
+			nMetaConstraints.Args = append(nMetaConstraints.Args, &ql.ASTNode{
+				Symbol: k,
+				Value:  ql.WrapValue(typedV),
+			})
+		}
+	}
+
+	nExpression := agg.expParsed
+
+	var nCursor *ql.ASTNode
+	if agg.cursor != nil {
+		nCursor, err = agg.cursor.ToAST(nil, nil)
+		if err != nil {
+			return
+		}
+	}
+
+	having = &ql.ASTNode{
+		Ref: "and",
+	}
+
+	if nConstraints != nil {
+		having.Args = append(having.Args, nConstraints)
+	}
+	if nStateConstraints != nil {
+		having.Args = append(having.Args, nStateConstraints)
+	}
+	if nMetaConstraints != nil {
+		having.Args = append(having.Args, nMetaConstraints)
+	}
+	if nExpression != nil {
+		having.Args = append(having.Args, nExpression)
+	}
+	if nCursor != nil {
+		having.Args = append(having.Args, nCursor)
+	}
+
+	// In case everything is empty, no need for the having part
+	if len(having.Ref) == 0 {
+		having = nil
 	}
 
 	return
--- pkg/dal/pipeline.go
+++ pkg/dal/pipeline.go
@@ -24,7 +24,13 @@ type (
 		Attributes() [][]AttributeMapping
 
 		Analyze(ctx context.Context) error
-		Analysis() stepAnalysis
+		Analysis() map[string]OpAnalysis
+	}
+
+	// clobberableStep can have other steps offload their work to it.
+	// This is primarily used to offload the work to the database.
+	clobberableStep interface {
+		clobber(PipelineStep) bool
 	}
 
 	// Attribute mapping outlines specific attributes within a pipeline
@@ -38,11 +44,13 @@ type (
 
 	// MapProperties describe the attribute such as it's type and constraints
 	MapProperties struct {
-		Label     string
-		IsPrimary bool
-		IsSystem  bool
-		Nullable  bool
-		Type      Type
+		Label        string
+		IsPrimary    bool
+		IsSystem     bool
+		IsFilterable bool
+		IsSortable   bool
+		Nullable     bool
+		Type         Type
 	}
 )
 
@@ -96,56 +104,6 @@ func (pp Pipeline) LinkSteps() (err error) {
 	return nil
 }
 
-// Analyze runs analysis over each step in the pipeline
-//
-// Step analysis hints to the optimizers as to how expensive specific operations
-// are and the general dataset size involved.
-func (pp Pipeline) Analyze(ctx context.Context) (err error) {
-	for _, p := range pp {
-		err = p.Analyze(ctx)
-		if err != nil {
-			return
-		}
-	}
-	return
-}
-
-// Optimize runs all optimization and returns an optimized pipeline
-func (base Pipeline) Optimize(ctx context.Context) (optimized Pipeline, err error) {
-	base, err = base.OptimizeStructure(ctx)
-	if err != nil {
-		return
-	}
-	return base.OptimizeSteps(ctx)
-}
-
-// OptimizeStructure performs general pipeline structure optimizations such as
-// restructuring and clobbering steps onto the datasource layer
-func (base Pipeline) OptimizeStructure(ctx context.Context) (optimized Pipeline, err error) {
-	optimized = base.Clone()
-
-	return optimized, optimized.walkSubtrees(optimized.root(), func(step PipelineStep, isRoot bool) (out PipelineStep, err error) {
-		out = step
-		for _, opt := range pipelineOptimizers {
-			out, err = opt(out, isRoot)
-			if err != nil {
-				return
-			}
-		}
-		return
-	})
-}
-
-// OptimizeSteps performs step specific optimizations such as pushing filters
-// on the lower levels, determining step-specific plans, ...
-func (base Pipeline) OptimizeSteps(ctx context.Context) (optimized Pipeline, err error) {
-	optimized = base.Clone()
-	return optimized, optimized.optimizeSteps(base.root(), internalFilter{})
-}
-
-// // // // // // // // // // // // // // // // // // // // // // // // //
-// Utilities
-
 func (pp Pipeline) root() PipelineStep {
 	ix := make(map[string]PipelineStep)
 
@@ -168,19 +126,16 @@ func (pp Pipeline) root() PipelineStep {
 
 // Slice returns a new pipeline with all steps of the subtree with ident as root
 func (pp Pipeline) Slice(ident string) (out Pipeline) {
-	// Make a copy so we can assure the caller can go ham over the pipeline
-	ppc := pp.Clone()
-
 	// Find root
 	var r PipelineStep
-	for _, p := range ppc {
+	for _, p := range pp {
 		if p.Identifier() == ident {
 			r = p
 			break
 		}
 	}
 
-	return ppc.slice(r)
+	return pp.slice(r)
 }
 
 // slice is the recursive counterpart for the Slice method
@@ -201,159 +156,5 @@ func (pp Pipeline) slice(s PipelineStep) (out Pipeline) {
 		return append(out, append(pp.slice(n.relLeft), pp.slice(n.relRight)...)...)
 	}
 
-	return
-}
-
-// optimizeSteps is the recursive counterpart to the .OptimizeSteps method
-func (p Pipeline) optimizeSteps(node PipelineStep, inF internalFilter) (err error) {
-	switch n := node.(type) {
-	case *Datasource:
-		inF, err = n.Optimize(inF)
-		if err != nil {
-			return
-		}
-		if !inF.empty() {
-			return fmt.Errorf("a datasource can not offload optimizations")
-		}
-		return
-
-	case *Aggregate:
-		inF, err = n.Optimize(inF)
-		if err != nil {
-			return
-		}
-		return p.optimizeSteps(n.rel, inF)
-
-	case *Join:
-		inF, err = n.Optimize(inF)
-		if err != nil {
-			return
-		}
-		err = p.optimizeSteps(n.relLeft, inF)
-		if err != nil {
-			return
-		}
-		err = p.optimizeSteps(n.relRight, inF)
-		if err != nil {
-			return
-		}
-
-	case *Link:
-		inF, err = n.Optimize(inF)
-		if err != nil {
-			return
-		}
-		err = p.optimizeSteps(n.relLeft, inF)
-		if err != nil {
-			return
-		}
-		err = p.optimizeSteps(n.relRight, inF)
-		if err != nil {
-			return
-		}
-	}
-
-	return
-}
-
-// walkSubtrees performs a DFS and invokes fn for every sub-tree node in the returning order
-func (p Pipeline) walkSubtrees(root PipelineStep, fn func(step PipelineStep, isRoot bool) (PipelineStep, error)) (err error) {
-	err = p.walkSubtreesRec(root, true, fn)
-	if err != nil {
-		return
-	}
-
-	// dfsRec reports only subtrees; in case no sub tree was there, do it
-	switch root.(type) {
-	case *Join, *Link:
-		n, err := fn(root, true)
-		if err != nil {
-			return err
-		}
-		p.replace(root, n)
-	}
-
-	return
-}
-
-// walkSubtreesRec is the recursive counterpart to the .walkSubtrees method
-func (p Pipeline) walkSubtreesRec(root PipelineStep, isRoot bool, fn func(step PipelineStep, isRoot bool) (PipelineStep, error)) (err error) {
-	var n PipelineStep
-
-	switch s := root.(type) {
-	case *Datasource:
-		// this one doesn't have anything under it
-		return
-
-	case *Aggregate:
-		return p.walkSubtreesRec(s.rel, false, fn)
-
-	case *Join:
-		err = p.walkSubtreesRec(s.relLeft, false, fn)
-		if err != nil {
-			return
-		}
-		err = p.walkSubtreesRec(s.relRight, false, fn)
-		if err != nil {
-			return
-		}
-		n, err = fn(root, isRoot)
-		if err != nil {
-			return
-		}
-		p.replace(root, n)
-
-	case *Link:
-		err = p.walkSubtreesRec(s.relLeft, false, fn)
-		if err != nil {
-			return
-		}
-		err = p.walkSubtreesRec(s.relRight, false, fn)
-		if err != nil {
-			return
-		}
-		n, err = fn(root, isRoot)
-		if err != nil {
-			return
-		}
-		p.replace(root, n)
-	}
-
-	return
-}
-
-func (pp Pipeline) replace(o, n PipelineStep) {
-	for i, p := range pp {
-		if p == o {
-			pp[i] = n
-			return
-		}
-	}
-}
-
-func (p Pipeline) Clone() (out Pipeline) {
-	out = make(Pipeline, 0, len(p))
-	for _, s := range p {
-		switch s := s.(type) {
-		case *Aggregate:
-			aux := *s
-			out = append(out, &aux)
-
-		case *Join:
-			aux := *s
-			out = append(out, &aux)
-
-		case *Link:
-			aux := *s
-			out = append(out, &aux)
-
-		case *Datasource:
-			aux := *s
-			out = append(out, &aux)
-
-		default:
-			panic("unsupported step")
-		}
-	}
 	return
 }
