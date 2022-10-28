abf62fdabe0763be5474cbafc8cd71017564c298
hantian_pang <hantian.pang@gmail.com>
2022-10-28 15:12:59 +0800

    Fix fetch all records when no dedup rules
    Current dedup config of module is always enable (as front-end comment).
    So the better way to control dedup is rules length.

--- compose/service/record.go
+++ compose/service/record.go
@@ -1680,7 +1680,7 @@ func (svc record) DupDetection(ctx context.Context, m *types.Module, rec *types.
 		config = m.Config.RecordDeDup
 	)
 
-	if config.Enabled {
+	if len(config.Rules) > 0 {
 		records, _, err = svc.Find(ctx, types.RecordFilter{
 			ModuleID:    m.ID,
 			NamespaceID: m.NamespaceID,
