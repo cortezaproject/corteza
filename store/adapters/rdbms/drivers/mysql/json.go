8e8893d518cb0dea5287e23c280d00bdd8b5c298
Denis Arh <denis.arh@gmail.com>
2022-11-08 12:43:21 +0100

    Remove old 'Deep JSON' leftover code

--- store/adapters/rdbms/drivers/mysql/json.go
+++ store/adapters/rdbms/drivers/mysql/json.go
@@ -7,27 +7,6 @@ import (
 	"strings"
 )
 
-func JsonPath(asJSON bool, jsonDoc exp.Expression, pp ...any) (_ exp.LiteralExpression, err error) {
-	var (
-		sql  strings.Builder
-		path string
-	)
-
-	if path, err = jsonPath(pp...); err != nil {
-		return nil, err
-	}
-
-	sql.WriteString(`?->'`)
-	if !asJSON {
-		// interested in un-encoded value
-		sql.WriteString(`>'`)
-	}
-	sql.WriteString(path)
-	sql.WriteString(`'`)
-
-	return exp.NewLiteralExpression(sql.String(), jsonDoc), nil
-}
-
 func jsonPathExpr(pp ...any) (exp.LiteralExpression, error) {
 	if path, err := jsonPath(pp...); err != nil {
 		return nil, err
@@ -58,38 +37,4 @@ func jsonPath(pp ...any) (string, error) {
 	}
 
 	return sql.String(), nil
-}
-
-// JSONArrayContains prepares MySQL compatible comparison of value and JSON array
-//
-// # literal value = multi-value field / plain
-// # multi-value field = single-value field / plain
-// JSON_CONTAINS(v, JSON_EXTRACT(V, '$.f3'), '$.f2')
-//
-// # single-value field = multi-value field / plain
-// # multi-value field = single-value field / plain
-// JSON_CONTAINS(v, '"aaa"', '$.f2')
-//
-// This approach is not optimal, but it is the only way to make it work
-func JSONArrayContains(left exp.Expression, ident exp.IdentifierExpression, pp ...any) (jc exp.Expression, err error) {
-	var (
-		pathAux string
-		path    exp.Expression
-	)
-
-	// this is the least painful way how to encode
-	// an unknown value as JSON
-	left = exp.NewSQLFunctionExpression(
-		"JSON_EXTRACT",
-		exp.NewSQLFunctionExpression("JSON_ARRAY", left),
-		exp.NewLiteralExpression(`'$[0]'`),
-	)
-
-	if pathAux, err = jsonPath(pp...); err != nil {
-		return nil, err
-	} else {
-		path = exp.NewLiteralExpression(`'` + pathAux + `'`)
-	}
-
-	return exp.NewSQLFunctionExpression("JSON_CONTAINS", ident, left, path), nil
 }
