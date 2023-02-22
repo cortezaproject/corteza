b180dc3867162e7c874404e68bdbe207280cfaa0
Vivek Patel <ervicpatelwork@gmail.com>
2023-02-22 14:53:29 +0530

    Fix tests for AuthClient
    Added meta.name to test to avoid, missing error

--- server/system/service/auth_client.go
+++ server/system/service/auth_client.go
@@ -137,9 +137,9 @@ func (svc *authClient) Search(ctx context.Context, af types.AuthClientFilter) (a
 			//
 			// not the best solution but ATM it allows us to have at least
 			// some kind of control over who can see deleted authClients
-			//if !svc.ac.CanAccess(ctx) {
+			// if !svc.ac.CanAccess(ctx) {
 			//	return AuthClientErrNotAllowedToListAuthClients()
-			//}
+			// }
 		}
 
 		if len(af.Labels) > 0 {
@@ -187,7 +187,7 @@ func (svc *authClient) Create(ctx context.Context, new *types.AuthClient) (res *
 	)
 
 	err = func() (err error) {
-		if new.Meta.Name == "" {
+		if new.Meta == nil || new.Meta.Name == "" {
 			return AuthClientErrMissingName()
 		}
 
@@ -264,7 +264,7 @@ func (svc *authClient) Update(ctx context.Context, upd *types.AuthClient) (res *
 		if upd.ID == 0 {
 			return AuthClientErrInvalidID()
 		}
-		if upd.Meta.Name == "" {
+		if upd.Meta == nil || upd.Meta.Name == "" {
 			return AuthClientErrMissingName()
 		}
 
