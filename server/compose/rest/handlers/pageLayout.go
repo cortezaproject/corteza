4a2341fa58870b5b33e2a0f73f88a7c36c7d3521
Tomaž Jerman <tomaz.jerman121@gmail.com>
2023-03-15 15:39:26 +0100

    Add page independent layout search endpoint

--- server/compose/rest/handlers/pageLayout.go
+++ server/compose/rest/handlers/pageLayout.go
@@ -19,6 +19,7 @@ import (
 type (
 	// Internal API interface
 	PageLayoutAPI interface {
+		ListNamespace(context.Context, *request.PageLayoutListNamespace) (interface{}, error)
 		List(context.Context, *request.PageLayoutList) (interface{}, error)
 		Create(context.Context, *request.PageLayoutCreate) (interface{}, error)
 		Read(context.Context, *request.PageLayoutRead) (interface{}, error)
@@ -31,6 +32,7 @@ type (
 
 	// HTTP API interface
 	PageLayout struct {
+		ListNamespace      func(http.ResponseWriter, *http.Request)
 		List               func(http.ResponseWriter, *http.Request)
 		Create             func(http.ResponseWriter, *http.Request)
 		Read               func(http.ResponseWriter, *http.Request)
@@ -44,6 +46,22 @@ type (
 
 func NewPageLayout(h PageLayoutAPI) *PageLayout {
 	return &PageLayout{
+		ListNamespace: func(w http.ResponseWriter, r *http.Request) {
+			defer r.Body.Close()
+			params := request.NewPageLayoutListNamespace()
+			if err := params.Fill(r); err != nil {
+				api.Send(w, r, err)
+				return
+			}
+
+			value, err := h.ListNamespace(r.Context(), params)
+			if err != nil {
+				api.Send(w, r, err)
+				return
+			}
+
+			api.Send(w, r, value)
+		},
 		List: func(w http.ResponseWriter, r *http.Request) {
 			defer r.Body.Close()
 			params := request.NewPageLayoutList()
@@ -178,6 +196,7 @@ func NewPageLayout(h PageLayoutAPI) *PageLayout {
 func (h PageLayout) MountRoutes(r chi.Router, middlewares ...func(http.Handler) http.Handler) {
 	r.Group(func(r chi.Router) {
 		r.Use(middlewares...)
+		r.Get("/namespace/{namespaceID}/page-layout", h.ListNamespace)
 		r.Get("/namespace/{namespaceID}/page/{pageID}/layout/", h.List)
 		r.Post("/namespace/{namespaceID}/page/{pageID}/layout/", h.Create)
 		r.Get("/namespace/{namespaceID}/page/{pageID}/layout/{pageLayoutID}", h.Read)
