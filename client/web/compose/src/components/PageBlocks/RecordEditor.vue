7524260bed6d7395bd5f841fff1e6bf42589c8f4
Jože Fortun <joze.fortun@gmail.com>
2023-03-23 11:17:43 +0100

    Respect module field order in non configured record blocks

--- client/web/compose/src/components/PageBlocks/RecordEditor.vue
+++ client/web/compose/src/components/PageBlocks/RecordEditor.vue
@@ -118,8 +118,8 @@ export default {
       }
 
       if (!this.options.fields || this.options.fields.length === 0) {
-        // No fields defined in the options, show all (buy system)
-        return this.module.fields.slice().sort((a, b) => a.label.localeCompare(b.label))
+        // No fields defined in the options, show all (but system)
+        return this.module.fields
       }
 
       // Show filtered & ordered list of fields
