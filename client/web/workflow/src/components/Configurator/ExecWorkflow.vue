6a0177f7a4770b74dfa8535b2d336eec3c6ddc8d
Jože Fortun <joze.fortun@gmail.com>
2023-03-31 12:39:00 +0200

    Remove required from exec workflow scope parameter

--- client/web/workflow/src/components/Configurator/ExecWorkflow.vue
+++ client/web/workflow/src/components/Configurator/ExecWorkflow.vue
@@ -43,7 +43,7 @@ export default {
               types: [
                 'Vars',
               ],
-              required: true,
+              required: false,
             },
           ],
           results: [],
