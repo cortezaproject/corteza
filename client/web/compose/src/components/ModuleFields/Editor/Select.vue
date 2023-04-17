0798331d70b4f30d38ff8cf10125b5952cf2237b
Kelani Tolulope <kelanik8@gmail.com>
2023-04-17 14:09:40 +0100

    Add clearer add option to select module field configurator

--- client/web/compose/src/components/ModuleFields/Editor/Select.vue
+++ client/web/compose/src/components/ModuleFields/Editor/Select.vue
@@ -113,7 +113,9 @@ export default {
           ? this.value === o.value
           : (this.value || []).includes(o.value)
         return { ...o, disabled }
-      }).filter(({ value = '' }) => value)
+      }).filter(({ value = '', text = '' }) => {
+        return value !== '' || text !== ''
+      })
     },
   },
 
