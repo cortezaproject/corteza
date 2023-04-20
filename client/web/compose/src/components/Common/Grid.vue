424df000ee21e970bbc2b389a0776d787118f8f3
Jože Fortun <joze.fortun@gmail.com>
2023-04-20 13:57:38 +0200

    Fix new block y position

--- client/web/compose/src/components/Common/Grid.vue
+++ client/web/compose/src/components/Common/Grid.vue
@@ -125,8 +125,8 @@ export default {
           return meta.hidden ? { i, x: 0, y: 0, w: 0, h: 0 } : { i, x, y, w, h }
         })
 
-        // Only use next tick if previous layout exists
-        if (this.layout.length) {
+        // Only use next tick if previous layout exists and not editing
+        if (this.layout.length && !this.editable) {
           this.layout = []
 
           // Next tick is important, otherwise it can lead to overlapping blocks
