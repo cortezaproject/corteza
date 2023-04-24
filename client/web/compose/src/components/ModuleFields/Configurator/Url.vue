dadca706a1ad87bf48b70449dd430ee16d893c3c
Mumbi Francis <kinya.francis.mumbi@gmail.com>
2023-04-24 17:19:50 +0300

    Fix hash and query URL trimming on URL input field

--- client/web/compose/src/components/ModuleFields/Configurator/Url.vue
+++ client/web/compose/src/components/ModuleFields/Configurator/Url.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
     <div>
-      <b-form-checkbox v-model="f.options.trimFragment">
+      <b-form-checkbox v-model="f.options.trimPath">
         {{ $t('kind.url.label') }}
       </b-form-checkbox>
     </div>
     <div>
-      <b-form-checkbox v-model="f.options.trimQuery">
+      <b-form-checkbox v-model="f.options.trimFragment">
         {{ $t('kind.url.trimHash') }}
       </b-form-checkbox>
     </div>
     <div>
-      <b-form-checkbox v-model="f.options.trimPath">
+      <b-form-checkbox v-model="f.options.trimQuery">
         {{ $t('kind.url.trimQuestionMark') }}
       </b-form-checkbox>
     </div>
