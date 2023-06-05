9fbcc0d148ff0864f2e9c885095e58149469e67f
Jože Fortun <joze.fortun@gmail.com>
2023-06-05 12:22:17 +0200

    Adjust reminder UI/UX

--- client/web/compose/src/components/Namespaces/Reminders/List.vue
+++ client/web/compose/src/components/Namespaces/Reminders/List.vue
@@ -1,108 +1,98 @@
 <template>
   <div
-    class="h-100"
+    class="d-flex flex-column h-100"
   >
-    <div
-      v-for="(r, i) in sortedReminders"
-      :key="r.reminderID"
-      :style="`${!!r.dismissedAt ? 'opacity:.6;' : ''}`"
-    >
-      <hr v-if="r.dismissedAt && sortedReminders[i - 1] ? !sortedReminders[i - 1].dismissedAt : false ">
-
+    <div class="flex-fill overflow-auto px-2">
       <div
-        class="overflow-auto border card shadow my-2 p-1"
+        v-for="(r, i) in sortedReminders"
+        :key="r.reminderID"
+        :style="`${!!r.dismissedAt ? 'opacity:.6;' : ''}`"
       >
+        <hr v-if="r.dismissedAt && sortedReminders[i - 1] ? !sortedReminders[i - 1].dismissedAt : false ">
+
         <div
-          class="d-flex flex-row flex-nowrap align-items-center"
+          class="overflow-auto border card shadow-sm my-2 p-1"
         >
-          <b-form-checkbox
-            v-b-tooltip.hover.left.350
-            data-test-id="checkbox-dismiss-reminder"
-            :checked="!!r.dismissedAt"
-            :title="$t(`reminder.${!!r.dismissedAt ? 'undismiss' : 'dismiss'}`)"
-            class="my-2 ml-2"
-            @change="$emit('dismiss', r, $event)"
-          />
-
           <div
-            data-test-id="span-reminder-title"
-            class="text-break text-truncate"
-            :style="`${!!r.dismissedAt ? 'text-decoration: line-through;' : ''}`"
+            class="d-flex flex-row flex-nowrap align-items-center"
           >
-            {{ r.payload.title || r.link || rlLabel(r) || r.linkLabel }}
-          </div>
-
-          <div
-            class="d-flex align-items-center text-primary ml-auto px-2"
-          >
-            <font-awesome-icon
-              v-if="r.snoozeCount"
-              data-test-id="icon-snoozed-reminder"
-              :title="makeTooltip(r)"
-              :icon="['far', 'clock']"
-              class="m-2"
+            <b-form-checkbox
+              v-b-tooltip.hover.left.350
+              data-test-id="checkbox-dismiss-reminder"
+              :checked="!!r.dismissedAt"
+              :title="$t(`reminder.${!!r.dismissedAt ? 'undismiss' : 'dismiss'}`)"
+              class="my-2 ml-2"
+              @change="$emit('dismiss', r, $event)"
             />
 
-            <font-awesome-icon
-              v-if="r.remindAt"
-              data-test-id="icon-remind-at"
-              :title="makeTooltip(r)"
-              :icon="['far', 'bell']"
-              class="m-2"
-            />
-
-            <b-button-group
-              size="sm"
+            <div
+              data-test-id="span-reminder-title"
+              class="text-break text-truncate"
+              :style="`${!!r.dismissedAt ? 'text-decoration: line-through;' : ''}`"
             >
-              <b-button
-                v-if="r.payload.link"
-                :to="recordViewer(r.payload.link)"
-                :title="$t('reminder.recordPageLink')"
-                variant="outline-light"
-                class="d-flex align-items-center py-2 text-primary border-0"
-              >
-                <font-awesome-icon :icon="['far', 'file-alt']" />
-              </b-button>
+              {{ r.payload.title || r.link || rlLabel(r) || r.linkLabel }}
+            </div>
 
-              <b-button
-                data-test-id="button-edit-reminder"
-                variant="outline-light"
-                :title="$t('reminder.edit.label')"
-                class="d-flex align-items-center py-2 text-primary border-0"
-                @click="$emit('edit', r)"
+            <div
+              class="d-flex align-items-center text-primary ml-auto px-2"
+            >
+              <b-button-group
+                size="sm"
               >
-                <font-awesome-icon :icon="['far', 'edit']" />
-              </b-button>
+                <b-button
+                  v-if="r.payload.link"
+                  :to="recordViewer(r.payload.link)"
+                  :title="$t('reminder.recordPageLink')"
+                  variant="outline-light"
+                  class="d-flex align-items-center py-2 text-primary border-0"
+                >
+                  <font-awesome-icon :icon="['far', 'file-alt']" />
+                </b-button>
 
-              <b-button
-                data-test-id="button-delete-reminder"
-                variant="outline-light"
-                :title="$t('reminder.delete')"
-                class="d-flex align-items-center py-2 text-danger border-0"
-                @click.prevent="$emit('delete', r)"
-              >
-                <font-awesome-icon
-                  :icon="['far', 'trash-alt']"
-                />
-              </b-button>
-            </b-button-group>
+                <b-button
+                  data-test-id="button-edit-reminder"
+                  variant="outline-light"
+                  :title="$t('reminder.edit.label')"
+                  class="d-flex align-items-center py-2 text-primary border-0"
+                  @click="$emit('edit', r)"
+                >
+                  <font-awesome-icon :icon="['far', 'edit']" />
+                </b-button>
+
+                <b-button
+                  data-test-id="button-delete-reminder"
+                  variant="outline-light"
+                  :title="$t('reminder.delete')"
+                  class="d-flex align-items-center py-2 text-danger border-0"
+                  @click.prevent="$emit('delete', r)"
+                >
+                  <font-awesome-icon
+                    :icon="['far', 'trash-alt']"
+                  />
+                </b-button>
+              </b-button-group>
+            </div>
           </div>
-        </div>
 
-        <div
-          v-if="r.remindAt"
-          class="text-secondary small px-2 pb-1"
-          :title="makeTooltip(r)"
-        >
-          {{ r.remindAt | locFullDateTime }}
-        </div>
+          <div
+            v-if="r.remindAt"
+            class="text-secondary small px-2 pb-1"
+          >
+            <font-awesome-icon
+              data-test-id="icon-remind-at"
+              :title="$t('reminder.snooze.count', { count: r.snoozeCount })"
+              :icon="['far', 'bell']"
+              class="text-primary"
+            />
+            {{ r.remindAt | locFullDateTime }}
+          </div>
 
-        <div
-          v-if="r.payload.notes"
-          class="text-secondary text-truncate px-2 pb-2 small"
-          :style="`${!!r.dismissedAt ? 'text-decoration: line-through;' : ''}`"
-        >
-          {{ r.payload.notes }}
+          <div
+            v-if="r.payload.notes"
+            class="text-secondary text-truncate px-2 pb-2 small"
+          >
+            {{ r.payload.notes }}
+          </div>
         </div>
       </div>
     </div>
