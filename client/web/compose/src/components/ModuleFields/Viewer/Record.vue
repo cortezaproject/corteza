sjiian/crm

Fix record selector, metric, chart values not refreshed when inline and bulk edit changes are applied to related records

client/web/compose/src/components/ModuleFields/Viewer/Record.vue
@@ -48,6 +48,8 @@ export default {
       recordValues: {},
 
       relRecords: [],
+
+      relatedModuleID: undefined
     }
   },
 
@@ -77,6 +79,7 @@ export default {
     value: {
       immediate: true,
       handler (value) {
+        this.relatedModuleID = undefined
         this.formatRecordValues(value)
       },
     },
@@ -86,12 +89,27 @@ export default {
     this.setDefaultValues()
   },
 
+  mounted () {
+    this.$root.$on('trigger-recordlist-refresh', this.refreshOnRelatedModuleUpdate)
+  },
+
+  beforeDestroy() {
+    this.$root.$off('trigger-recordlist-refresh', this.refreshOnRelatedModuleUpdate)
+  },
+
   methods: {
     ...mapActions({
       findModuleByID: 'module/findByID',
       resolveUsers: 'user/resolveUsers',
       resolveRecords: 'record/resolveRecords',
     }),
+
+
+    refreshOnRelatedModuleUpdate (module) {
+      if (this.relatedModuleID === module.moduleID) {
+        this.formatRecordValues(this.value)
+      }
+    },
 
     linkToRecord (recordID) {
       if (!this.recordPage || !recordID) {
@@ -126,6 +144,8 @@ export default {
 
           const relatedModule = await this.findModuleByID({ namespaceID, moduleID: relatedField.options.moduleID })
           const relatedRecordIDs = new Set()
+
+          this.relatedModuleID = relatedModule.moduleID
 
           records.forEach(r => {
             const recordValue = relatedField.isMulti ? r.values[relatedField.name] : [r.values[relatedField.name]]

client/web/compose/src/components/PageBlocks/ChartBase.vue
@@ -55,6 +55,8 @@ export default {
     this.refreshBlock(this.refresh)
 
     this.$root.$on('drill-down-chart', this.drillDown)
+
+    this.$root.$on('trigger-recordlist-refresh', this.refreshOnRelatedRecordsUpdate)
   },
 
   beforeDestroy () {
@@ -66,6 +68,12 @@ export default {
     ...mapActions({
       findChartByID: 'chart/findByID',
     }),
+
+    refreshOnRelatedRecordsUpdate (module) {
+      if (this.filter.moduleID === module.moduleID) {
+        this.refresh()
+      }
+    },
 
     async fetchChart (params = {}) {
       const { chartID } = this.options
@@ -178,6 +186,7 @@ export default {
 
     destroyEvents () {
       this.$root.$off('drill-down-chart', this.drillDown)
+      this.$root.$off('trigger-recordlist-refresh', this.refreshOnRelatedRecordsUpdate)
     },
   },
 }

cortezaproject/corteza

client/web/compose/src/components/ModuleFields/Viewer/Record.vue
@@ -48,6 +48,8 @@ export default {
       recordValues: {},
 
       relRecords: [],
+
+      relatedModuleID: undefined,
     }
   },
 
@@ -84,6 +86,11 @@ export default {
 
   beforeDestroy () {
     this.setDefaultValues()
+    this.destroyEvents()
+  },
+
+  mounted () {
+    this.$root.$on('module-records-updated', this.refreshOnRelatedModuleUpdate)
   },
 
   methods: {
@@ -92,6 +99,12 @@ export default {
       resolveUsers: 'user/resolveUsers',
       resolveRecords: 'record/resolveRecords',
     }),
+
+    refreshOnRelatedModuleUpdate (module) {
+      if (this.relatedModuleID === module.moduleID) {
+        this.formatRecordValues(this.value)
+      }
+    },
 
     linkToRecord (recordID) {
       if (!this.recordPage || !recordID) {
@@ -126,6 +139,8 @@ export default {
 
           const relatedModule = await this.findModuleByID({ namespaceID, moduleID: relatedField.options.moduleID })
           const relatedRecordIDs = new Set()
+
+          this.relatedModuleID = relatedModule.moduleID
 
           records.forEach(r => {
             const recordValue = relatedField.isMulti ? r.values[relatedField.name] : [r.values[relatedField.name]]
@@ -197,6 +212,11 @@ export default {
       this.processing = false
       this.recordValues = {}
       this.relRecords = []
+      this.relatedModuleID = undefined
+    },
+
+    destroyEvents () {
+      this.$root.$off('module-records-updated', this.refreshOnRelatedModuleUpdate)
     },
   },
 }

client/web/compose/src/components/PageBlocks/ChartBase.vue
@@ -55,6 +55,8 @@ export default {
     this.refreshBlock(this.refresh)
 
     this.$root.$on('drill-down-chart', this.drillDown)
+
+    this.$root.$on('module-records-updated', this.refreshOnRelatedRecordsUpdate)
   },
 
   beforeDestroy () {
@@ -66,6 +68,12 @@ export default {
     ...mapActions({
       findChartByID: 'chart/findByID',
     }),
+
+    refreshOnRelatedRecordsUpdate (module) {
+      if (this.filter.moduleID === module.moduleID) {
+        this.refresh()
+      }
+    },
 
     async fetchChart (params = {}) {
       const { chartID } = this.options
@@ -178,6 +186,7 @@ export default {
 
     destroyEvents () {
       this.$root.$off('drill-down-chart', this.drillDown)
+      this.$root.$off('module-records-updated', this.refreshOnRelatedRecordsUpdate)
     },
   },
 }