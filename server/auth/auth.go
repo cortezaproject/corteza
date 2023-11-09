3cc331c1da074fedf7b68ea8afbf062d8e15876a
Tomaž Jerman <tomaz.jerman121@gmail.com>
2023-11-09 15:58:29 +0100

    Change external auth providers to always reload on settings change

--- server/auth/auth.go
+++ server/auth/auth.go
@@ -340,10 +340,13 @@ func (svc *service) UpdateSettings(s *settings.Settings) {
 		}
 	}
 
-	if len(svc.settings.Providers) != len(s.Providers) {
-		svc.log.Debug("setting changed", zap.Int("providers", len(s.Providers)))
-		external.SetupGothProviders(svc.log, svc.opt.ExternalRedirectURL, s.Providers...)
-	}
+	// Always reload external providers.
+	// This could be optionally skipped by strictly comparing if they actually changed.
+	// OIDC complicates it a bit wit all the mix and matching.
+	// Should be ok for now.
+	// @todo see if we can conditionally skip this
+	svc.log.Debug("setting changed", zap.Int("providers", len(s.Providers)))
+	external.SetupGothProviders(svc.log, svc.opt.ExternalRedirectURL, s.Providers...)
 
 	svc.settings = s
 	svc.handlers.Settings = s
