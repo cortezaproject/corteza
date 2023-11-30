cdee03511c4a63722d1abd28e446a492666f3142
Jože Fortun <joze.fortun@gmail.com>
2023-11-30 13:20:59 +0100

    Fix field description and hint edit being enabled when updating translations for field

--- client/web/compose/src/lib/resource-translations/module-field.js
+++ client/web/compose/src/lib/resource-translations/module-field.js
@@ -4,10 +4,10 @@ export default function (field, translations, currentLanguage, resource) {
   const get = getter(translations, currentLanguage, resource)
 
   field.label = get('label')
-  field.options.description.view = get('meta.description.view')
-  field.options.description.edit = get('meta.description.edit')
-  field.options.hint.view = get('meta.hint.view')
-  field.options.hint.edit = get('meta.hint.edit')
+  field.options.description.view = get('meta.description.view') || undefined
+  field.options.description.edit = get('meta.description.edit') || undefined
+  field.options.hint.view = get('meta.hint.view') || undefined
+  field.options.hint.edit = get('meta.hint.edit') || undefined
 
   if (field.expressions && Array.isArray(field.expressions.validators)) {
     for (const vld of field.expressions.validators) {
