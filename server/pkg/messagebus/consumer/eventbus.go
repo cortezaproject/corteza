0346f90a0c05d26ee5a8e7a84534e6ad9ac98270
Peter Grlica <peter.grlica@gmail.com>
2024-01-08 09:28:01 +0100

    Removed deprecated code from messagebus

--- server/pkg/messagebus/consumer/eventbus.go
+++ server/pkg/messagebus/consumer/eventbus.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/cortezaproject/corteza/server/pkg/eventbus"
 	"github.com/cortezaproject/corteza/server/pkg/messagebus/types"
-	st "github.com/cortezaproject/corteza/server/system/types"
 )
 
 type (
@@ -36,11 +35,4 @@ func NewEventbusConsumer(q string, servicer types.QueueEventBuilder) *EventbusCo
 func (cq *EventbusConsumer) Write(ctx context.Context, p []byte) error {
 	cq.dispatcher.Dispatch(ctx, cq.servicer.CreateQueueEvent(cq.queue, p))
 	return nil
-}
-
-func makeEvent(q string, p []byte) *st.QueueMessage {
-	return &st.QueueMessage{
-		Queue:   q,
-		Payload: p,
-	}
 }
