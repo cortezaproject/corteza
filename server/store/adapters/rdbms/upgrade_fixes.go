f71987fb935b449fc31d3f2a66d3caf906dbf83a
Toocky <toocky@gmail.com>
2026-02-28 19:50:41 -0600

    Fixed Migration

--- server/store/adapters/rdbms/upgrade_fixes.go
+++ server/store/adapters/rdbms/upgrade_fixes.go
@@ -1314,7 +1314,7 @@ func fix_2024_9_7_migrateLabelsValueToJsonb(ctx context.Context, s *Store) (err 
 
 }
 
-func fix_2024_03_00_migrateNamespacePageBlocks(ctx context.Context, s *Store) (err error) {
+func fix_2024_09_07_migrateNamespacePageBlocks(ctx context.Context, s *Store) (err error) {
 	return addColumn(ctx, s,
 		"compose_namespace",
 		&dal.Attribute{Ident: "blocks", Type: &dal.TypeJSON{DefaultValue: "[]"}},
